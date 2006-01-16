2f6c781de0f57fb03cdbeedc37d5f8f23a6f601c
vivainio <>
2006-01-16 21:07:24 +0000

    fixed , and ; autocall to not consider exclusion regexp

--- IPython/iplib.py
+++ IPython/iplib.py
@@ -6,7 +6,7 @@ Requires Python 2.3 or newer.
 
 This file contains all the classes and helper functions specific to IPython.
 
-$Id: iplib.py 1019 2006-01-14 13:02:12Z vivainio $
+$Id: iplib.py 1027 2006-01-16 21:07:24Z vivainio $
 """
 
 #*****************************************************************************
@@ -1894,15 +1894,20 @@ want to merge them back into the new files.""" % locals()
         if not oinfo['found']:
             return self.handle_normal(line,continue_prompt)
         else:
-            #print 'iFun <%s> rest <%s>' % (iFun,theRest) # dbg
+            #print 'pre<%s> iFun <%s> rest <%s>' % (pre,iFun,theRest) # dbg
             if oinfo['isalias']:
                 return self.handle_alias(line,continue_prompt,
                                              pre,iFun,theRest)
 
-            if self.rc.autocall and \
-                   not self.re_exclude_auto.match(theRest) and \
-                   self.re_fun_name.match(iFun) and \
-                   callable(oinfo['obj']) :
+            if (self.rc.autocall 
+                 and
+                   (
+                   #only consider exclusion re if not "," or ";" autoquoting
+                   (pre == self.ESC_QUOTE or pre == self.ESC_QUOTE2) or 
+                   (not self.re_exclude_auto.match(theRest)))
+                 and 
+                   self.re_fun_name.match(iFun) and 
+                   callable(oinfo['obj'])) :
                 #print 'going auto'  # dbg
                 return self.handle_auto(line,continue_prompt,
                                         pre,iFun,theRest,oinfo['obj'])
