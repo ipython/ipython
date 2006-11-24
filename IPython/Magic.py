6c4f009687c4e39e774b8e60013409573193d145
vivainio <>
2006-11-24 15:37:21 +0000

    Kirill Smelkov patch: try to use 'cProfile' instead of the slower pure python 'profile' module

--- IPython/Magic.py
+++ IPython/Magic.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Magic functions for InteractiveShell.
 
-$Id: Magic.py 1922 2006-11-21 20:56:48Z vivainio $"""
+$Id: Magic.py 1927 2006-11-24 15:37:21Z vivainio $"""
 
 #*****************************************************************************
 #       Copyright (C) 2001 Janko Hauser <jhauser@zscout.de> and
@@ -36,11 +36,16 @@ from cStringIO import StringIO
 from getopt import getopt,GetoptError
 from pprint import pprint, pformat
 
-# profile isn't bundled by default in Debian for license reasons
+# cProfile was added in Python2.5
 try:
-    import profile,pstats
+    import cProfile as profile
+    import pstats
 except ImportError:
-    profile = pstats = None
+    # profile isn't bundled by default in Debian for license reasons
+    try:
+        import profile,pstats
+    except ImportError:
+        profile = pstats = None
 
 # Homebrewed
 import IPython
