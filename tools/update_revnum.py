204b7185b928a1ff65189e70dabfcf34ccaefd38
vivainio <>
2006-11-26 21:14:59 +0000

    new tool to set useful (global) revision number to Release.py

---
+++ tools/update_revnum.py
@@ -0,0 +1,13 @@
+""" Change the revision number in Release.py """
+
+import os
+import re
+
+rev = os.popen('svnversion').read().strip()
+
+print "current rev is",rev
+assert ':' not in rev
+
+rfile = open('../IPython/Release.py').read()
+newcont = re.sub(r'revision\s*=.*', "revision = '%s'" % rev, rfile)
+open('../IPython/Release.py','w').write(newcont)
