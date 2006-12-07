29be26c19e40dc823023dbe4df2856209e971568
vivainio <>
2006-12-07 18:14:03 +0000

    do export in make_tarball

--- IPython/Release.py
+++ IPython/Release.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Release data for the IPython project.
 
-$Id: Release.py 1965 2006-12-07 16:07:14Z vivainio $"""
+$Id: Release.py 1968 2006-12-07 18:14:03Z vivainio $"""
 
 #*****************************************************************************
 #       Copyright (C) 2001-2006 Fernando Perez <fperez@colorado.edu>
@@ -22,11 +22,11 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '1964M'
+revision = '1967'
 
 #version = '0.7.3.svn' 
 
-version = '0.7.3.svn.r' + revision.rstrip('M')
+version = '0.7.3b2.r' + revision.rstrip('M')
 
 
 description = "An enhanced interactive Python shell."
