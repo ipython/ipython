afd9d18e4f8cb9a3a706d494c953a5b00130cad9
vivainio <>
2006-12-07 19:17:19 +0000

    update changelog

--- IPython/Release.py
+++ IPython/Release.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Release data for the IPython project.
 
-$Id: Release.py 1969 2006-12-07 18:17:05Z vivainio $"""
+$Id: Release.py 1971 2006-12-07 19:17:19Z vivainio $"""
 
 #*****************************************************************************
 #       Copyright (C) 2001-2006 Fernando Perez <fperez@colorado.edu>
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '1968'
+revision = '1970M'
 
 #version = '0.7.3.svn' 
 
