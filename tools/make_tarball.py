5caf76126fe567c5dd39268f24880b4b66733348
vivainio <>
2006-12-07 16:07:14 +0000

    make_tarball.py fixes

--- tools/make_tarball.py
+++ tools/make_tarball.py
@@ -1,13 +1,18 @@
-import os
+import os,sys,shutil
 
-repo = "http://ipython.scipy.org/svn/ipython/trunk"
+repo = "http://ipython.scipy.org/svn/ipython/ipython/trunk"
 basename = 'ipython'
 workdir = './mkdist'
 
 workdir = os.path.abspath(workdir)
+
+print "working at",workdir
 def oscmd(c):
     print ">",c
-    os.system(c)
+    s = os.system(c)
+    if s:
+        print "Error",s
+        sys.exit(s)
 
 
 assert not os.path.isdir(workdir)
@@ -15,7 +20,8 @@ os.mkdir(workdir)
 os.chdir(workdir)
 
 oscmd('svn co %s %s' % (repo,basename))
-ver = os.popen('svnversion %s' % basename)
+ver = os.popen('svnversion %s' % basename).read().strip()
 tarname = '%s.r%s.tgz' % (basename, ver)
-oscmd('tar czvf %s %s' % (tarname, basename))
+oscmd('tar czvf ../%s %s' % (tarname, basename))
+print "Produced: ",os.path.abspath('../' + tarname)
 shutil.rmtree(workdir)
