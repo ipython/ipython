01dfe124e41e14ec2e4ac9289a5f1168f378f1b1
vivainio <>
2007-08-06 21:14:35 +0000

    beef up mkrel.py

--- tools/mkrel.py
+++ tools/mkrel.py
@@ -1,5 +1,11 @@
+""" Create ipykit, source package and exe installer
+
+requires py2exe
+
+"""
 #!/bin/sh
 # IPython release script
+
 
 import os
 import distutils.dir_util
@@ -8,9 +14,12 @@ def c(cmd):
     os.system(cmd)
 
 os.chdir('..')
-
 distutils.dir_util.remove_tree('dist')
+distutils.dir_util.remove_tree('ipykit')
 
+c("python exesetup.py py2exe")
+os.rename('dist','ipykit')
+
+c("zip -r ipykit.zip ipykit")
 
 c("python setup.py bdist_wininst --install-script=ipython_win_post_install.py")
-c("python exesetup.py py2exe")
