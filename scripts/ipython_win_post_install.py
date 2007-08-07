3edd1b264d5f0d27686ce9f63b17dbe268186170
vivainio <>
2007-08-07 17:10:45 +0000

    sci profile => scipy profile

--- scripts/ipython_win_post_install.py
+++ scripts/ipython_win_post_install.py
@@ -55,7 +55,7 @@ def install():
     mkshortcut(python,'IPython command prompt mode',f,a)
 
     f = ip_dir + r'\scipy.lnk'
-    a = prefix + r'\scripts\ipython -pylab -p sci'
+    a = prefix + r'\scripts\ipython -pylab -p scipy'
     mkshortcut(python,'IPython scipy profile',f,a)
 
     # Create documentation shortcuts ...    
