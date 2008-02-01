e0ca6b387efc3869c0aceaa8bd91adb0faed5f6d
fperez <>
2008-02-01 07:17:00 +0000

    update revnum to make test release

--- IPython/Release.py
+++ IPython/Release.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Release data for the IPython project.
 
-$Id: Release.py 2920 2007-12-31 15:22:11Z fperez $"""
+$Id: Release.py 3002 2008-02-01 07:17:00Z fperez $"""
 
 #*****************************************************************************
 #       Copyright (C) 2001-2006 Fernando Perez <fperez@colorado.edu>
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '2919'
+revision = '3001'
 
 version = '0.8.3.svn.r' + revision.rstrip('M')
 
