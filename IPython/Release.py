98e45a7fc325c6c6ba84e088c480eeaa0e8e06e6
Ville M. Vainio <vivainio@gmail.com>
2008-04-09 21:12:12 +0300

    Do not crash on from __future__ import absolute_import. Close #244.

--- IPython/Release.py
+++ IPython/Release.py
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '95'
+revision = '96'
 branch = 'ipython'
 
 if branch == 'ipython':
