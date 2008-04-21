eae36089cf4a724f7030278611db8966251a3e7c
Ville M. Vainio <vivainio@gmail.com>
2008-04-21 19:51:32 +0300

    do_sphinx: implement pdf creation (for non-windows platforms)

--- doc/do_sphinx.py
+++ doc/do_sphinx.py
@@ -1,4 +1,18 @@
-import os
+import os,sys
+
 def oscmd(c):
-        os.system(c)
+    os.system(c)
+
 oscmd('sphinx-build -d build/doctrees source build/html')
+
+if sys.platform != 'win32':
+    oscmd('sphinx-build -b latex -d build/doctrees source build/latex')
+    os.chdir('build/latex')
+    oscmd('pdflatex ipython.tex')
+    oscmd('pdflatex ipython.tex')
+    oscmd('pdflatex ipython.tex')
+    oscmd('makeindex -s python.ist ipython.idx')
+    oscmd('makeindex -s python.ist modipython.idx')
+    oscmd('pdflatex ipython.tex')
+    oscmd('pdflatex ipython.tex')
+    os.chdir('../..')
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -113,14 +113,14 @@ htmlhelp_basename = 'IPythondoc'
 # ------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
+latex_font_size = '10pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
-#latex_documents = []
+latex_documents = [('ipython','ipython.tex','IPython Documentation','Authors','manual')]
 
 # Additional stuff for the LaTeX preamble.
 #latex_preamble = ''
