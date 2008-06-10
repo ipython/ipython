9ed9604c536da92cd433ee3bec8a2aaab839d72a
Brian E Granger <ellisonbg@gmail.com>
2008-06-10 15:51:52 -0600

    Beginning to organize the rst documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # IPython documentation build configuration file, created by
-# sphinx-quickstart.py on Sat Mar 29 15:36:13 2008.
+# sphinx-quickstart on Thu May  8 16:45:02 2008.
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -11,38 +11,40 @@
 # All configuration values have a default value; values that are commented out
 # serve to show the default value.
 
-import sys
+import sys, os
 
-# If your extensions are in another directory, add it here.
-#sys.path.append('some/directory')
+# If your extensions are in another directory, add it here. If the directory
+# is relative to the documentation root, use os.path.abspath to make it
+# absolute, like shown here.
+#sys.path.append(os.path.abspath('some/directory'))
 
 # General configuration
 # ---------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.addons.*') or your custom ones.
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 #extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = '.txt'
 
 # The master toctree document.
-master_doc = 'ipython'
+master_doc = 'index'
 
 # General substitutions.
 project = 'IPython'
-copyright = '2008, Fernando Perez'
+copyright = '2008, The IPython Development Team'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.8.3'
+version = '0.8.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.8.3'
+release = '0.8.4'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -52,6 +54,10 @@ today_fmt = '%B %d, %Y'
 
 # List of documents that shouldn't be included in the build.
 #unused_docs = []
+
+# List of directories, relative to source directories, that shouldn't be searched
+# for source files.
+#exclude_dirs = []
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
@@ -76,6 +82,14 @@ pygments_style = 'sphinx'
 # given in html_static_path.
 html_style = 'default.css'
 
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+#html_title = None
+
+# The name of an image file (within the static path) to place at the top of
+# the sidebar.
+#html_logo = None
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -88,9 +102,6 @@ html_last_updated_fmt = '%b %d, %Y'
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
 #html_use_smartypants = True
-
-# Content template for the index page.
-#html_index = ''
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
@@ -105,6 +116,14 @@ html_last_updated_fmt = '%b %d, %Y'
 # If true, the reST sources are included in the HTML build as _sources/<name>.
 #html_copy_source = True
 
+# If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#html_use_opensearch = ''
+
+# If nonempty, this is the file name suffix for HTML files (e.g. ".xhtml").
+#html_file_suffix = ''
+
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'IPythondoc'
 
@@ -113,14 +132,24 @@ htmlhelp_basename = 'IPythondoc'
 # ------------------------
 
 # The paper size ('letter' or 'a4').
-latex_paper_size = 'a4'
+latex_paper_size = 'letter'
 
 # The font size ('10pt', '11pt' or '12pt').
 latex_font_size = '10pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
-latex_documents = [('ipython','ipython.tex','IPython Documentation','Fernando Perez (and contributors)','manual')]
+latex_documents = [
+  ('index', 'IPython.tex', 'IPython Documentation', 'The IPython Development Team', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+#latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
 
 # Additional stuff for the LaTeX preamble.
 #latex_preamble = ''
