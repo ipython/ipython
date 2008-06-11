ccc2fc9b32715f9b7721451c8002e6ea9cdc3a0d
Brian E Granger <ellisonbg@gmail.com>
2008-06-11 15:29:58 -0600

    Massive reorganization of the IPython documentation.  It is now ready to be hacked on by users. Overall the transition to Sphinx is great.  BUT, we have lots of broken links in the docs. Developers need to become familiar with how Sphinx handles links.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,9 @@ copyright = '2008, The IPython Development Team'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.8.4'
+version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.8.4'
+release = '0.9'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
