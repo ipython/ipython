7abaf3d52bd9e0d67778d843bd5f99951eb38078
Barry Wark <barrywarkatgmaildotcom>
2008-06-30 15:47:25 -0700

    py2app plugin for Cocoa frontend

---
+++ IPython/frontend/cocoa/plugin/FrontendLoader.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+"""
+Provides a namespace for loading the Cocoa frontend via a Cocoa plugin.
+
+Author: Barry Wark
+"""
+__docformat__ = "restructuredtext en"
+
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2008  The IPython Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+from Foundation import NSObject
+from IPython.frontend.cocoa.cocoa_frontend import IPythonCocoaController
