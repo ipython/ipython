77f5ded5ce3ae79affe65b9973003887e04a7442
Fernando Perez <Fernando.Perez@berkeley.edu>
2008-07-15 01:09:21 -0700

    Clean up docstrings so more doctests pass

--- IPython/kernel/engineservice.py
+++ IPython/kernel/engineservice.py
@@ -226,29 +226,31 @@ class IEngineThreaded(zi.Interface):
 class StrictDict(dict):
     """This is a strict copying dictionary for use as the interface to the 
     properties of an Engine.
+
     :IMPORTANT:
         This object copies the values you set to it, and returns copies to you
         when you request them.  The only way to change properties os explicitly
         through the setitem and getitem of the dictionary interface.
-        Example:
-        >>> e = kernel.get_engine(id)
-        >>> L = someList
+
+    Example:
+        >>> e = get_engine(id)
+        >>> L = [1,2,3]
         >>> e.properties['L'] = L
         >>> L == e.properties['L']
-        ... True
-        >>> L.append(something Else)
+        True
+        >>> L.append(99)
         >>> L == e.properties['L']
-        ... False
+        False
         
-        getitem copies, so calls to methods of objects do not affect the
-        properties, as in the following example:
+        Note that getitem copies, so calls to methods of objects do not affect
+        the properties, as seen here:
+        
         >>> e.properties[1] = range(2)
         >>> print e.properties[1]
-        ... [0, 1]
+        [0, 1]
         >>> e.properties[1].append(2)
         >>> print e.properties[1]
-        ... [0, 1]
-        
+        [0, 1]
     """
     def __init__(self, *args, **kwargs):
         dict.__init__(self, *args, **kwargs)
