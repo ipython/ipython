fddc4586e90773b7bb8e1d7d57a93137650c7729
Brian Granger <ellisonbg@gmail.com>
2008-08-23 22:29:14 -0700

    Skipping a few tests related to the wx frontend that fail on Windows.

--- setup.py
+++ setup.py
@@ -140,15 +140,15 @@ if 'setuptools' in sys.modules:
             'ipythonx = IPython.frontend.wx.ipythonx:main'
         ]
     }
-    setup_args["extras_require"] = dict(
+    setup_args['extras_require'] = dict(
         kernel = [
-            "zope.interface>=3.4.1",
-            "Twisted>=8.0.1",
-            "foolscap>=0.2.6"        
+            'zope.interface>=3.4.1',
+            'Twisted>=8.0.1',
+            'foolscap>=0.2.6'
         ],
-        doc=['Sphinx>=0.3','pygments'],
+        doc='Sphinx>=0.3',
         test='nose>=0.10.1',
-        security=["pyOpenSSL>=0.6"]
+        security='pyOpenSSL>=0.6'
     )
     # Allow setuptools to handle the scripts
     scripts = []
