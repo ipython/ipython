42062fa7a7a3923b7a1d5cc6d2db11774aba7c8c
Ville M. Vainio <vivainio@gmail.com>
2008-08-27 20:25:35 +0300

    add ipy_miscapps.py, with one completer (surfraw)

---
+++ IPython/Extensions/ipy_miscapps.py
@@ -0,0 +1,19 @@
+""" Completers for miscellaneous command line apps
+
+"""
+import IPython.ipapi
+ip = IPython.ipapi.get()
+import os
+
+def surfraw_completer(self,cmdline):
+    """ Completer for 'surfraw'
+
+    example::
+      sr go<tob>  => sr google
+
+    """
+    compl = [l.split(None,1)[0] for l in os.popen('sr -elvi')]
+    return compl
+
+
+ip.set_hook('complete_command', surfraw_completer, str_key = 'sr')
