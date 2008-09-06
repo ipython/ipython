dfd921008287b08c4dfd152c81f0d1d15157c6d7
gvaroquaux <gvaroquaux@gvaroquaux-desktop>
2008-09-05 22:26:50 -0500

    Add a test for the error previously discovered.

--- setupegg.py
+++ setupegg.py
@@ -5,9 +5,9 @@ import os
 import sys
 
 # Add my local path to sys.path
-home = os.environ['HOME']
-sys.path.insert(0,'%s/usr/local/lib/python%s/site-packages' %
-                (home,sys.version[:3]))
+#home = os.environ['HOME']
+#sys.path.insert(0,'%s/usr/local/lib/python%s/site-packages' %
+#                (home,sys.version[:3]))
 
 # now, import setuptools and call the actual setup
 import setuptools
