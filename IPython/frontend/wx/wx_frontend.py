979aa92d83a0a5f398e5a849d39c655aab88696d
gvaroquaux <gvaroquaux@gvaroquaux-desktop>
2008-09-08 14:44:07 -0500

    Remove one more Yield, to make the frontend more robust under Vista.

--- IPython/frontend/wx/wx_frontend.py
+++ IPython/frontend/wx/wx_frontend.py
@@ -233,8 +233,7 @@ class WxController(ConsoleWidget, PrefilterFrontEnd):
             for name in symbol_string.split('.')[1:] + ['__doc__']:
                 symbol = getattr(symbol, name)
             self.AutoCompCancel()
-            wx.Yield()
-            self.CallTipShow(self.GetCurrentPos(), symbol)
+            wx.CallAfter(self.CallTipShow, self.GetCurrentPos(), symbol)
         except:
             # The retrieve symbol couldn't be converted to a string
             pass
