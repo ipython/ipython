835fe7acd6590b5219a0a9a3ad88501127342237
Ville M. Vainio <vivainio@gmail.com>
2008-09-13 17:01:46 +0300

    sh profile: renders _prompt_title variable in prompt (because changing the whole prompt is pain)

--- IPython/Extensions/ipy_profile_sh.py
+++ IPython/Extensions/ipy_profile_sh.py
@@ -50,9 +50,15 @@ def main():
     ip.ex('import os')
     ip.ex("def up(): os.chdir('..')")
     ip.user_ns['LA'] = LastArgFinder() 
-    # Nice prompt
     
-    o.prompt_in1= r'\C_LightBlue[\C_LightCyan\Y2\C_LightBlue]\C_Green|\#> '
+    # You can assign to _prompt_title variable 
+    # to provide some extra information for prompt
+    # (e.g. the current mode, host/username...)
+
+    ip.user_ns['_prompt_title'] = ''
+    
+    # Nice prompt
+    o.prompt_in1= r'\C_Green${_prompt_title}\C_LightBlue[\C_LightCyan\Y2\C_LightBlue]\C_Green|\#> '
     o.prompt_in2= r'\C_Green|\C_LightGreen\D\C_Green> '
     o.prompt_out= '<\#> '
     
