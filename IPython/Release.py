e8de50bbe09fe7954ada22ed13e6248aa8c94842
Fernando Perez <Fernando.Perez@berkeley.edu>
2008-09-13 22:02:36 -0700

    Checkpoint before merging with upstream

--- IPython/Release.py
+++ IPython/Release.py
@@ -23,7 +23,7 @@ name = 'ipython'
 development = False    # change this to False to do a release
 version_base = '0.9'
 branch = 'ipython'
-revision = '1143'
+revision = '1137'
 
 if development:
     if branch == 'ipython':
