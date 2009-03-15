1681b2798ee829bd6ad78cd66a93be81280ea6f8
Robert Kern <rkern@Sacrilege.local>
2009-03-14 20:15:07 -0500

    BUG: Refactor the extension a little bit in response to review. Added an activate() function. Exposed the registration functions. Exposed the example dtype_pprinter in code rather than docstring. Added some doctests (not running yet!).

--- IPython/Extensions/ipy_pretty.py
+++ IPython/Extensions/ipy_pretty.py
@@ -1,32 +1,19 @@
 """ Use pretty.py for configurable pretty-printing.
 
-Register pretty-printers for types using pretty.for_type() or
-pretty.for_type_by_name(). For example, to make a pretty-printer for numpy dtype
-objects, add the following to your ipy_user_conf.py::
+Register pretty-printers for types using ipy_pretty.for_type() or
+ipy_pretty.for_type_by_name(). For example, to use the example pretty-printer
+for numpy dtype objects, add the following to your ipy_user_conf.py::
 
     from IPython.Extensions import ipy_pretty
-    from IPython.external import pretty
 
-    def dtype_pprinter(obj, p, cycle):
-        if cycle:
-            return p.text('dtype(...)')
-        if obj.fields is None:
-            p.text(repr(obj))
-        else:
-            p.begin_group(7, 'dtype([')
-            for i, field in enumerate(obj.descr):
-                if i > 0:
-                    p.text(',')
-                    p.breakable()
-                p.pretty(field)
-            p.end_group(7, '])')
+    ipy_pretty.activate()
 
     # If you want to have numpy always imported anyways:
     import numpy
-    pretty.for_type(numpy.dtype, dtype_pprinter)
+    ipy_pretty.for_type(numpy.dtype, ipy_pretty.dtype_pprinter)
 
     # If you don't want to have numpy imported until it needs to be:
-    pretty.for_type_by_name('numpy', 'dtype', dtype_pprinter)
+    ipy_pretty.for_type_by_name('numpy', 'dtype', ipy_pretty.dtype_pprinter)
 """
 
 import IPython.ipapi
@@ -35,6 +22,9 @@ from IPython.genutils import Term
 from IPython.external import pretty
 
 ip = IPython.ipapi.get()
+
+
+#### Implementation ############################################################
 
 def pretty_result_display(self, arg):
     """ Uber-pretty-printing display hook.
@@ -54,5 +44,89 @@ def pretty_result_display(self, arg):
     else:
         raise TryNext
 
-ip.set_hook('result_display', pretty_result_display, priority=99)
+
+#### API #######################################################################
+
+# Expose the for_type and for_type_by_name functions for easier use.
+for_type = pretty.for_type
+for_type_by_name = pretty.for_type_by_name
+
+
+# FIXME: write deactivate(). We need a way to remove a hook.
+def activate():
+    """ Activate this extension.
+    """
+    ip.set_hook('result_display', pretty_result_display, priority=99)
+
+
+#### Example pretty-printers ###################################################
+
+def dtype_pprinter(obj, p, cycle):
+    """ A pretty-printer for numpy dtype objects.
+    """
+    if cycle:
+        return p.text('dtype(...)')
+    if obj.fields is None:
+        p.text(repr(obj))
+    else:
+        p.begin_group(7, 'dtype([')
+        for i, field in enumerate(obj.descr):
+            if i > 0:
+                p.text(',')
+                p.breakable()
+            p.pretty(field)
+        p.end_group(7, '])')
+
+
+#### Tests #####################################################################
+
+def test_pretty():
+    """
+    In [1]: from IPython.Extensions import ipy_pretty
+
+    In [2]: ipy_pretty.activate()
+
+    In [3]: class A(object):
+       ...:     def __repr__(self):
+       ...:         return 'A()'
+       ...:     
+       ...:     
+
+    In [4]: a = A()
+
+    In [5]: a
+    Out[5]: A()
+
+    In [6]: def a_pretty_printer(obj, p, cycle):
+       ...:     p.text('<A>')
+       ...:     
+       ...:     
+
+    In [7]: ipy_pretty.for_type(A, a_pretty_printer)
+
+    In [8]: a
+    Out[8]: <A>
+
+    In [9]: class B(object):
+       ...:     def __repr__(self):
+       ...:         return 'B()'
+       ...:     
+       ...:     
+
+    In [10]: B.__module__, B.__name__
+    Out[10]: ('__main__', 'B')
+
+    In [11]: def b_pretty_printer(obj, p, cycle):
+       ....:     p.text('<B>')
+       ....:     
+       ....:     
+
+    In [12]: ipy_pretty.for_type_by_name('__main__', 'B', b_pretty_printer)
+
+    In [13]: b = B()
+
+    In [14]: b
+    Out[14]: <B>
+    """
+    assert False, "This should only be doctested, not run."
 
