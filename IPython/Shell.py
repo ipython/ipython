7316506792757efd328c7acf40f369ef256ed46c
Brian Granger <ellisonbg@gmail.com>
2009-07-20 15:04:20 -0700

    Added compatibility layer for Shell/ipapi/iplib.
    These 3 top-level modules have been moved to core.  But, to ease the
    transition to their new location, we have re-created these modules
    at the top-level.  The new top-level modules print a warning statement
    and simple import the moved modules for now.  In the long run, we can
    use these for further compatibility related things.

---
+++ IPython/Shell.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+A backwards compatibility layer for IPython.Shell.
+
+Previously, IPython had an IPython.Shell module.  IPython.Shell has been moved
+to IPython.core.shell and is being refactored.  This new module is provided
+for backwards compatability.  We strongly encourage everyone to start using
+the new code in IPython.core.shell.
+"""
+
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2008-2009  The IPython Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+from warnings import warn
+
+msg = """
+This module (IPython.Shell) has been moved to a new location
+(IPython.core.shell) and is being refactored.  Please update your code
+to use the new IPython.core.shell module"""
+
+warn(msg, category=DeprecationWarning, stacklevel=1)
+
+from IPython.core.shell import *
+
