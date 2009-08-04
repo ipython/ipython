31ce543b1a891fcecce69b8582f75a752a513dbf
Fernando Perez <Fernando.Perez@berkeley.edu>
2009-08-04 16:38:35 -0700

    Opening trunk for 0.11 and development.
    The 0.10 series now will hold any fixes for that series.  The trunk is open
    for the refactoring work that will become 0.11.

--- IPython/Release.py
+++ IPython/Release.py
@@ -20,10 +20,10 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-development = False    # change this to False to do a release
-version_base = '0.10'
+development = True    # change this to False to do a release
+version_base = '0.11'
 branch = 'ipython'
-revision = '1204'
+revision = '1205'
 
 if development:
     if branch == 'ipython':
