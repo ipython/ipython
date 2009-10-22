27359b4d87b2499f7d76728895f4166db1a1b3c8
Brian Granger <ellisonbg@gmail.com>
2009-10-21 20:22:33 -0700

    Minor revisions to ipengine_config.py

--- IPython/config/default/ipengine_config.py
+++ IPython/config/default/ipengine_config.py
@@ -1,6 +1,6 @@
 c = get_config()
 
-c.MPI.use = 'mpi4py'
+c.MPI.use = ''
 
 c.MPI.mpi4py = """from mpi4py import MPI as mpi
 mpi.size = mpi.COMM_WORLD.Get_size()
@@ -16,10 +16,17 @@ mpi.size = 0
 """
 
 c.Global.log_to_file = False
-c.Global.exec_lines = []
-c.Global.log_dir_name = 'log'
-c.Global.security_dir_name = 'security'
-c.Global.shell_class = 'IPython.kernel.core.interpreter.Interpreter'
-self.default_config.Global.furl_file_name = 'ipcontroller-engine.furl'
-self.default_config.Global.furl_file = ''
 
+c.Global.exec_lines = ['import numpy']
+
+c.Global.log_dir_name = 'log'
+
+c.Global.security_dir_name = 'security'
+
+c.Global.log_level = 10
+
+c.Global.shell_class = 'IPython.kernel.core.interpreter.Interpreter'
+
+c.Global.furl_file_name = 'ipcontroller-engine.furl'
+
+c.Global.furl_file = ''
