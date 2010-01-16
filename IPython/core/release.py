311f84ebd5024dbd56faa32756deca70fe5a24b7
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-01-16 15:25:18 -0800

    Doc updates to testing to reflect recent changes.
    Updated revnumber to push an updated testing release out for this branch.

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -23,7 +23,7 @@ name = 'ipython'
 development = True    # change this to False to do a release
 version_base = '0.11'
 branch = 'ipython'
-revision = '1321'
+revision = '1340'
 
 if development:
     if branch == 'ipython':
