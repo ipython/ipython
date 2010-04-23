e74d2ebedb53a1932f362a334a5fade8fa9a2c5c
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-04-22 22:17:26 -0700

    Fix print_function bug for python 2.5 as well.

--- IPython/Magic.py
+++ IPython/Magic.py
@@ -42,6 +42,11 @@ except ImportError:
     except ImportError:
         profile = pstats = None
 
+# print_function was added to __future__ in Python2.6, remove this when we drop
+# 2.5 compatibility
+if not hasattr(__future__,'CO_FUTURE_PRINT_FUNCTION'):
+    __future__.CO_FUTURE_PRINT_FUNCTION = 65536
+
 # Homebrewed
 import IPython
 from IPython import Debugger, OInspect, wildcard
