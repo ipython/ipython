8ecc9ec44437fea301ab8bdf6ed8b9b9a2a5c242
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-08-27 13:45:44 -0700

    Fix svg rich backend to correctly show multiple figures.

--- IPython/zmq/pylab/backend_payload_svg.py
+++ IPython/zmq/pylab/backend_payload_svg.py
@@ -1,3 +1,9 @@
+"""Produce SVG versions of active plots for display by the rich Qt frontend.
+"""
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
 # Standard library imports
 from cStringIO import StringIO
 
@@ -8,12 +14,14 @@ from matplotlib._pylab_helpers import Gcf
 # Local imports.
 from backend_payload import add_plot_payload
 
+#-----------------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------------
 
 def show():
     """ Deliver a SVG payload.
     """
-    figure_manager = Gcf.get_active()
-    if figure_manager is not None:
+    for figure_manager in Gcf.get_all_fig_managers():
         # Make the background transparent.
         # figure_manager.canvas.figure.patch.set_alpha(0.0)
         # Set the background to white instead so it looks good on black.
@@ -21,6 +29,7 @@ def show():
         figure_manager.canvas.figure.set_edgecolor('white')
         data = svg_from_canvas(figure_manager.canvas)
         add_plot_payload('svg', data)
+
 
 def svg_from_canvas(canvas):
     """ Return a string containing the SVG representation of a FigureCanvasSvg.
