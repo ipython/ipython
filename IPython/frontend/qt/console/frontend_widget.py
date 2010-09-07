0490e17e9851d60adc334a664ab616fdfec75853
epatters <epatters@enthought.com>
2010-09-07 15:51:49 -0500

    Moved HistoryConsoleWidget to its own file to make console_widget.py a little smaller.

--- IPython/frontend/qt/console/frontend_widget.py
+++ IPython/frontend/qt/console/frontend_widget.py
@@ -15,7 +15,7 @@ from IPython.utils.traitlets import Bool
 from bracket_matcher import BracketMatcher
 from call_tip_widget import CallTipWidget
 from completion_lexer import CompletionLexer
-from console_widget import HistoryConsoleWidget
+from history_console_widget import HistoryConsoleWidget
 from pygments_highlighter import PygmentsHighlighter
 
 
