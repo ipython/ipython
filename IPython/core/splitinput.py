3f194000f95e10aaa135ce3b03e0d85018f6c1e9
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-09-16 00:34:59 -0700

    Unicode fix for case when stdin is a pipe (like during test suite)

--- IPython/core/splitinput.py
+++ IPython/core/splitinput.py
@@ -58,7 +58,10 @@ def split_user_input(line, pattern=None):
     """
     # We need to ensure that the rest of this routine deals only with unicode
     if type(line)==str:
-        line = line.decode(sys.stdin.encoding)
+        codec = sys.stdin.encoding
+        if codec is None:
+            codec = 'utf-8'
+        line = line.decode(codec)
         
     if pattern is None:
         pattern = line_split
