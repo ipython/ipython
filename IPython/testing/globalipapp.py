65b72354238fc66c2ed91d9a51ece5c325b16444
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-09-16 00:37:22 -0700

    Fix small bug in test suite for calling ipython's getoutput.x

--- IPython/testing/globalipapp.py
+++ IPython/testing/globalipapp.py
@@ -114,7 +114,7 @@ def xsys(self, cmd):
     """
     # We use getoutput, but we need to strip it because pexpect captures
     # the trailing newline differently from commands.getoutput
-    print(self.getoutput(cmd).rstrip(), end='', file=sys.stdout)
+    print(self.getoutput(cmd, split=False).rstrip(), end='', file=sys.stdout)
     sys.stdout.flush()
 
 
