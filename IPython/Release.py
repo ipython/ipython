8f60cada5d3c87f7722a5fae4be144fd2b1b74f9
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-10-12 00:15:01 -0700

    Open 0.10.2 branch for development.
    I don't expect to see much happen in this branch, but we'll have it
    available so people can contribute bug fixes and make pull requests.
    
    If we get enough contributions here, eventually we'll put a 0.10.2
    release out.

--- IPython/Release.py
+++ IPython/Release.py
@@ -20,8 +20,8 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-development = False    # change this to False to do a release
-version_base = '0.10.1'
+development = True    # change this to False to do a release
+version_base = '0.10.2'
 branch = 'ipython'
 
 if development:
