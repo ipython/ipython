cf5ffcf5e770b8ee2abf0dbf32d95f0fbe2e6082
Thomas Kluyver <takowl@gmail.com>
2011-01-13 01:04:46 +0000

    Reinstate newlines in %save and %macro, first part of issue 245.

--- IPython/core/macro.py
+++ IPython/core/macro.py
@@ -21,7 +21,7 @@ class Macro(IPyAutocall):
 
     def __init__(self,data):
         """store the macro value, as a single string which can be executed"""
-        self.value = ''.join(data).rstrip()+'\n'
+        self.value = '\n'.join(data).rstrip()+'\n'
         
     def __str__(self):
         return self.value
