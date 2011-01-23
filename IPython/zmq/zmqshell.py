00041a1fdbe74088ed35cb689d41ea4f2f3a8341
Brian Granger <ellisonbg@gmail.com>
2011-01-22 16:17:25 -0800

    Using session.send in DisplayPublisher now.

--- IPython/zmq/zmqshell.py
+++ IPython/zmq/zmqshell.py
@@ -91,11 +91,14 @@ class ZMQDisplayPublisher(DisplayPublisher):
         if metadata is None:
             metadata = {}
         self._validate_data(source, data, metadata)
-        msg = self.session.msg(u'display_data', {}, parent=self.parent_header)
-        msg['content']['source'] = source
-        msg['content']['data'] = data
-        msg['content']['metadata'] = metadata
-        self.pub_socket.send_json(msg)
+        content = {}
+        content['source'] = source
+        content['data'] = data
+        content['metadata'] = metadata
+        self.session.send(
+            self.pub_socket, u'display_data', content,
+            parent=self.parent_header
+        )
 
 
 class ZMQInteractiveShell(InteractiveShell):
