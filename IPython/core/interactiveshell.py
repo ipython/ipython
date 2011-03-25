7a726af7fc337a81041b0c59ccdcdd6d41c4e83b
Brian Granger <ellisonbg@gmail.com>
2011-03-25 12:12:11 -0700

    Starting help in InteractiveShell.

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -189,7 +189,16 @@ class InteractiveShell(Configurable, Magic):
     """An enhanced, interactive shell for Python."""
 
     _instance = None
-    autocall = Enum((0,1,2), default_value=1, config=True)
+    autocall = Enum((0,1,2), default_value=1, config=True,
+        help=
+        """Make IPython automatically call any callable object even if you
+        didn't type explicit parentheses. For example, 'str 43' becomes
+        'str(43)' automatically.  The value can be '0' to disable the feature,
+        '1' for 'smart' autocall, where it is not applied if there are no more
+        arguments on the line, and '2' for 'full' autocall, where all callable
+        objects are automatically called (even if no arguments are present).
+        The default is '1'."""
+    )
     # TODO: remove all autoindent logic and put into frontends.
     # We can't do this yet because even runlines uses the autoindent.
     autoindent = CBool(True, config=True)
