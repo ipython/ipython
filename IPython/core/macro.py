66344e2a9a02387cadb78350f703a2b778e708a9
Thomas Kluyver <takowl@gmail.com>
2011-04-01 23:34:59 +0100

    Refactor code-finding logic, and use it for %save and %macro as well.

--- IPython/core/macro.py
+++ IPython/core/macro.py
@@ -7,7 +7,12 @@
 #  the file COPYING, distributed as part of this software.
 #*****************************************************************************
 
+import re
+import sys
+
 import IPython.utils.io
+
+coding_declaration = re.compile(r"#\s*coding[:=]\s*([-\w.]+)")
 
 class Macro(object):
     """Simple class to store the value of macros as strings.
@@ -20,9 +25,24 @@ class Macro(object):
 
     def __init__(self,code):
         """store the macro value, as a single string which can be executed"""
-        self.value = code.rstrip()+'\n'
-        
+        lines = []
+        enc = None
+        for line in code.splitlines():
+            coding_match = coding_declaration.match(line)
+            if coding_match:
+                enc = coding_match.group(1)
+            else:
+                lines.append(line)
+        code = "\n".join(lines)
+        if isinstance(code, bytes):
+            code = code.decode(enc or sys.getdefaultencoding())
+        self.value = code + '\n'
+    
     def __str__(self):
+        enc = sys.stdin.encoding or sys.getdefaultencoding()
+        return self.value.encode(enc, "replace")
+    
+    def __unicode__(self):
         return self.value
 
     def __repr__(self):
