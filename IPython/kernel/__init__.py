584e9d75c49c848f6e0d18dbf5029296bd8ed47a
Brian Granger <ellisonbg@gmail.com>
2011-04-06 12:58:04 -0700

    Fixed docstring of IPython.kernel.__init__.

--- IPython/kernel/__init__.py
+++ IPython/kernel/__init__.py
@@ -1,6 +1,10 @@
-msg = """IPython.kernel has been replaced by IPython.parallel.
+"""IPython.kernel has been replaced by IPython.parallel.
+
+The previous version of IPython's parallel library was located at this
+location (IPython.kernel). It has been moved to the IPython.parallel
+subpackage and has been refactored to use zeromq/pyzmq instead of twisted.
 
 Please see INSERT URL for further details.
 """
 
-raise ImportError(msg)
+raise ImportError(__doc__)
