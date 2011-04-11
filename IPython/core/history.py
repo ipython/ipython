4a9c6fa823e7992c884a6b96b20a4912f044ba4a
Thomas Kluyver <takowl@gmail.com>
2011-04-11 22:34:38 +0100

    History expects single output per cell, and doesn't use JSON to store them in the database.

--- IPython/core/history.py
+++ IPython/core/history.py
@@ -15,13 +15,10 @@ from __future__ import print_function
 # Stdlib imports
 import atexit
 import datetime
-import json
 import os
 import re
 import sqlite3
 import threading
-
-from collections import defaultdict
 
 # Our own packages
 from IPython.config.configurable import Configurable
@@ -56,11 +53,10 @@ class HistoryManager(Configurable):
             return []
 
     # A dict of output history, keyed with ints from the shell's
-    # execution count. If there are several outputs from one command,
-    # only the last one is stored.
+    # execution count.
     output_hist = Dict()
-    # Contains all outputs, in lists of reprs.
-    output_hist_reprs = Instance(defaultdict, args=(list,))
+    # The text/plain repr of outputs.
+    output_hist_reprs = Dict()
 
     # String holding the path to the history file
     hist_file = Unicode(config=True)
@@ -218,9 +214,7 @@ class HistoryManager(Configurable):
         cur = self.db.execute("SELECT session, line, %s FROM %s " %\
                                 (toget, sqlfrom) + sql, params)
         if output:    # Regroup into 3-tuples, and parse JSON
-            loads = lambda out: json.loads(out) if out else None
-            return ((ses, lin, (inp, loads(out))) \
-                                        for ses, lin, inp, out in cur)
+            return ((ses, lin, (inp, out)) for ses, lin, inp, out in cur)
         return cur
         
     
@@ -419,9 +413,9 @@ class HistoryManager(Configurable):
         line_num : int
           The line number from which to save outputs
         """
-        if (not self.db_log_output) or not self.output_hist_reprs[line_num]:
+        if (not self.db_log_output) or (line_num not in self.output_hist_reprs):
             return
-        output = json.dumps(self.output_hist_reprs[line_num])
+        output = self.output_hist_reprs[line_num]
         
         with self.db_output_cache_lock:
             self.db_output_cache.append((line_num, output))
@@ -696,7 +690,7 @@ def magic_history(self, parameter_s = ''):
                 inline = "\n... ".join(inline.splitlines()) + "\n..."
         print(inline, file=outfile)
         if get_output and output:
-            print("\n".join(output), file=outfile)
+            print(output, file=outfile)
 
     if close_at_end:
         outfile.close()
