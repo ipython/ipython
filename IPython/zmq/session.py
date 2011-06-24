319dfea2cbcbdeea336db6984baf85f04eb2b18f
MinRK <benjaminrk@gmail.com>
2011-06-23 22:36:33 -0700

    use CBytes instead of Bytes in Session

--- IPython/zmq/session.py
+++ IPython/zmq/session.py
@@ -47,7 +47,7 @@ from zmq.eventloop.zmqstream import ZMQStream
 from IPython.config.configurable import Configurable, LoggingConfigurable
 from IPython.utils.importstring import import_item
 from IPython.utils.jsonutil import extract_dates, squash_dates, date_default
-from IPython.utils.traitlets import (Bytes, Unicode, Bool, Any, Instance, Set,
+from IPython.utils.traitlets import (CBytes, Unicode, Bool, Any, Instance, Set,
                                         DottedObjectName)
 
 #-----------------------------------------------------------------------------
@@ -239,7 +239,7 @@ class Session(Configurable):
         else:
             self.unpack = import_item(str(new))
         
-    session = Bytes(b'', config=True,
+    session = CBytes(b'', config=True,
         help="""The UUID identifying this session.""")
     def _session_default(self):
         return bytes(uuid.uuid4())
@@ -248,7 +248,7 @@ class Session(Configurable):
         help="""Username for the Session. Default is your system username.""")
     
     # message signature related traits:
-    key = Bytes(b'', config=True,
+    key = CBytes(b'', config=True,
         help="""execution key, for extra authentication.""")
     def _key_changed(self, name, old, new):
         if new:
