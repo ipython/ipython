6bc24fa32394bb740edaaa6199b9f70cbfe2c37f
MinRK <benjaminrk@gmail.com>
2011-07-05 22:24:51 -0700

    reorder qt support in kernel
    import priority:
    
    1. ask matplotlib (if imported) - if PyQt4 specified, use API v1
    2. ask QT_API - if pyqt specified, use API v2
    3. if nothing specified, try PyQt4 v1, fallback on PySide.

--- IPython/external/qt.py
+++ IPython/external/qt.py
@@ -1,4 +1,9 @@
 """ A Qt API selector that can be used to switch between PyQt and PySide.
+
+This uses the ETS 4.0 selection pattern of:
+PySide first, PyQt with API v2. second.
+
+Do not use this if you need PyQt with the old QString/QVariant API.
 """
 
 import os
