546c8fbcad23911ecfc9a6c846e05c07bcce0ad8
Ben Edwards <bedwards@sausage.(none)>
2011-07-15 08:44:06 -0500

    Revert "Display source code correctly for decorated functions."
    This reverts commit 1b4b159ff1425bed893b3a0e1bf7a874094078b2.
    
    Conflicts:
    
    	IPython/core/oinspect.py

--- IPython/core/oinspect.py
+++ IPython/core/oinspect.py
@@ -129,15 +129,18 @@ def getsource(obj,is_binary=False):
     - is_binary: whether the object is known to come from a binary source.
     This implementation will skip returning any output for binary objects, but
     custom extractors may know how to meaningfully process them."""
-
+    
     if is_binary:
         return None
     else:
+<<<<<<< HEAD
         #get source if obj was decoratred with @decorator
         while hasattr(obj,"__wrapped__"):
             if obj is obj.__wrapped__:
                 break
             obj = obj.__wrapped__
+=======
+>>>>>>> parent of 1b4b159... Display source code correctly for decorated functions.
         try:
             src = inspect.getsource(obj)
         except TypeError:
