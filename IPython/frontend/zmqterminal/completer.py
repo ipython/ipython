8360d3a9c6a782dc9c959b88867db44366335074
MinRK <benjaminrk@gmail.com>
2011-07-18 11:10:01 -0700

    zmqterminal subclasses TerminalInteractiveShell/IPApp
    this does a lot of the boiler plate for us, so Omar's communication
    code is used in place of run_cell / complete.
    
    Now colors, prompts, indentation, cl-args, etc. are all inherited
    from regular existing code.
    
    keyboard interrupts don't work yet, the kernel just dies...

--- IPython/frontend/zmqterminal/completer.py
+++ IPython/frontend/zmqterminal/completer.py
@@ -2,17 +2,17 @@
 import readline
 from Queue import Empty
 
-class ClientCompleter2p(object):
+class ZMQCompleter(object):
     """Client-side completion machinery.
 
     How it works: self.complete will be called multiple times, with
     state=0,1,2,... When state=0 it should compute ALL the completion matches,
     and then return them for each value of state."""
     
-    def __init__(self,client, km):
+    def __init__(self, shell, km):
+        self.shell = shell
         self.km =  km
         self.matches = []
-        self.client = client
         
     def complete_request(self,text):
         line = readline.get_line_buffer()
@@ -20,15 +20,15 @@ class ClientCompleter2p(object):
         
         # send completion request to kernel
         # Give the kernel up to 0.5s to respond
-        msg_id = self.km.xreq_channel.complete(text=text, line=line,
+        msg_id = self.km.shell_channel.complete(text=text, line=line,
                                                         cursor_pos=cursor_pos)
         
-        msg_xreq = self.km.xreq_channel.get_msg(timeout=0.5)
-        if msg_xreq['parent_header']['msg_id'] == msg_id:
-            return msg_xreq["content"]["matches"]
+        msg = self.km.shell_channel.get_msg(timeout=0.5)
+        if msg['parent_header']['msg_id'] == msg_id:
+            return msg["content"]["matches"]
         return []
     
-    def complete(self, text, state):
+    def rlcomplete(self, text, state):
         if state == 0:
             try:
                 self.matches = self.complete_request(text)
@@ -39,3 +39,6 @@ class ClientCompleter2p(object):
             return self.matches[state]
         except IndexError:
             return None
+    
+    def complete(self, text, line, cursor_pos=None):
+        return self.rlcomplete(text, 0)
