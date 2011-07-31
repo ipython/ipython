e9b1ebbf2e96e82704c880eea51235ae6bb01d42
Thomas Spura <thomas.spura@gmail.com>
2011-07-31 16:22:14 +0200

    Fix rpmlint: incorrect-fsf-address
    """
    The Free Software Foundation address in this file seems to be outdated or
    misspelled.  Ask upstream to update the address, or if this is a license file,
    possibly the entire file with a new copy available from the FSF.
    """
    Replacing it with the correct one.

--- IPython/external/ssh/forward.py
+++ IPython/external/ssh/forward.py
@@ -17,7 +17,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Paramiko; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+# 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA.
 
 """
 Sample script showing how to do local port forwarding over paramiko.
