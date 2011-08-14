6c940b74d142b3c629c4c65429aec0016a4d480f
Thomas Kluyver <takowl@gmail.com>
2011-08-14 14:02:18 +0100

    Safer loading of default config inside Python 3 config.

--- IPython/config/profile/python3/ipython_config.py
+++ IPython/config/profile/python3/ipython_config.py
@@ -1,3 +1,11 @@
-# This can be used at any point in a config file to load a sub config
-# and merge it into the current one.
-load_subconfig('ipython_config.py', profile='default')
+c = get_config()
+
+# If the master config file uses syntax that's invalid in Python 3, we'll skip
+# it and just use the factory defaults.
+try:
+    load_subconfig('ipython_config.py', profile='default')
+except Exception:
+    pass
+else:
+    # We reset exec_lines in case they're not compatible with Python 3.
+    c.InteractiveShellApp.exec_lines = []
