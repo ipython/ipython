3c0f17f82bfa1b8b12ea52c6b1aa55ca45dbdcdd
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2011-09-13 09:20:42 +0200

    Added the close handler

--- docs/examples/lib/gui-pyglet.py
+++ docs/examples/lib/gui-pyglet.py
@@ -18,6 +18,10 @@ label = pyglet.text.Label('Hello, world',
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
 @window.event
+def on_close():
+    window.close()
+
+@window.event
 def on_draw():
     window.clear()
     label.draw()
