0aab55d359800255153b9b7ce2deb284f3c7beef
Thomas Kluyver <takowl@gmail.com>
2011-09-18 17:28:59 +0100

    Fix for notebook in Python 3.

--- IPython/frontend/html/notebook/handlers.py
+++ IPython/frontend/html/notebook/handlers.py
@@ -172,7 +172,7 @@ class ZMQStreamHandler(websocket.WebSocketHandler):
 
 class AuthenticatedZMQStreamHandler(ZMQStreamHandler):
     def open(self, kernel_id):
-        self.kernel_id = kernel_id
+        self.kernel_id = kernel_id.decode('ascii')
         self.session = Session()
         self.save_on_message = self.on_message
         self.on_message = self.on_first_message
