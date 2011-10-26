21bd93534e6056cdecdb66f27d8b3befa59850c7
Thomas Kluyver <takowl@gmail.com>
2011-10-26 21:47:22 +0100

    Add decorators to mark known failures on Python 3.

--- IPython/testing/skipdoctest.py
+++ IPython/testing/skipdoctest.py
@@ -4,6 +4,7 @@ The IPython.testing.decorators module triggers various extra imports, including
 numpy and sympy if they're present. Since this decorator is used in core parts
 of IPython, it's in a separate module so that running IPython doesn't trigger
 those imports."""
+import sys
 
 def skip_doctest(f):
     """Decorator - mark a function or method for skipping its doctest.
@@ -12,4 +13,9 @@ def skip_doctest(f):
     omit from testing, while preserving the docstring for introspection, help,
     etc."""
     f.skip_doctest = True
+    return f
+
+def skip_doctest_py3(f):
+    """Decorator - skip the doctest under Python 3."""
+    f.skip_doctest = (sys.version_info[0] >= 3)
     return f
