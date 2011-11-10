49af183272a9c231562bb99dc3989b5c5e6fbad1
MinRK <benjaminrk@gmail.com>
2011-11-10 14:57:12 -0800

    shorten json indent to 1sp

--- IPython/nbformat/v2/nbjson.py
+++ IPython/nbformat/v2/nbjson.py
@@ -51,7 +51,7 @@ class JSONWriter(NotebookWriter):
 
     def writes(self, nb, **kwargs):
         kwargs['cls'] = BytesEncoder
-        kwargs['indent'] = 4
+        kwargs['indent'] = 1
         kwargs['sort_keys'] = True
         if kwargs.pop('split_lines', True):
             nb = split_lines(copy.deepcopy(nb))
