09314aeef3ee332ba5326373b93626f1c315b9a9
Thomas Kluyver <takowl@gmail.com>
2011-11-27 01:30:20 +0000

    Update copyright notice on _pexpect.py

--- IPython/external/pexpect/_pexpect.py
+++ IPython/external/pexpect/_pexpect.py
@@ -61,7 +61,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Pexpect Copyright (c) 2010 Noah Spurrier
+Pexpect Copyright (c) 2008-2011 Noah Spurrier
 http://pexpect.sourceforge.net/
 """
 
