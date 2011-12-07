c87e2e83a06b2c995f32fb2805c4a07e3e57e809
Thomas Kluyver <takowl@gmail.com>
2011-12-07 23:04:59 +0000

    Add test for gh-1117

--- IPython/core/tests/test_handlers.py
+++ IPython/core/tests/test_handlers.py
@@ -132,6 +132,8 @@ def test_handlers():
     run([
         ('len "abc"',       'len "abc"'),
         ('autocallable',    'autocallable()'),
+        # Don't add extra brackets (gh-1117)
+        ('autocallable()',    'autocallable ()'),
         (",list 1 2 3",     'list("1", "2", "3")'),
         (";list 1 2 3",     'list("1 2 3")'),
         ("/len range(1,4)", 'len(range(1,4))'),
