7d2dff1a0a0303021b715527deaf182995bb2ea8
Brian Granger <ellisonbg@gmail.com>
2012-01-17 14:31:14 -0800

    Updating to CodeMirror 2.2, latest stable release.

--- IPython/frontend/html/notebook/static/codemirror/mode/markdown/markdown.js
+++ IPython/frontend/html/notebook/static/codemirror/mode/markdown/markdown.js
@@ -3,12 +3,12 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   var htmlMode = CodeMirror.getMode(cmCfg, { name: 'xml', htmlMode: true });
 
   var header   = 'header'
-  ,   code     = 'code'
+  ,   code     = 'comment'
   ,   quote    = 'quote'
-  ,   list     = 'list'
+  ,   list     = 'string'
   ,   hr       = 'hr'
-  ,   linktext = 'linktext'
-  ,   linkhref = 'linkhref'
+  ,   linktext = 'link'
+  ,   linkhref = 'string'
   ,   em       = 'em'
   ,   strong   = 'strong'
   ,   emstrong = 'emstrong';
@@ -38,11 +38,11 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
       stream.skipToEnd();
       return code;
     }
-
+    
     if (stream.eatSpace()) {
       return null;
     }
-
+    
     if (stream.peek() === '#' || stream.match(headerRE)) {
       stream.skipToEnd();
       return header;
@@ -50,9 +50,6 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
     if (stream.eat('>')) {
       state.indentation++;
       return quote;
-    }
-    if (stream.peek() === '<') {
-      return switchBlock(stream, state, htmlBlock);
     }
     if (stream.peek() === '[') {
       return switchInline(stream, state, footnoteLink);
@@ -63,62 +60,70 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
         return hr;
       }
     }
-
+    
     var match;
     if (match = stream.match(ulRE, true) || stream.match(olRE, true)) {
       state.indentation += match[0].length;
       return list;
     }
-
+    
     return switchInline(stream, state, state.inline);
   }
 
   function htmlBlock(stream, state) {
-    var type = htmlMode.token(stream, state.htmlState);
-    if (stream.eol() && !state.htmlState.context) {
+    var style = htmlMode.token(stream, state.htmlState);
+    if (style === 'tag' && state.htmlState.type !== 'openTag' && !state.htmlState.context) {
+      state.f = inlineNormal;
       state.block = blockNormal;
     }
-    return type;
+    return style;
   }
 
 
   // Inline
+  function getType(state) {
+    return state.strong ? (state.em ? emstrong : strong)
+                        : (state.em ? em       : null);
+  }
+
+  function handleText(stream, state) {
+    if (stream.match(textRE, true)) {
+      return getType(state);
+    }
+    return undefined;        
+  }
 
   function inlineNormal(stream, state) {
-    function getType() {
-      return state.strong ? (state.em ? emstrong : strong)
-                          : (state.em ? em       : null);
-    }
-
-    if (stream.match(textRE, true)) {
-      return getType();
-    }
-
+    var style = state.text(stream, state)
+    if (typeof style !== 'undefined')
+      return style;
+    
     var ch = stream.next();
-
+    
     if (ch === '\\') {
       stream.next();
-      return getType();
+      return getType(state);
     }
     if (ch === '`') {
       return switchInline(stream, state, inlineElement(code, '`'));
     }
-    if (ch === '<') {
-      return switchInline(stream, state, inlineElement(linktext, '>'));
-    }
     if (ch === '[') {
       return switchInline(stream, state, linkText);
     }
-
-    var t = getType();
-    if (ch === '*' || ch === '_') {
-      if (stream.eat(ch)) {
-        return (state.strong = !state.strong) ? getType() : t;
-      }
-      return (state.em = !state.em) ? getType() : t;
+    if (ch === '<' && stream.match(/^\w/, false)) {
+      stream.backUp(1);
+      return switchBlock(stream, state, htmlBlock);
     }
 
-    return getType();
+    var t = getType(state);
+    if (ch === '*' || ch === '_') {
+      if (stream.eat(ch)) {
+        return (state.strong = !state.strong) ? getType(state) : t;
+      }
+      return (state.em = !state.em) ? getType(state) : t;
+    }
+    
+    return getType(state);
   }
 
   function linkText(stream, state) {
@@ -157,17 +162,20 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
     return linkhref;
   }
 
+  function inlineRE(endChar) {
+    if (!inlineRE[endChar]) {
+      // match any not-escaped-non-endChar and any escaped char
+      // then match endChar or eol
+      inlineRE[endChar] = new RegExp('^(?:[^\\\\\\' + endChar + ']|\\\\.)*(?:\\' + endChar + '|$)');
+    }
+    return inlineRE[endChar];
+  }
+
   function inlineElement(type, endChar, next) {
     next = next || inlineNormal;
     return function(stream, state) {
-      while (!stream.eol()) {
-        var ch = stream.next();
-        if (ch === '\\') stream.next();
-        if (ch === endChar) {
-          state.inline = state.f = next;
-          return type;
-        }
-      }
+      stream.match(inlineRE(endChar));
+      state.inline = state.f = next;
       return type;
     };
   }
@@ -176,12 +184,13 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
     startState: function() {
       return {
         f: blockNormal,
-
+        
         block: blockNormal,
         htmlState: htmlMode.startState(),
         indentation: 0,
-
+        
         inline: inlineNormal,
+        text: handleText,
         em: false,
         strong: false
       };
@@ -190,12 +199,13 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
     copyState: function(s) {
       return {
         f: s.f,
-
+        
         block: s.block,
         htmlState: CodeMirror.copyState(htmlMode, s.htmlState),
         indentation: s.indentation,
-
+        
         inline: s.inline,
+        text: s.text,
         em: s.em,
         strong: s.strong
       };
@@ -218,11 +228,13 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
           }
         }
         state.indentation = currentIndentation;
-
+        
         if (currentIndentation > 0) return null;
       }
       return state.f(stream, state);
-    }
+    },
+
+    getType: getType
   };
 
 });
