26a70f4b6c6573568d4acf83cd0cf6b81e7f3734
Thomas Kluyver <takowl@gmail.com>
2012-03-07 21:53:22 +0000

    Add test for installing, loading, unloading an extension.

--- IPython/core/tests/test_magic.py
+++ IPython/core/tests/test_magic.py
@@ -15,6 +15,7 @@ import nose.tools as nt
 from IPython.testing import decorators as dec
 from IPython.testing import tools as tt
 from IPython.utils import py3compat
+from IPython.utils.tempdir import TemporaryDirectory
 
 #-----------------------------------------------------------------------------
 # Test functions begin
@@ -394,3 +395,20 @@ def test_prun_quotes():
     "Test that prun does not clobber string escapes (GH #1302)"
     _ip.magic("prun -q x = '\t'")
     nt.assert_equal(_ip.user_ns['x'], '\t')
+
+def test_extension():
+    tmpdir = TemporaryDirectory()
+    orig_ipython_dir = _ip.ipython_dir
+    try:
+        _ip.ipython_dir = tmpdir.name
+        nt.assert_raises(ImportError, _ip.magic, "load_ext daft_extension")
+        url = os.path.join(os.path.dirname(__file__), "daft_extension.py")
+        _ip.magic("install_ext %s" % url)
+        _ip.user_ns.pop('arq', None)
+        _ip.magic("load_ext daft_extension")
+        tt.assert_equal(_ip.user_ns['arq'], 185)
+        _ip.magic("unload_ext daft_extension")
+        assert 'arq' not in _ip.user_ns
+    finally:
+        _ip.ipython_dir = orig_ipython_dir
+        
