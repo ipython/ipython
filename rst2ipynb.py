eb51d5750909b763f258f85b9360ceb6928ca471
Paul Ivanov <pivanov314@gmail.com>
2012-03-20 18:21:07 -0700

    unused import and typos

--- rst2ipynb.py
+++ rst2ipynb.py
@@ -6,12 +6,11 @@ A minimal front end to the Docutils Publisher, producing an ipython notebook.
 import docutils.readers.standalone
 import docutils.parsers.rst
 import docutils.core
-import docutils.io
 import rst2ipynblib
 from docutils.core import publish_cmdline
 
-description = ('Generates an ipython notebooks from standalone '
-               'reStructuredText sources. ' +
+description = ('Generates an ipython notebook from standalone '
+               'reStructuredText source. ' +
                docutils.core.default_description)
 
 publish_cmdline(reader=docutils.readers.standalone.Reader(),
