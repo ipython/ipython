27ee2752a71ee415154c40e1978edb9d5221a331
Bradley M. Froehle <brad.froehle@gmail.com>
2012-04-17 11:49:59 -0700

    Reformat test to a standard style.

--- IPython/lib/tests/test_deepreload.py
+++ IPython/lib/tests/test_deepreload.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
 """Test suite for the deepreload module."""
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
 
 from IPython.testing import decorators as dec
 from IPython.lib.deepreload import reload as dreload
+
+#-----------------------------------------------------------------------------
+# Test functions begin
+#-----------------------------------------------------------------------------
 
 @dec.skipif_not_numpy
 def test_deepreload_numpy():
