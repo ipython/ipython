446de5d3bb24a744de1eb0161c230d043f865bdd
MinRK <benjaminrk@gmail.com>
2012-04-16 20:21:41 -0700

    0.12.1 rc1

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -22,7 +22,7 @@ name = 'ipython'
 _version_major = 0
 _version_minor = 12
 _version_micro = 1  # use '' for first of series, number for 1 and above
-_version_extra = 'dev'
+_version_extra = 'rc1'
 #_version_extra = 'rc1'
 #_version_extra = ''  # Uncomment this for full releases
 
