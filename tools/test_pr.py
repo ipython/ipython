6ceb0bb2a46fdc20a797fa3c92ace7080b3de62f
Thomas Kluyver <takowl@gmail.com>
2012-05-02 23:04:37 +0100

    PR tester can now post results to Github as a comment.

--- tools/test_pr.py
+++ tools/test_pr.py
@@ -13,6 +13,7 @@ from glob import glob
 import json
 import os
 import re
+import requests
 import shutil
 from subprocess import call, check_call, check_output, PIPE, STDOUT, CalledProcessError
 try:
@@ -20,9 +21,12 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
+import gh_auth
+
 basedir = os.path.join(os.path.expanduser("~"), ".ipy_pr_tests")
 repodir = os.path.join(basedir, "ipython")
 ipy_repository = 'git://github.com/ipython/ipython.git'
+gh_project="ipython/ipython"
 
 supported_pythons = ['python2.6', 'python2.7', 'python3.1', 'python3.2']
 unavailable_pythons = []
@@ -66,8 +70,8 @@ def setup():
     check_call(['git', 'pull', ipy_repository, 'master'])
     os.chdir(basedir)
 
-def get_pull_request(num, project="ipython/ipython"):
-    url = "https://api.github.com/repos/{project}/pulls/{num}".format(project=project, num=num)
+def get_pull_request(num):
+    url = "https://api.github.com/repos/{project}/pulls/{num}".format(project=gh_project, num=num)
     response = urlopen(url).read().decode('utf-8')
     return json.loads(response)
 
@@ -102,7 +106,7 @@ def run_tests(venv):
     try:
         return True, check_output([iptest], stderr=STDOUT).decode('utf-8')
     except CalledProcessError as e:
-        return False, e.output
+        return False, e.output.decode('utf-8')
 
 def post_gist(content, description='IPython test log', filename="results.log"):
     """Post some text to a Gist, and return the URL."""
@@ -119,6 +123,34 @@ def post_gist(content, description='IPython test log', filename="results.log"):
     response = urlopen("https://api.github.com/gists", post_data)
     response_data = json.loads(response.read().decode('utf-8'))
     return response_data['html_url']
+
+def markdown_format(pr, results):
+    def format_result(py, passed, gist_url, missing_libraries):
+        s = "* %s: " % py
+        if passed:
+            s += "OK"
+        else:
+            s += "Failed, log at %s" % gist_url
+        if missing_libraries:
+            s += " (libraries not available: " + missing_libraries + ")"
+        return s
+    
+    lines = ["**Test results for commit %s merged into master**" % pr['head']['sha'][:7],
+             "Platform: " + sys.platform,
+             ""] + \
+            [format_result(*r) for r in results] + \
+            ["",
+             "Not available for testing:" + ", ".join(unavailable_pythons)] 
+    return "\n".join(lines)
+
+def post_results_comment(pr, results, num):
+    body = markdown_format(pr, results)
+    url = 'https://api.github.com/repos/{project}/issues/{num}/comments'.format(project=gh_project, num=num)
+    payload = json.dumps({'body': body})
+    auth_token = gh_auth.get_auth_token()
+    headers = {'Authorization': 'token ' + auth_token}
+    r = requests.post(url, data=payload, headers=headers)
+            
 
 if __name__ == '__main__':
     import sys
@@ -151,3 +183,5 @@ if __name__ == '__main__':
         if missing_libraries:
             print("    Libraries not available:", missing_libraries)
     print("Not available for testing:", ", ".join(unavailable_pythons))
+    post_results_comment(pr, results, num)
+    print("(Posted to Github)")
