3adb246f4c3c66ad542a52cb728ce8ded3e9718d
MinRK <benjaminrk@gmail.com>
2012-05-10 18:03:47 -0700

    add --no-ff to git pull in test_pr
    fast-forward could result in the subsequent `git commit` call failing with nothing to commit.

--- tools/test_pr.py
+++ tools/test_pr.py
@@ -85,7 +85,7 @@ def get_branch(repo, branch, owner, mergeable):
         # Delete the branch first
         call(['git', 'branch', '-D', merged_branch])
         check_call(['git', 'checkout', '-b', merged_branch])
-        check_call(['git', 'pull', '--no-commit', repo, branch])
+        check_call(['git', 'pull', '--no-ff', '--no-commit', repo, branch])
         check_call(['git', 'commit', '-m', "merge %s/%s" % (repo, branch)])
     else:
         # Fetch the branch without merging it.
