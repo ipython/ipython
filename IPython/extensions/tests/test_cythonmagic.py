ba7083e181bd05ae01e73c11a30263e54a39d6c9
Brian Granger <ellisonbg@gmail.com>
2012-05-27 21:07:08 -0700

    Using better approach for skipping test if Cython no pres.

--- IPython/extensions/tests/test_cythonmagic.py
+++ IPython/extensions/tests/test_cythonmagic.py
@@ -12,34 +12,34 @@ code = """def f(x):
 try:
     import Cython
 except:
-    pass
-else:
-    def setup():
-        ip = get_ipython()
-        ip.extension_manager.load_extension('cythonmagic')
+    __test__ = False
 
-    def test_cython_inline():
-        ip = get_ipython()
-        ip.ex('a=10; b=20')
-        result = ip.run_cell_magic('cython_inline','','return a+b')
-        nt.assert_equals(result, 30)
+def setup():
+    ip = get_ipython()
+    ip.extension_manager.load_extension('cythonmagic')
 
-    def test_cython_pyximport():
-        module_name = '_test_cython_pyximport'
-        ip = get_ipython()
-        ip.run_cell_magic('cython_pyximport', module_name, code)
-        ip.ex('g = f(10)')
-        nt.assert_equals(ip.user_ns['g'], 20.0)
-        try:
-            os.remove(module_name+'.pyx')
-        except OSError:
-            pass
+def test_cython_inline():
+    ip = get_ipython()
+    ip.ex('a=10; b=20')
+    result = ip.run_cell_magic('cython_inline','','return a+b')
+    nt.assert_equals(result, 30)
 
-    def test_cython():
-        ip = get_ipython()
-        ip.run_cell_magic('cython', '', code)
-        ip.ex('g = f(10)')
-        nt.assert_equals(ip.user_ns['g'], 20.0)        
+def test_cython_pyximport():
+    module_name = '_test_cython_pyximport'
+    ip = get_ipython()
+    ip.run_cell_magic('cython_pyximport', module_name, code)
+    ip.ex('g = f(10)')
+    nt.assert_equals(ip.user_ns['g'], 20.0)
+    try:
+        os.remove(module_name+'.pyx')
+    except OSError:
+        pass
+
+def test_cython():
+    ip = get_ipython()
+    ip.run_cell_magic('cython', '', code)
+    ip.ex('g = f(10)')
+    nt.assert_equals(ip.user_ns['g'], 20.0)        
 
 
 
