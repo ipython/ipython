b0204e0817d0c51eab0535593d2c4426a8e50358
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-06-09 07:07:17 -0300

    pep 8 edits

--- IPython/extensions/tests/test_virtualenvmagic.py
+++ IPython/extensions/tests/test_virtualenvmagic.py
@@ -4,15 +4,16 @@ Tests for the virtualenvmagic extension
 Author: Flávio Codeço Coelho - @fccoelho
 """
 
-import os
 import nose.tools as nt
 
 
 def setup():
     ip = get_ipython()
     ip.extension_manager.load_extension('virtualenvmagic')
-    
+
+
 def test_virtualenv():
     ip = get_ipython()
-    result = ip.run_cell_magic('virtualenv','pypyEnv','import sys;print sys.version')
+    result = ip.run_cell_magic('virtualenv', 'pypyEnv', 'import sys;print\
+     sys.version')
     nt.assert_true('PyPy' in result)
--- IPython/extensions/virtualenvmagic.py
+++ IPython/extensions/virtualenvmagic.py
@@ -19,24 +19,25 @@ def virtualenv(line, cell):
     pre-existing virtual env. It Requires Virtualenv and VirtualenvWrapper
     to be installed in the system.
     The virtual env to be used must be created in advance.
-    
+
     Usage
     =====
     To activate this magic just write at the top of the cell:
-    
+
         %%virtualenv my_env
         import sys
         print sys.version
-        
-    
+
+
     """
-    if not os.path.exists(os.environ['WORKON_HOME']+'/'+line):
+    if not os.path.exists(os.environ['WORKON_HOME'] + '/' + line):
         print >> sys.stderr, "Environment {} does no exist.".format(line)
         return
-    env_activate_cmd = 'bash -c "source {}/{}/bin/activate && python -"'.format(os.environ['WORKON_HOME'],line)
+    env_activate_cmd = 'bash -c "source {}/{}/bin/activate && python -"'\
+    .format(os.environ['WORKON_HOME'], line)
     cmd = shlex.split(env_activate_cmd)
     p = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-    out,err = p.communicate(cell)
+    out, err = p.communicate(cell)
     p.wait()
     if err:
         print >> sys.stderr, err
@@ -44,14 +45,16 @@ def virtualenv(line, cell):
 
 _loaded = False
 
+
 def load_ipython_extension(ip):
-    """Load the extension in IPython."""
+    """
+    Load the extension in IPython.
+    """
     global _loaded
     if not _loaded:
         if 'WORKON_HOME' in os.environ:
             ip.register_magic_function(virtualenv, 'cell')
             _loaded = True
         else:
-            print >> sys.stderr, "You must have Virtualenv and VirtualenvWrapper installed."
-
-
+            print >> sys.stderr, "You must have Virtualenv and \
+            VirtualenvWrapper installed."
