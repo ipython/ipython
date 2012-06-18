e382085c89371002706ab6a9efd31ca1ed79796c
Fernando Perez <Fernando.Perez@berkeley.edu>
2012-06-17 23:34:03 -0700

    Mark version info with beta1 tag.

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -22,7 +22,8 @@ name = 'ipython'
 _version_major = 0
 _version_minor = 13
 _version_micro = ''  # use '' for first of series, number for 1 and above
-_version_extra = 'dev'
+#_version_extra = 'dev'
+_version_extra = 'beta1'
 #_version_extra = 'rc1'
 #_version_extra = ''  # Uncomment this for full releases
 
