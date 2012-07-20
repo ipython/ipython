9203caef8c3fd211baa18113f21c0d0f4afad7ca
MinRK <benjaminrk@gmail.com>
2012-07-19 23:04:53 -0500

    add some basic css/js for HTML export

---
+++ js/initmathjax.js
@@ -0,0 +1,17 @@
+init_mathjax = function() {
+    if (window.MathJax) {
+        // MathJax loaded
+        MathJax.Hub.Config({
+            tex2jax: {
+                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
+            },
+            displayAlign: 'left', // Change this to 'center' to center equations.
+            "HTML-CSS": {
+                styles: {'.MathJax_Display': {"margin": 0}}
+            }
+        });
+        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+    }
+}
+init_mathjax();
