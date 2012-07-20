c466c4922e8e0c9628f3aa158dd622a1487b8b30
Bradley M. Froehle <brad.froehle@gmail.com>
2012-07-20 09:28:52 -0700

    Fix indentation of help text in %magic.

--- IPython/core/magics/basic.py
+++ IPython/core/magics/basic.py
@@ -22,7 +22,7 @@ from pprint import pformat
 from IPython.core import magic_arguments
 from IPython.core.error import UsageError
 from IPython.core.magic import Magics, magics_class, line_magic, magic_escapes
-from IPython.utils.text import format_screen
+from IPython.utils.text import format_screen, dedent, indent
 from IPython.core import magic_arguments, page
 from IPython.testing.skipdoctest import skip_doctest
 from IPython.utils.ipstruct import Struct
@@ -147,15 +147,17 @@ class BasicMagics(Magics):
         docs = mman.lsmagic_docs(brief, missing='No documentation')
 
         if rest:
-            format_string = '**%s%s**::\n\n\t%s\n\n'
+            format_string = '**%s%s**::\n\n%s\n\n'
         else:
-            format_string = '%s%s:\n\t%s\n'
+            format_string = '%s%s:\n%s\n'
 
         return ''.join(
-            [format_string % (magic_escapes['line'], fname, fndoc)
+            [format_string % (magic_escapes['line'], fname,
+                              indent(dedent(fndoc)))
              for fname, fndoc in sorted(docs['line'].items())]
             +
-            [format_string % (magic_escapes['cell'], fname, fndoc)
+            [format_string % (magic_escapes['cell'], fname,
+                              indent(dedent(fndoc)))
              for fname, fndoc in sorted(docs['cell'].items())]
         )
 
