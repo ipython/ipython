c765312f6ff6915b3bc7b8c505f54eb7e503be38
Ryan May <rmay31@gmail.com>
2012-07-21 00:52:00 -0500

    Add tests of switching logic.

--- IPython/core/tests/test_pylabtools.py
+++ IPython/core/tests/test_pylabtools.py
@@ -61,3 +61,68 @@ def test_import_pylab():
     pt.import_pylab(ns, import_all=False)
     nt.assert_true('plt' in ns)
     nt.assert_equal(ns['np'], np)
+
+
+class TestPylabSwitch(object):
+    class Shell(object):
+        pylab_gui_select = None
+
+    def setup(self):
+        self._save_am = pt.activate_matplotlib
+        pt.activate_matplotlib = lambda *a,**kw:None
+        self._save_ip = pt.import_pylab
+        pt.import_pylab = lambda *a,**kw:None
+        self._save_cis = pt.configure_inline_support
+        pt.configure_inline_support = lambda *a,**kw:None
+
+    def teardown(self):
+        pt.activate_matplotlib = self._save_am
+        pt.import_pylab = self._save_ip
+        pt.configure_inline_support = self._save_cis
+
+    def test_qt(self):
+        s = self.Shell()
+        gui = pt.pylab_activate(dict(), 'qt', False, s)
+        nt.assert_equal(gui, 'qt')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+        gui = pt.pylab_activate(dict(), 'inline', False, s)
+        nt.assert_equal(gui, 'inline')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+        gui = pt.pylab_activate(dict(), None, False, s)
+        nt.assert_equal(gui, 'qt')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+        gui = pt.pylab_activate(dict(), 'inline', False, s)
+        nt.assert_equal(gui, 'inline')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+        gui = pt.pylab_activate(dict(), None, False, s)
+        nt.assert_equal(gui, 'qt')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+    def test_inline(self):
+        s = self.Shell()
+        gui = pt.pylab_activate(dict(), 'inline', False, s)
+        nt.assert_equal(gui, 'inline')
+        nt.assert_equal(s.pylab_gui_select, None)
+
+        gui = pt.pylab_activate(dict(), 'inline', False, s)
+        nt.assert_equal(gui, 'inline')
+        nt.assert_equal(s.pylab_gui_select, None)
+
+        gui = pt.pylab_activate(dict(), 'qt', False, s)
+        nt.assert_equal(gui, 'qt')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+    def test_qt_gtk(self):
+        s = self.Shell()
+        gui = pt.pylab_activate(dict(), 'qt', False, s)
+        nt.assert_equal(gui, 'qt')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
+        gui = pt.pylab_activate(dict(), 'gtk', False, s)
+        nt.assert_equal(gui, 'qt')
+        nt.assert_equal(s.pylab_gui_select, 'qt')
+
