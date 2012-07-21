1c1222fa0845abb03ceec55390600fe07628fa79
MinRK <benjaminrk@gmail.com>
2012-07-21 09:55:23 -0500

    Backport PR #2086: re-aliad %ed to %edit in qtconsole
    fixes #2085
    
    should be backported to 0.13.1

--- IPython/zmq/zmqshell.py
+++ IPython/zmq/zmqshell.py
@@ -575,6 +575,7 @@ class ZMQInteractiveShell(InteractiveShell):
     def init_magics(self):
         super(ZMQInteractiveShell, self).init_magics()
         self.register_magics(KernelMagics)
+        self.run_line_magic('alias_magic', 'ed edit')
 
 
 
