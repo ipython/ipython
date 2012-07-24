55f0234b4b1f4659e7ea0aa4f8e34f84aba051d7
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2012-07-24 22:53:54 +0200

    prototype sticky notification

--- IPython/frontend/html/notebook/static/js/notificationwidget.js
+++ IPython/frontend/html/notebook/static/js/notificationwidget.js
@@ -21,75 +21,24 @@ var IPython = (function (IPython) {
         if (this.selector !== undefined) {
             this.element = $(selector);
             this.style();
-            this.bind_events();
+            //this.bind_events();
         }
+        this.element.button();
+        this.element.hide();
+        var that = this;
+        this.element.click(function(){
+            that.element.fadeOut(100, function () {that.element.html('');});
+            if (that.timeout !== undefined) {
+                that.timeout = undefined
+                clearTimeout(that.timeout);
+            }
+        });
     };
 
 
     NotificationWidget.prototype.style = function () {
         this.element.addClass('notification ui-widget ui-widget-content ui-corner-all');
         this.element.addClass('border-box-sizing');
-    };
-
-
-    NotificationWidget.prototype.bind_events = function () {
-        var that = this;
-        // Kernel events
-        $([IPython.events]).on('status_idle.Kernel',function () {
-            IPython.save_widget.update_document_title();
-            if (that.get_message() === 'Kernel busy') {
-                that.element.fadeOut(100, function () {
-                    that.element.html('');
-                });
-            };
-        });
-        $([IPython.events]).on('status_busy.Kernel',function () {
-            window.document.title='(Busy) '+window.document.title;
-            that.set_message("Kernel busy");
-        });
-        $([IPython.events]).on('status_restarting.Kernel',function () {
-            IPython.save_widget.update_document_title();
-            that.set_message("Restarting kernel",500);
-        });
-        $([IPython.events]).on('status_interrupting.Kernel',function () {
-            that.set_message("Interrupting kernel",500);
-        });
-        $([IPython.events]).on('status_dead.Kernel',function () {
-            var dialog = $('<div/>');
-            dialog.html('The kernel has died, would you like to restart it? If you do not restart the kernel, you will be able to save the notebook, but running code will not work until the notebook is reopened.');
-            $(document).append(dialog);
-            dialog.dialog({
-                resizable: false,
-                modal: true,
-                title: "Dead kernel",
-                buttons : {
-                    "Restart": function () {
-                        $([IPython.events]).trigger('status_restarting.Kernel');
-                        IPython.notebook.start_kernel();
-                        $(this).dialog('close');
-                    },
-                    "Continue running": function () {
-                        $(this).dialog('close');
-                    }
-                }
-            });
-        });
-        // Notebook events
-        $([IPython.events]).on('notebook_loading.Notebook', function () {
-            that.set_message("Loading notebook",500);
-        });
-        $([IPython.events]).on('notebook_loaded.Notebook', function () {
-            that.set_message("Notebook loaded",500);
-        });
-        $([IPython.events]).on('notebook_saving.Notebook', function () {
-            that.set_message("Saving notebook",500);
-        });
-        $([IPython.events]).on('notebook_saved.Notebook', function () {
-            that.set_message("Notebook saved",2000);
-        });
-        $([IPython.events]).on('notebook_save_failed.Notebook', function () {
-            that.set_message("Notebook save failed",2000);
-        });
     };
 
 
@@ -101,11 +50,11 @@ var IPython = (function (IPython) {
             clearTimeout(this.timeout);
             this.timeout = null;
         };
-        if (timeout !== undefined) {
+        if (timeout !== undefined && timeout >=0) {
             this.timeout = setTimeout(function () {
                 that.element.fadeOut(100, function () {that.element.html('');});
                 that.timeout = null;
-            }, timeout)
+            }, timeout);
         };
     };
 
