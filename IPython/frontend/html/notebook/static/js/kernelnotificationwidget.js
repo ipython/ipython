88f925f979a819e8400158c04155589d539dd317
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2012-07-25 09:08:08 +0200

    separate kernel notification from save/load notification

--- IPython/frontend/html/notebook/static/js/kernelnotificationwidget.js
+++ IPython/frontend/html/notebook/static/js/kernelnotificationwidget.js
@@ -11,111 +11,73 @@
 
 var IPython = (function (IPython) {
 
-    var utils = IPython.utils;
+    IPython.kernelnotificationwidget = new IPython.NotificationWidget('#notification');
 
-
-    var NotificationWidget = function (selector) {
-        this.selector = selector;
-        this.timeout = null;
-        this.busy = false;
-        if (this.selector !== undefined) {
-            this.element = $(selector);
-            this.style();
-            this.bind_events();
+    var knw = IPython.kernelnotificationwidget;
+    // Kernel events
+    $([IPython.events]).on('status_idle.Kernel',function () {
+        IPython.save_widget.update_document_title();
+        knw.set_message('Kernel Idle',200);
         }
-    };
+    );
 
+    $([IPython.events]).on('status_busy.Kernel',function () {
+        window.document.title='(Busy) '+window.document.title;
+        knw.set_message("Kernel busy");
+    });
 
-    NotificationWidget.prototype.style = function () {
-        this.element.addClass('notification ui-widget ui-widget-content ui-corner-all');
-        this.element.addClass('border-box-sizing');
-    };
+    $([IPython.events]).on('status_restarting.Kernel',function () {
+        IPython.save_widget.update_document_title();
+        knw.set_message("Restarting kernel");
+    });
 
+    $([IPython.events]).on('status_interrupting.Kernel',function () {
+        knw.set_message("Interrupting kernel");
+    });
 
-    NotificationWidget.prototype.bind_events = function () {
-        var that = this;
-        // Kernel events
-        $([IPython.events]).on('status_idle.Kernel',function () {
-            IPython.save_widget.update_document_title();
-            if (that.get_message() === 'Kernel busy') {
-                that.element.fadeOut(100, function () {
-                    that.element.html('');
-                });
-            };
-        });
-        $([IPython.events]).on('status_busy.Kernel',function () {
-            window.document.title='(Busy) '+window.document.title;
-            that.set_message("Kernel busy");
-        });
-        $([IPython.events]).on('status_restarting.Kernel',function () {
-            IPython.save_widget.update_document_title();
-            that.set_message("Restarting kernel",500);
-        });
-        $([IPython.events]).on('status_interrupting.Kernel',function () {
-            that.set_message("Interrupting kernel",500);
-        });
-        $([IPython.events]).on('status_dead.Kernel',function () {
-            var dialog = $('<div/>');
-            dialog.html('The kernel has died, would you like to restart it? If you do not restart the kernel, you will be able to save the notebook, but running code will not work until the notebook is reopened.');
-            $(document).append(dialog);
-            dialog.dialog({
-                resizable: false,
-                modal: true,
-                title: "Dead kernel",
-                buttons : {
-                    "Restart": function () {
-                        $([IPython.events]).trigger('status_restarting.Kernel');
-                        IPython.notebook.start_kernel();
-                        $(this).dialog('close');
-                    },
-                    "Continue running": function () {
-                        $(this).dialog('close');
-                    }
+    $([IPython.events]).on('status_dead.Kernel',function () {
+        var dialog = $('<div/>');
+        dialog.html('The kernel has died, would you like to restart it? If you do not restart the kernel, you will be able to save the notebook, but running code will not work until the notebook is reopened.');
+        $(document).append(dialog);
+        dialog.dialog({
+            resizable: false,
+            modal: true,
+            title: "Dead kernel",
+            buttons : {
+                "Restart": function () {
+                    $([IPython.events]).trigger('status_restarting.Kernel');
+                    IPython.notebook.start_kernel();
+                    $(this).dialog('close');
+                },
+                "Continue running": function () {
+                    $(this).dialog('close');
                 }
-            });
+            }
         });
-        // Notebook events
-        $([IPython.events]).on('notebook_loading.Notebook', function () {
-            that.set_message("Loading notebook",500);
-        });
-        $([IPython.events]).on('notebook_loaded.Notebook', function () {
-            that.set_message("Notebook loaded",500);
-        });
-        $([IPython.events]).on('notebook_saving.Notebook', function () {
-            that.set_message("Saving notebook",500);
-        });
-        $([IPython.events]).on('notebook_saved.Notebook', function () {
-            that.set_message("Notebook saved",2000);
-        });
-        $([IPython.events]).on('notebook_save_failed.Notebook', function () {
-            that.set_message("Notebook save failed",2000);
-        });
-    };
+    });
+
+    var div = $('<div/>').attr('id','notebook_notification');
+    $('#notification_area').append(div);
+    IPython.notebooknotificationwidget = new IPython.NotificationWidget('#notebook_notification');
+    var nnw = IPython.notebooknotificationwidget;
 
 
-    NotificationWidget.prototype.set_message = function (msg, timeout) {
-        var that = this;
-        this.element.html(msg);
-        this.element.fadeIn(100);
-        if (this.timeout !== null) {
-            clearTimeout(this.timeout);
-            this.timeout = null;
-        };
-        if (timeout !== undefined) {
-            this.timeout = setTimeout(function () {
-                that.element.fadeOut(100, function () {that.element.html('');});
-                that.timeout = null;
-            }, timeout)
-        };
-    };
-
-
-    NotificationWidget.prototype.get_message = function () {
-        return this.element.html();
-    };
-
-
-    IPython.NotificationWidget = NotificationWidget;
+    // Notebook events
+    $([IPython.events]).on('notebook_loading.Notebook', function () {
+        nnw.set_message("Loading notebook",500);
+    });
+    $([IPython.events]).on('notebook_loaded.Notebook', function () {
+        nnw.set_message("Notebook loaded",500);
+    });
+    $([IPython.events]).on('notebook_saving.Notebook', function () {
+        nnw.set_message("Saving notebook",500);
+    });
+    $([IPython.events]).on('notebook_saved.Notebook', function () {
+        nnw.set_message("Notebook saved",2000);
+    });
+    $([IPython.events]).on('notebook_save_failed.Notebook', function () {
+        nnw.set_message("Notebook save failed");
+    });
 
     return IPython;
 
