e340cf76c5062c88e985d066cefb9df1c47870d5
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2012-07-27 22:20:26 +0200

    mergesplit

--- docs/examples/notebooks/merge.py
+++ docs/examples/notebooks/merge.py
@@ -2,10 +2,10 @@
 from IPython.nbformat import current as nbf
 
 
-f = file('00_notebook_tour.ipynb')
+f = file('000-test-merge-split.ipynb')
 nb1 = nbf.read(f,'ipynb')
 
-f = file('00_notebook_tour-merge.ipynb')
+f = file('000-test-merge-split-step3.ipynb')
 nb2 = nbf.read(f,'ipynb')
 
 cells1 = nb1['worksheets'][0]['cells']
@@ -14,33 +14,32 @@ cells2 = nb2['worksheets'][0]['cells']
 known_uuid =  set([c['metadata']['uuid'] for c in cells1])
 
 
+def find_cell_ancester(cell,known_ids) :
+    if(cell['metadata']['uuid'] in known_ids) :
+        return [cell['metadata']['uuid']]
+
+    return find_familly(cell['metadata']['parents_id'], known_ids)
+
 def find_familly(dct,idset):
     a = [];
-    print('looking in... ',dct)
     if len(dct) == 0 :
-        print('return empty')
         return []
     else :
         for l in dct:
-            print('dctl', dct)
             k,v = l.items()[0]
-            print('looping on parents', k,v)
             if k in idset:
-                print('append k')
                 a.append(k)
             else :
-                print(' deepens')
                 a.extend(find_familly(v,idset))
     return a
 
-c = cells2[1]
+c = cells2[0]
 #dd = {
 #        'z' : {'a':{},'x':{}},
 #        'y' : {'t':{},'b':{}}
 #     }
 #ki = set(['a','b'])
 #find_familly(dd,ki)
-
 
 f = find_familly(c['metadata']['parents_id'], known_uuid)
 f
