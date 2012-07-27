c2f556153537cbfa9b5f911054f6689bc6a3b3d8
Bussonnier Matthias <bussonniermatthias@gmail.com>
2012-07-27 18:30:49 +0200

    add familly script

---
+++ docs/examples/notebooks/merge.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+from IPython.nbformat import current as nbf
+
+
+f = file('00_notebook_tour.ipynb')
+nb1 = nbf.read(f,'ipynb')
+
+f = file('00_notebook_tour-merge.ipynb')
+nb2 = nbf.read(f,'ipynb')
+
+cells1 = nb1['worksheets'][0]['cells']
+cells2 = nb2['worksheets'][0]['cells']
+
+known_uuid =  set([c['metadata']['uuid'] for c in cells1])
+
+
+def find_familly(dct,idset):
+    a = [];
+    print('looking in... ',dct)
+    if len(dct) == 0 :
+        print('return empty')
+        return []
+    else :
+        for k in dct.keys():
+            print('looping on keys', k)
+            if k in idset:
+                print('append k')
+                a.append(k)
+            else :
+                print(' deepens')
+                a.extend(find_familly(dct[k],idset))
+    return a
+
+c = cells2[1]
+dd = {
+        'z' : {'a':{},'x':{}},
+        'y' : {'t':{},'b':{}}
+     }
+ki = set(['a','b'])
+find_familly(dd,ki)
+
+
+find_familly(c['metadata']['parents_id'], known_uuid)
