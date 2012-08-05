5b8ed216342b12e817b7d34d2fb1f8e2fb5eb832
Brian Granger <ellisonbg@gmail.com>
2012-08-05 11:42:51 -0700

    Adding copyright to xtkdisplay.

--- IPython/lib/xtkdisplay.py
+++ IPython/lib/xtkdisplay.py
@@ -15,9 +15,25 @@ Authors:
 * Brian Granger
 """
 
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2012  The IPython Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
 import os
 
 from IPython.core.display import Javascript
+
+#-----------------------------------------------------------------------------
+# Classes
+#-----------------------------------------------------------------------------
+
 
 code = """
     container.show();
