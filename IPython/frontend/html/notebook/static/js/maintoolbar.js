23e6c236dc5cf527d3ec1b651f538bbbac021ca5
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2012-08-12 14:30:18 +0200

    space around :

--- IPython/frontend/html/notebook/static/js/maintoolbar.js
+++ IPython/frontend/html/notebook/static/js/maintoolbar.js
@@ -24,7 +24,7 @@ var IPython = (function (IPython) {
 
     MainToolBar.prototype = new IPython.ToolBar(); 
 
-    MainToolBar.prototype.add_drop_down_list = function() {
+    MainToolBar.prototype.add_drop_down_list = function () {
         var select = $(this.selector)
             .append($('<select/>')
                 .attr('id','cell_type')
@@ -43,39 +43,39 @@ var IPython = (function (IPython) {
                 );
     };
 
-    MainToolBar.prototype.construct = function() {
+    MainToolBar.prototype.construct = function () {
         this.add_buttons_group([
                 {
-                    id:'save_b',
-                    label:'Save',
-                    icon:'ui-icon-disk',
-                    callback:function(){
+                    id : 'save_b',
+                    label : 'Save',
+                    icon : 'ui-icon-disk',
+                    callback : function () {
                         IPython.notebook.save_notebook();
                         }
                 }
             ]);
         this.add_buttons_group([
                 {
-                    id:'cut_b',
-                    label:'Cut Cell',
-                    icon:'ui-icon-scissors',
-                    callback:function(){
+                    id : 'cut_b',
+                    label : 'Cut Cell',
+                    icon : 'ui-icon-scissors',
+                    callback : function () {
                         IPython.notebook.cut_cell();
                         }
                 },
                 {
-                    id:'copy_b',
-                    label:'Copy Cell',
-                    icon:'ui-icon-copy',
-                    callback:function(){
+                    id : 'copy_b',
+                    label : 'Copy Cell',
+                    icon : 'ui-icon-copy',
+                    callback : function () {
                         IPython.notebook.copy_cell();
                         }
                 },
                 {
-                    id:'paste_b',
-                    label:'Paste Cell',
-                    icon:'ui-icon-clipboard',
-                    callback:function(){
+                    id : 'paste_b',
+                    label : 'Paste Cell',
+                    icon : 'ui-icon-clipboard',
+                    callback : function () {
                         IPython.notebook.paste_cell();
                         }
                 }
@@ -83,18 +83,18 @@ var IPython = (function (IPython) {
 
         this.add_buttons_group([
                 {
-                    id:'move_up_b',
-                    label:'Move Cell Up',
-                    icon:'ui-icon-arrowthick-1-n',
-                    callback:function(){
+                    id : 'move_up_b',
+                    label : 'Move Cell Up',
+                    icon : 'ui-icon-arrowthick-1-n',
+                    callback : function () {
                         IPython.notebook.move_cell_up();
                         }
                 },
                 {
-                    id:'move_down_b',
-                    label:'Move Cell Down',
-                    icon:'ui-icon-arrowthick-1-s',
-                    callback:function(){
+                    id : 'move_down_b',
+                    label : 'Move Cell Down',
+                    icon : 'ui-icon-arrowthick-1-s',
+                    callback : function () {
                         IPython.notebook.move_cell_down();
                         }
                 }
@@ -102,18 +102,18 @@ var IPython = (function (IPython) {
         
         this.add_buttons_group([
                 {
-                    id:'insert_above_b',
-                    label:'Insert Cell Above',
-                    icon:'ui-icon-arrowthickstop-1-n',
-                    callback:function(){
+                    id : 'insert_above_b',
+                    label : 'Insert Cell Above',
+                    icon : 'ui-icon-arrowthickstop-1-n',
+                    callback : function () {
                         IPython.notebook.insert_cell_above('code');
                         }
                 },
                 {
-                    id:'insert_below_b',
-                    label:'Insert Cell Below',
-                    icon:'ui-icon-arrowthickstop-1-s',
-                    callback:function(){
+                    id : 'insert_below_b',
+                    label : 'Insert Cell Below',
+                    icon : 'ui-icon-arrowthickstop-1-s',
+                    callback : function () {
                         IPython.notebook.insert_cell_below('code');
                         }
                 }
@@ -121,18 +121,18 @@ var IPython = (function (IPython) {
 
         this.add_buttons_group([
                 {
-                    id:'run_b',
-                    label:'Run Cell',
-                    icon:'ui-icon-play',
-                    callback:function(){
+                    id : 'run_b',
+                    label : 'Run Cell',
+                    icon : 'ui-icon-play',
+                    callback : function () {
                     IPython.notebook.execute_selected_cell();
                         }
                 },
                 {
-                    id:'interrupt_b',
-                    label:'Interrupt',
-                    icon:'ui-icon-stop',
-                    callback:function(){
+                    id : 'interrupt_b',
+                    label : 'Interrupt',
+                    icon : 'ui-icon-stop',
+                    callback : function () {
                         IPython.notebook.kernel.interrupt();
                         }
                 }
