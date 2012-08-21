57462a94a99b9fed7d199b3a3a3b9edff3a1bea9
Takafumi Arakaki <aka.tkf@gmail.com>
2012-08-21 20:22:27 +0200

    Document tests/writetofile.py bit more

--- IPython/frontend/terminal/console/tests/writetofile.py
+++ IPython/frontend/terminal/console/tests/writetofile.py
@@ -7,6 +7,14 @@
 
 """
 Copy data from input file to output file for testing.
+
+Command line usage:
+
+    python writetofile.py INPUT OUTPUT
+
+Binary data from INPUT file is copied to OUTPUT file.
+If INPUT is '-', stdin is used.
+
 """
 
 import sys
