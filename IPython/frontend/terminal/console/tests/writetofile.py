61e38b8d8a4d29e03e8895729f8f330fdf6335e0
Takafumi Arakaki <aka.tkf@gmail.com>
2012-08-21 20:33:34 +0200

    Protect tests/writetofile.py from test discovery

--- IPython/frontend/terminal/console/tests/writetofile.py
+++ IPython/frontend/terminal/console/tests/writetofile.py
@@ -17,16 +17,17 @@ If INPUT is '-', stdin is used.
 
 """
 
-import sys
-from IPython.utils.py3compat import PY3
-(inpath, outpath) = sys.argv[1:]
+if __name__ == '__main__':
+    import sys
+    from IPython.utils.py3compat import PY3
+    (inpath, outpath) = sys.argv[1:]
 
-if inpath == '-':
-    if PY3:
-        infile = sys.stdin.buffer
+    if inpath == '-':
+        if PY3:
+            infile = sys.stdin.buffer
+        else:
+            infile = sys.stdin
     else:
-        infile = sys.stdin
-else:
-    infile = open(inpath, 'rb')
+        infile = open(inpath, 'rb')
 
-open(outpath, 'w+b').write(infile.read())
+    open(outpath, 'w+b').write(infile.read())
