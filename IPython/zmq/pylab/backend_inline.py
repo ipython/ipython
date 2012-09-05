f152a1a2fd2f575ee0105944e94fff2279621ebd
Jens H. Nielsen <jenshnielsen@gmail.com>
2012-09-05 09:18:20 +0200

    Provide default figure canvas for inline backend needed due to matplotlib change.

--- IPython/zmq/pylab/backend_inline.py
+++ IPython/zmq/pylab/backend_inline.py
@@ -10,7 +10,7 @@ import sys
 
 # Third-party imports
 import matplotlib
-from matplotlib.backends.backend_agg import new_figure_manager
+from matplotlib.backends.backend_agg import new_figure_manager, FigureCanvasAgg
 from matplotlib._pylab_helpers import Gcf
 
 # Local imports.
@@ -222,3 +222,4 @@ def send_figure(fig):
         {mime : data}
     )
 
+FigureCanvas = FigureCanvasAgg
