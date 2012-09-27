1080b1bf1c2c7c53ab017f3a3f89d58d09b7ff5f
Bradley M. Froehle <brad.froehle@gmail.com>
2012-09-27 09:41:17 -0700

    Py3k: Octal (0777 -> 0o777)

--- IPython/external/path/_path.py
+++ IPython/external/path/_path.py
@@ -860,10 +860,10 @@ class path(unicode):
 
     # --- Create/delete operations on directories
 
-    def mkdir(self, mode=0777):
+    def mkdir(self, mode=0o777):
         os.mkdir(self, mode)
 
-    def makedirs(self, mode=0777):
+    def makedirs(self, mode=0o777):
         os.makedirs(self, mode)
 
     def rmdir(self):
@@ -879,7 +879,7 @@ class path(unicode):
         """ Set the access/modified times of this file to the current time.
         Create the file if it does not exist.
         """
-        fd = os.open(self, os.O_WRONLY | os.O_CREAT, 0666)
+        fd = os.open(self, os.O_WRONLY | os.O_CREAT, 0o666)
         os.close(fd)
         os.utime(self, None)
 
