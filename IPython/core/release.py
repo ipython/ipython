acc1ee13d9c65b3928061bea0b1953801452a6f9
MinRK <benjaminrk@gmail.com>
2012-10-11 15:42:39 -0700

    mark 0.13.1.rc1

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -22,8 +22,8 @@ name = 'ipython'
 _version_major = 0
 _version_minor = 13
 _version_micro = 1  # use '' for first of series, number for 1 and above
-_version_extra = 'dev'
-#_version_extra = 'rc1'
+# _version_extra = 'dev'
+_version_extra = 'rc1'
 # _version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
