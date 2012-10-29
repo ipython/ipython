1a5c1cdf667b5256e1f42ec54821ac5aea23611e
Danny Staple <danny@orionrobots.co.uk>
2012-10-29 13:15:33 +0000

    Adding time offsets to the video
    This is based on the suggestions here - http://stackoverflow.com/questions/13119791/can-the-ipython-notebook-youtubevideo-class-play-from-time-offset.

--- IPython/lib/display.py
+++ IPython/lib/display.py
@@ -2,6 +2,7 @@
 
 Authors : MinRK
 """
+from datetime import timedelta, datetime
 
 class YouTubeVideo(object):
     """Class for embedding a YouTube Video in an IPython session, based on its video id.
@@ -14,12 +15,18 @@ class YouTubeVideo(object):
 
     vid = YouTubeVideo("foo")
     display(vid)
+    
+    To start from a particular time offset:
+    
+    vid = YouTubeVideo("abc", start=timedelta(hours=1, minutes=47, seconds=3))
+    display(vid)
     """
 
-    def __init__(self, id, width=400, height=300):
+    def __init__(self, id, width=400, height=300, start=timedelta()):
         self.id = id
         self.width = width
         self.height = height
+        self.start = start.total_seconds()
 
     def _repr_html_(self):
         """return YouTube embed iframe for this video id"""
@@ -27,9 +34,9 @@ class YouTubeVideo(object):
             <iframe
                 width="%i"
                 height="%i"
-                src="http://www.youtube.com/embed/%s"
+                src="http://www.youtube.com/embed/%s?start=%i"
                 frameborder="0"
                 allowfullscreen
             ></iframe>
-        """%(self.width, self.height, self.id)
+        """%(self.width, self.height, self.id, self.start)
 
