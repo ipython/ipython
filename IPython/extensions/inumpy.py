f90b3d9de898f05e1b0a8eaacaf37353eea771e1
Piti Ongmongkolkul <piti118@gmail.com>
2012-11-05 16:03:13 -0800

    shorter pydoc and remove loaded state tracking

--- IPython/extensions/inumpy.py
+++ IPython/extensions/inumpy.py
@@ -4,20 +4,13 @@ Features:
 """
 
 def install_ipython_completers():
-    """Register the recarray type with IPython's tab completion machinery, so
-    that it knows about accessing column names as attributes."""
+    """Register recarray with IPython tab completion machinery
+    """
     from IPython.utils.generics import complete_object
     from numpy import recarray
     @complete_object.when_type(recarray)
     def complete_recarray_colname(obj, prev_completions):
         return prev_completions + list(obj.dtype.names)
 
-
-_loaded = False
-
-
 def load_ipython_extension(ip):
-	global _loaded
-	if not _loaded:
-		install_ipython_completers()
-		_loaded = True
+	install_ipython_completers()
