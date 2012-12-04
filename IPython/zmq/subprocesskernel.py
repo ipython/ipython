bb0b397ad5fd7cc49fe39e28bc52a75005eac8cb
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2012-12-04 10:09:03 +0100

    working generic subprocess

--- IPython/zmq/subprocesskernel.py
+++ IPython/zmq/subprocesskernel.py
@@ -1,26 +1,43 @@
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2012  The IPython Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+
 from subprocess import Popen
+from os.path import expanduser as expanduser
 
 from IPython.zmq.kernelmanager import KernelManager
-from IPython.utils.traitlets import Unicode
+from IPython.utils.traitlets import List
+from IPython.config.configurable import Configurable
 
 
-class SubprocessKernelManager(KernelManager):
+class SubprocessKernelManager(KernelManager, Configurable):
 
-    kernel_launch_command = Unicode(
-        'echo "this is not configurated"',
+    kernel_launch_program = List(
+        [],
         config=True,
-        help="""the command to launch a foreing language kernel, use %(cfile) to have 
+        help="""the command to launch a foreing language kernel, use '{connexion_file_name}' to have
         the full path of the connexion file.
         """
     )
 
     def start_kernel(self, **kw):
-        kw['launcher'] = self.launch_ruby_kernel
+        kw['launcher'] = self.launch_subprocess_kernel
         return KernelManager.start_kernel(self, **kw)
 
-    def launch_ruby_kernel(self, fname='cf.json', **kw):
-        cmd = ['/usr/local/Cellar/ruby/1.9.3-p286/bin/ruby',
-                '/Users/matthiasbussonnier/iruby/lib/kernel.rb',
-                fname]
-                #~/.ipython/profile_default/security/kernel-10234.json
+    def launch_subprocess_kernel(self, fname='cf.json', **kw):
+        if not self.kernel_launch_program :
+            raise ValueError("""kernel_launch program should be defined
+            in config with the following form :
+
+            c.SubprocessKernelManager.kernel_launch_program=['interpreter','program','{connexion_file_name}']
+            """)
+        cmd = [ expanduser(_.format(connexion_file_name=fname)) for _ in self.kernel_launch_program]
         return Popen(cmd)
