f146a48dee30eb5c6a68cfef53c91736802f4822
damianavila <damianavila82@yahoo.com.ar>
2012-12-04 20:29:06 -0300

    New config file revealmathjax.js to configure mathjax in the reveal slides. The previous attemp to share initmathjax.js contained bugs

--- converters/reveal.py
+++ converters/reveal.py
@@ -116,8 +116,8 @@ class ConverterReveal(ConverterMarkdown):
                 { src: 'reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
                 { src: 'reveal/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
                 { src: 'reveal/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-                { src: 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS_HTML', async: true },                                     
-                { src: 'js/initmathjax.js?align=center', async: true}
+                { src: 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS_HTML', async: true },  
+                { src: 'js/revealmathjax.js', async: true}                                   
                 ]
                 });
     </script>
