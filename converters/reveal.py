82374eb3957d5ca26e0aba56caa97bc8e247a3cf
damianavila <damianavila82@yahoo.com.ar>
2012-12-12 22:04:18 -0300

    Added some comments to specific pieces of code

--- converters/reveal.py
+++ converters/reveal.py
@@ -15,24 +15,25 @@ class ConverterReveal(ConverterMarkdown):
 
     def __init__(self, infile, highlight_source=False, show_prompts=True,
                  inline_prompt=True):
-        super(ConverterMarkdown, self).__init__(infile)
+        super(ConverterReveal, self).__init__(infile)
         self.highlight_source = highlight_source
         self.show_prompts = show_prompts
         self.inline_prompt = inline_prompt
 
     def switch_meta(self, m_list):
-        "switch metadata delimiters order to build properly the slides"
+        "sort order m_list to [new_section, new_subsection, new_fragment]"
         if len(m_list) > 1:
+        # do not sort when m_list = [new_subsection, new_fragment]
             if not (len(m_list) == 2 and m_list[1] == [u'new_fragment = True']):
                 m_list[0], m_list[1] = m_list[1], m_list[0]
         return m_list
 
     def meta2str(self, meta):
-        "transform metadata dict to a string containing delimiters for slides"
+        "transform metadata dict (containing slides delimiters) to string "
         try:
             meta_tuple = meta[u'slideshow'].items()
         except KeyError as e:
-            meta_tuple = ()
+            meta_tuple = ()  # if there is not slideshow metadata
         meta_list = [[x + ' = ' + unicode(y)] for x, y in meta_tuple]
         meta_list = self.switch_meta(meta_list)
         return u'\n'.join(list(itertools.chain(*meta_list)))
@@ -53,11 +54,7 @@ class ConverterReveal(ConverterMarkdown):
 
     def convert(self, cell_separator='\n'):
         """
-        Generic method to converts notebook to a string representation.
-
-        This is accomplished by dispatching on the cell_type, so subclasses of
-        Convereter class do not need to re-implement this method, but just
-        need implementation for the methods that will be dispatched.
+        Specific method to converts notebook to a string representation.
 
         Parameters
         ----------
@@ -68,6 +65,7 @@ class ConverterReveal(ConverterMarkdown):
         -------
         out : string
         """
+
         lines = []
         lines.extend(self.optional_header())
         begin = ['<div class="reveal"><div class="slides">']
@@ -88,12 +86,14 @@ class ConverterReveal(ConverterMarkdown):
         text = [x for x in text if not x in delim_false]
         left = '<section data-markdown><script type="text/template">'
         right = '</script></section>'
+        # build lists representing each slide delimited by new_section
         slides = [list(x[1]) for x in itertools.groupby(text,
             lambda x: x == u'new_section = True') if not x[0]]
         for slide in slides:
-            slide.insert(0, u'')
+            slide.insert(0, u'')  # for proper interline in html file
             slide.insert(0, left)
             slide.append(right)
+            # build each vertical slide delimited by new_subsection
             if slide[2] == u'new_subsection = True':
                 slide.pop(2)
                 slide.insert(0, '<section>')
@@ -101,22 +101,24 @@ class ConverterReveal(ConverterMarkdown):
                 for i, j in enumerate(slide):
                     if j == u'new_subsection = True':
                         slide[i] = right + left
-                        slide.insert(i + 1, u'')
+                        slide.insert(i + 1, u'')  # for proper interline
+            #defensive: if user do not begin new_subsection with a new_section
             elif slide[4] == u'new_subsection = True':
                 slide[4] = right
-                slide.insert(5, u'')
+                slide.insert(5, u'')  # for proper interline
                 slide.insert(5, left)
                 slide.insert(5, '<section>')
                 slide.append('</section>')
                 for i, j in enumerate(slide):
                     if j == u'new_subsection = True':
                         slide[i] = right + left
-                        slide.insert(i + 1, u'')
+                        slide.insert(i + 1, u'')  # for proper interline
+            # build each fragment delimited by new_fragment
             for i, j in enumerate(slide):
                 if j == u'new_fragment = True':
                     slide[i] = '<p class="fragment">'
                     slide[i + 2] = '</p>'
-                    slide.insert(i + 3, u'')
+                    slide.insert(i + 3, u'')  # for proper interline
         return list(itertools.chain(*slides))
 
     def save(self, outfile=None, encoding=None):
@@ -136,7 +138,7 @@ class ConverterReveal(ConverterMarkdown):
             'reveal_base.html')
         with io.open(reveal_template, 'r', encoding='utf-8') as f:
             template = f.readlines()
-        template = [s.strip() for s in template]  # cosmetic one
+        template = [s.strip() for s in template]
         return template
 
     def template_split(self):
