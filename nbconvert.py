1143ce9d39c197448dcbd2da1263dbafd3d6c198
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2012-12-16 17:43:33 +0100

    working config

--- nbconvert.py
+++ nbconvert.py
@@ -76,16 +76,49 @@ def main(infile, format='rst', preamble=None, exclude=None):
         self.classes.insert(0,ConverterLaTeX)
         self.classes.insert(0,ConverterPy)
 
+    converter = ConverterClass(infile, highlight_source=highlight_source, exclude=exclude)
+    converter.render()
+
+
+    aliases = {
+            'format':'NbconvertApp.fmt',
+            'highlight':'Converter.highlight_source',
+            'preamble':'Converter.preamble',
+            'infile' : 'NbconvertApp.infile'
+            }
+
+    def __init__(self, **kwargs):
+        super(NbconvertApp, self).__init__(**kwargs)
+        # ensure those are registerd
+        self.classes.insert(0,Converter)
+        self.classes.insert(0,ConverterRST)
+        self.classes.insert(0,ConverterMarkdown)
+        self.classes.insert(0,ConverterBloggerHTML)
+        self.classes.insert(0,ConverterLaTeX)
+        self.classes.insert(0,ConverterPy)
+
     def initialize(self, argv=None):
+        # don't hook up crash handler before parsing command-line
         self.parse_command_line(argv)
         cl_config = self.config
         self.update_config(cl_config)
+        #self.init_crash_handler()
+        #self.foo = Cnf(config=self.config)
+        #if self.subapp is not None:
+            # stop here if subapp is taking over
+            #return
+        #cl_config = self.config
+        #self.init_profile_dir()
+        #self.init_config_files()
+        #self.load_config_file()
+        # enforce cl-opts override configfile opts:
+        #self.update_config(cl_config)
+
 
     def run(self):
         """Convert a notebook in one step"""
         ConverterClass = converters[self.fmt]
-        infile = (self.extra_args or [None])[0]
-        converter = ConverterClass(infile=infile,  config=self.config)
+        converter = ConverterClass(infile=self.extra_args[0],  config=self.config)
         converter.render()
 
 def main():
