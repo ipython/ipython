ec3b152dfa1825411bba82b7c30052540d544320
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-01-13 12:28:48 +0100

    try to play with data display priority

--- runme.py
+++ runme.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 from __future__ import print_function
 import sys
 import io
 from converters.template import *
 C = ConverterTemplate(tplfile=sys.argv[1])
-C.read('tests/ipynbref/IntroNumPy.orig.ipynb')
+C.read(sys.argv[2])
 
 print(C.convert().encode('utf-8'))
