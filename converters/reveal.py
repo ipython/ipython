3dcc585199984ff518ed0eb2a57f21677ae13d7b
damianavila <damianavila82@yahoo.com.ar>
2013-01-22 10:07:01 -0300

    Skipped cells are now hided instead of deleted.

--- converters/reveal.py
+++ converters/reveal.py
@@ -44,6 +44,96 @@ class ConverterReveal(ConverterMarkdown):
         lines.extend(self.optional_footer())
         return u'\n'.join(lines)
 
+    def clean_text(self, cell_separator='\n'):
+        "clean and reorganize the text list to be slided"
+        text = self.main_body(cell_separator)
+        self.delim = [u'slide_type = untouched',
+                      u'slide_type = -',
+                      u'slide_type = slide',
+                      u'slide_type = subslide',
+                      u'slide_type = fragment',
+                      u'slide_type = notes',
+                      u'slide_type = skip']  # keep this one the last
+        text_cell_render = \
+            u'<div class="text_cell_render border-box-sizing rendered_html">'
+        for i, j in enumerate(text):
+            if j in self.delim and text[i - 1] == text_cell_render:
+                if j == self.delim[0]:
+                    text[i - 1] = self.delim[0]
+                elif j == self.delim[1]:
+                    text[i - 1] = self.delim[1]
+                elif j == self.delim[2]:
+                    text[i - 1] = self.delim[2]
+                elif j == self.delim[3]:
+                    text[i - 1] = self.delim[3]
+                elif j == self.delim[4]:
+                    text[i - 1] = self.delim[4]
+                elif j == self.delim[5]:
+                    text[i - 1] = self.delim[5]
+                else:
+                    text[i - 1] = self.delim[6]
+                text[i] = text_cell_render
+        text.append(u'slide_type = untouched')  # to end search of skipped
+        return text
+
+    def build_slides(self):
+        "build the slides structure from text list and delimiters"
+        text = self.clean_text()
+        left = '<section>'
+        right = '</section>'
+        notes_start = '<aside class="notes">'
+        notes_end = '</aside>'
+        set_delim_skip = self.delim[:6]  # to skip adjacent skkiped cells
+        #elimination of skipped cells
+        for i, j in enumerate(text):
+            if j == u'slide_type = skip':
+                text.pop(i)
+                while not text[i] in set_delim_skip:
+                    text.pop(i)
+        #encapsulation of notes cells
+        for i, j in enumerate(text):
+            if j == u'slide_type = notes':
+                text.pop(i)
+                temp_list = []
+                while not text[i] in set_delim_skip:
+                    temp_list.append(text.pop(i))
+                else:
+                    temp_list.insert(0, notes_start)
+                    temp_list.append(notes_end)
+                    text[i:i] = temp_list
+        # elimination of none names
+        for i, j in enumerate(text):
+            if j in [u'slide_type = untouched', u'slide_type = -']:
+                text.pop(i)
+        #generation of slides as a list of list
+        slides = [list(x[1]) for x in itertools.groupby(text,
+            lambda x: x == u'slide_type = slide') if not x[0]]
+        for slide in slides:
+            slide.insert(0, left)
+            slide.append(right)
+            # encapsulation of each fragment
+            for i, j in enumerate(slide):
+                if j == u'slide_type = fragment':
+                    slide.pop(i)
+                    slide[i] = slide[i][:4] + \
+                        ' class="fragment"' + slide[i][4:]
+            # encapsulation of each nested slide
+            if u'slide_type = subslide' in slide:
+                slide.insert(0, left)
+                slide.append(right)
+            for i, j in enumerate(slide):
+                if j == u'slide_type = subslide':
+                    slide[i] = right + left
+        return list(itertools.chain(*slides))
+
+    def render(self):
+        "read, convert, and save self.infile"
+        if not hasattr(self, 'nb'):
+            self.read()
+        self.output = self.convert()
+        assert(type(self.output) == unicode)
+        return self.save()
+
     def save(self, outfile=None, encoding=None):
         "read and parse notebook into self.nb"
         if outfile is None:
