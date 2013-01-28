37e9b32650403277c3393abe8d078e04fbe4fb4f
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-01-28 09:57:03 +0100

    run some more test
    should yield them for more grained result

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -3,6 +3,9 @@ import nose.tools as nt
 import os
 from nose.tools import nottest
 from converters.template import ConverterTemplate
+from  IPython.config.loader import PyFileConfigLoader 
+
+
 
 
 def test_evens():
@@ -10,9 +13,15 @@ def test_evens():
             'tests/ipynbref/IntroNumPy.orig'
             ]
 
-    # null template should return empty
-    C = ConverterTemplate()
-    C.read('tests/ipynbref/IntroNumPy.orig.ipynb')
-    result,_ =  C.convert()
-    nt.assert_equal(result,'')
+    test_profiles = [prof for prof in os.listdir('profile/test/') if prof.endswith('.py')]
+
+    ### null template should return empty
+    for profile in test_profiles:
+        loader = PyFileConfigLoader(profile, path=[os.path.join(os.getcwdu(),'profile/test')])
+        config = loader.load_config()
+        C = ConverterTemplate(config=config)
+        C.read('tests/ipynbref/IntroNumPy.orig.ipynb')
+        result,_ =  C.convert()
+        nt.assert_equal(result,'')
+    ### end null test
 
