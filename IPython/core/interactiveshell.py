c50bd7f0d8a32b52b1829fab28d4adf02c22e4ce
Robert Marchman <bo.marchman@gmail.com>
2013-02-02 15:28:13 +1300

    replace warning with UsageError

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -2125,7 +2125,7 @@ class InteractiveShell(SingletonConfigurable):
                                     'did you mean that instead?)' % magic_name )
             error(etpl % (magic_name, extra))
         elif cell == '':
-            warn('%%%%%s line but no cell' % magic_name)
+            raise UsageError('%%%%%s has line but no cell' % magic_name)
         else:
             # Note: this is the distance in the stack to the user's frame.
             # This will need to be updated if the internal calling logic gets
