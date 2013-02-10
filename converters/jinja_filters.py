f76aadd1c385022d32cb383ee8a0bf436052c99a
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-02-10 13:27:59 +0100

    share some global config state

--- converters/jinja_filters.py
+++ converters/jinja_filters.py
@@ -17,30 +17,14 @@ from .utils import remove_ansi
 from .utils import highlight, ansi2html
 from .utils import markdown2latex
 
+from converters.config import GlobalConfigurable
+
 from IPython.config.configurable import Configurable
 from IPython.utils.traitlets import List
 
 #-----------------------------------------------------------------------------
 # Class declarations
 #-----------------------------------------------------------------------------
-
-class GlobalConfigurable(Configurable):
-    """Global configurable class for shared config
-
-    Usefull for display data priority that might be use by many trasnformers
-    """
-    
-    display_data_priority = List(['html', 'pdf', 'svg', 'latex', 'png', 'jpg', 'jpeg' , 'text'],
-            config=True,
-              help= """
-                    An ordered list of prefered output type, the first
-                    encounterd will usually be used when converting discarding
-                    the others.
-                    """
-            )
-
-    def __init__(self, config=None, **kw):
-        super(GlobalConfigurable, self).__init__( config=config, **kw)
 
 class ConfigurableFilter(GlobalConfigurable):
     """Configurable Jinja Filter"""
