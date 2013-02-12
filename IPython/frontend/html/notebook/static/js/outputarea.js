f43a56d1931c1ec3ef3947ddea00a9d64e533925
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-02-12 21:32:17 +0100

    fix bad logic

--- IPython/frontend/html/notebook/static/js/outputarea.js
+++ IPython/frontend/html/notebook/static/js/outputarea.js
@@ -331,10 +331,10 @@ var IPython = (function (IPython) {
         for(var type_i in OutputArea.display_order){
             var type = OutputArea.display_order[type_i];
             if(json[type] != undefined ){
-                if(type != 'javascript' && dynamic){
-                    this['append_'+type](json[type],element)
-                } else {
+                if(type == 'javascript' && dynamic){
                     this.append_javascript(json.javascript, element, dynamic);
+                } else {
+                    this['append_'+type](json[type],element)
                 }
                 return
             }
