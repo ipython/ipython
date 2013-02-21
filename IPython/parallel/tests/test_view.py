9a5f57ab741ebcc6e6d1a77120e89878822e70cc
MinRK <benjaminrk@gmail.com>
2013-02-21 09:57:09 -0800

    fix count when testing composite error output

--- IPython/parallel/tests/test_view.py
+++ IPython/parallel/tests/test_view.py
@@ -615,12 +615,15 @@ class TestView(ClusterTestCase, ParametricTestCase):
         ar = view.execute("1/0")
         ip = get_ipython()
         ip.user_ns['ar'] = ar
+
         with capture_output() as io:
             ip.run_cell("ar.get(2)")
         
-        self.assertEqual(io.stdout.count('ZeroDivisionError'), len(view) * 2, io.stdout)
-        self.assertEqual(io.stdout.count('by zero'), len(view), io.stdout)
-        self.assertEqual(io.stdout.count(':execute'), len(view), io.stdout)
+        count = min(error.CompositeError.tb_limit, len(view))
+        
+        self.assertEqual(io.stdout.count('ZeroDivisionError'), count * 2, io.stdout)
+        self.assertEqual(io.stdout.count('by zero'), count, io.stdout)
+        self.assertEqual(io.stdout.count(':execute'), count, io.stdout)
     
     def test_compositeerror_truncate(self):
         """Truncate CompositeErrors with many exceptions"""
