a66b28a79e16bea8622b4d17dbe9cb6ed234a367
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-03-02 21:54:58 +0100

    fix when cell do not have metadata

--- converters/transformers.py
+++ converters/transformers.py
@@ -201,6 +201,8 @@ class RevealHelpTransformer(ConfigurableTransformers):
     def __call__(self, nb, other):
         for worksheet in nb.worksheets :
             for i, cell in enumerate(worksheet.cells):
+                if not cell.get('metadata', None):
+                    break
                 cell.metadata.slide_type = cell.metadata.get('slideshow', {}).get('slide_type', None)
                 if cell.metadata.slide_type is None:
                     cell.metadata.slide_type = '-'
