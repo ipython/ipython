43e0fc223cb6f0e7a8f8ccfe6650c7974d239df3
MinRK <benjaminrk@gmail.com>
2013-03-04 15:03:38 -0800

    Backport PR #2662: qtconsole: wrap argument list in tooltip to match width of text body
    previously, a function with a long argument list would produce
    a very wide tooltip, hurting readability.
    
    Since the width of the docstring body is chosen by the developer
    whereas the formatting of the argument list is currently not, it's
    reasonable to wrap the argument list width to the maximum of
    a) 80 characters, or b) maximum length of a line in the docstring body.
    
    This improves readability, without unduly affecting the appearence
    of the docstring body itself.
    
    closes #2661
    
    I'm happy to add any additional tests or make any changes required to get this merged.

--- IPython/frontend/qt/console/call_tip_widget.py
+++ IPython/frontend/qt/console/call_tip_widget.py
@@ -137,7 +137,7 @@ class CallTipWidget(QtGui.QLabel):
 
         if call_line:
             doc = '\n\n'.join([call_line, doc])
-        return self.show_tip(doc)
+        return self.show_tip(self._format_tooltip(doc))
 
     def show_tip(self, tip):
         """ Attempts to show the specified tip at the current cursor location.
@@ -247,6 +247,17 @@ class CallTipWidget(QtGui.QLabel):
             # around when the tooltip is created.
             QtGui.qApp.topLevelAt(QtGui.QCursor.pos()) != self):
             self._hide_timer.start(300, self)
+
+    def _format_tooltip(self,doc):
+        import textwrap
+
+        # make sure a long argument list does not make
+        # the first row overflow the width of the actual tip body
+        rows = doc.split("\n")
+        max_text_width = max(80, max([len(x) for x in rows[1:]]))
+        rows= textwrap.wrap(rows[0],max_text_width) + rows[1:]
+        doc = "\n".join(rows)
+        return doc
 
     #------ Signal handlers ----------------------------------------------------
 
