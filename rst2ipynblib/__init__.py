ff4dd7b614cd521c782eb432edf1e606ae54963b
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-03-11 20:43:00 +0100

    fix docutils math import error on newer version

--- rst2ipynblib/__init__.py
+++ rst2ipynblib/__init__.py
@@ -15,9 +15,14 @@ import urllib
 import docutils
 from docutils import frontend, nodes, utils, writers, languages, io
 from docutils.transforms import writer_aux
-from docutils.math import unichar2tex, pick_math_environment
-from docutils.math.latex2mathml import parse_latex_math
-from docutils.math.math2html import math2html
+try:
+    from docutils.math import unichar2tex, pick_math_environment
+    from docutils.math.latex2mathml import parse_latex_math
+    from docutils.math.math2html import math2html
+except ImportError:
+    from docutils.utils.math import unichar2tex, pick_math_environment
+    from docutils.utils.math.latex2mathml import parse_latex_math
+    from docutils.utils.math.math2html import math2html
 
 try:
     from docutils.utils.error_reporting import SafeString
