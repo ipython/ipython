b47acf0960278668b6382656ecf4e24856d9d8fd
Julian Taylor <jtaylor.debian@googlemail.com>
2013-03-11 19:13:35 +0100

    test double pyximport

--- IPython/extensions/tests/test_cythonmagic.py
+++ IPython/extensions/tests/test_cythonmagic.py
@@ -34,6 +34,9 @@ def test_cython_pyximport():
     ip.run_cell_magic('cython_pyximport', module_name, code)
     ip.ex('g = f(10)')
     nt.assert_equal(ip.user_ns['g'], 20.0)
+    ip.run_cell_magic('cython_pyximport', module_name, code)
+    ip.ex('h = f(-10)')
+    nt.assert_equal(ip.user_ns['h'], -20.0)
     try:
         os.remove(module_name+'.pyx')
     except OSError:
