4a8ebe8e8f9e145b5365080b37dd637dafbbdfe0
MinRK <benjaminrk@gmail.com>
2013-03-31 12:01:19 -0700

    add in-process kernel examples

---
+++ examples/inprocess/embedded_terminal.py
@@ -0,0 +1,30 @@
+import os
+
+from IPython.kernel.inprocess import InProcessKernelManager
+from IPython.frontend.terminal.console.interactiveshell import ZMQTerminalInteractiveShell
+
+
+def print_process_id():
+    print 'Process ID is:', os.getpid()
+
+
+def main():
+    print_process_id()
+
+    # Create an in-process kernel
+    # >>> print_process_id()
+    # will print the same process ID as the main process
+    kernel_manager = InProcessKernelManager()
+    kernel_manager.start_kernel()
+    kernel = kernel_manager.kernel
+    kernel.gui = 'qt4'
+    kernel.shell.push({'foo': 43, 'print_process_id': print_process_id})
+    client = kernel_manager.client()
+    client.start_channels()
+
+    shell = ZMQTerminalInteractiveShell(kernel_manager=kernel_manager, kernel_client=client)
+    shell.mainloop()
+
+
+if __name__ == '__main__':
+    main()
