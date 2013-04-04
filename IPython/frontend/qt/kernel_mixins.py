2a8432200343a42627031eeaa5c59940fb8ba7b6
MinRK <benjaminrk@gmail.com>
2013-04-04 12:50:02 -0700

    add QtKernelManager with restarter

--- IPython/frontend/qt/kernel_mixins.py
+++ IPython/frontend/qt/kernel_mixins.py
@@ -168,6 +168,21 @@ class QtHBChannelMixin(ChannelQObject):
         self.kernel_died.emit(since_last_heartbeat)
 
 
+class QtKernelRestarterMixin(HasTraits, SuperQObject):
+
+    __metaclass__ = MetaQObjectHasTraits
+    _timer = None
+
+
+class QtKernelManagerMixin(HasTraits, SuperQObject):
+    """ A KernelClient that provides signals and slots.
+    """
+
+    __metaclass__ = MetaQObjectHasTraits
+
+    kernel_restarted = QtCore.Signal()
+
+
 class QtKernelClientMixin(HasTraits, SuperQObject):
     """ A KernelClient that provides signals and slots.
     """
