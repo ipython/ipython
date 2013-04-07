b7e5affb53334596e40d46566948c49ad9383bc4
MinRK <benjaminrk@gmail.com>
2013-04-07 12:50:10 -0700

    add `fab components`
    installs components with bower

--- IPython/frontend/html/notebook/fabfile.py
+++ IPython/frontend/html/notebook/fabfile.py
@@ -9,15 +9,19 @@ components_dir = os.path.join(static_dir,'components')
 
 def test_component(name):
     if not os.path.exists(os.path.join(components_dir,name)):
-        abort('cannot continue without component {}.'.format(name))
+        components()
 
+def components():
+    """install components with bower"""
+    with lcd(static_dir):
+        local('bower install')
 
 def css(minify=True):
     """generate the css from less files"""
     test_component('bootstrap')
     test_component('less.js')
-    if minify not in ['True','False',True,False]:
-        abort('need to get Boolean')
+    if minify not in ['True', 'False', True, False]:
+        abort('minify must be Boolean')
     minify = (minify in ['True',True])
 
     min_flag= '-x' if minify is True else ''
