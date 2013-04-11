e92008652ec3b7cbac582b06f7e0495dfd00e62c
MinRK <benjaminrk@gmail.com>
2013-04-11 15:51:24 -0700

    remove leftover duplication from splitting clientabc/channelabc

--- IPython/kernel/clientabc.py
+++ IPython/kernel/clientabc.py
@@ -1,4 +1,4 @@
-"""Abstract base classes for kernel clients and channels"""
+"""Abstract base class for kernel clients"""
 
 #-----------------------------------------------------------------------------
 #  Copyright (C) 2013  The IPython Development Team
@@ -15,119 +15,7 @@
 import abc
 
 #-----------------------------------------------------------------------------
-# Channels
-#-----------------------------------------------------------------------------
-
-
-class ChannelABC(object):
-    """A base class for all channel ABCs."""
-
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def start(self):
-        pass
-
-    @abc.abstractmethod
-    def stop(self):
-        pass
-
-    @abc.abstractmethod
-    def is_alive(self):
-        pass
-
-
-class ShellChannelABC(ChannelABC):
-    """ShellChannel ABC.
-
-    The docstrings for this class can be found in the base implementation:
-
-    `IPython.kernel.kernelmanager.ShellChannel`
-    """
-
-    @abc.abstractproperty
-    def allow_stdin(self):
-        pass
-
-    @abc.abstractmethod
-    def execute(self, code, silent=False, store_history=True,
-                user_variables=None, user_expressions=None, allow_stdin=None):
-        pass
-
-    @abc.abstractmethod
-    def complete(self, text, line, cursor_pos, block=None):
-        pass
-
-    @abc.abstractmethod
-    def object_info(self, oname, detail_level=0):
-        pass
-
-    @abc.abstractmethod
-    def history(self, raw=True, output=False, hist_access_type='range', **kwargs):
-        pass
-
-    @abc.abstractmethod
-    def kernel_info(self):
-        pass
-
-    @abc.abstractmethod
-    def shutdown(self, restart=False):
-        pass
-
-
-class IOPubChannelABC(ChannelABC):
-    """IOPubChannel ABC.
-
-    The docstrings for this class can be found in the base implementation:
-
-    `IPython.kernel.kernelmanager.IOPubChannel`
-    """
-
-    @abc.abstractmethod
-    def flush(self, timeout=1.0):
-        pass
-
-
-class StdInChannelABC(ChannelABC):
-    """StdInChannel ABC.
-
-    The docstrings for this class can be found in the base implementation:
-
-    `IPython.kernel.kernelmanager.StdInChannel`
-    """
-
-    @abc.abstractmethod
-    def input(self, string):
-        pass
-
-
-class HBChannelABC(ChannelABC):
-    """HBChannel ABC.
-
-    The docstrings for this class can be found in the base implementation:
-
-    `IPython.kernel.kernelmanager.HBChannel`
-    """
-
-    @abc.abstractproperty
-    def time_to_dead(self):
-        pass
-
-    @abc.abstractmethod
-    def pause(self):
-        pass
-
-    @abc.abstractmethod
-    def unpause(self):
-        pass
-
-    @abc.abstractmethod
-    def is_beating(self):
-        pass
-
-
-#-----------------------------------------------------------------------------
-# Main kernel manager class
+# Main kernel client class
 #-----------------------------------------------------------------------------
 
 class KernelClientABC(object):
@@ -135,7 +23,7 @@ class KernelClientABC(object):
 
     The docstrings for this class can be found in the base implementation:
 
-    `IPython.kernel.kernelmanager.KernelClient`
+    `IPython.kernel.client.KernelClient`
     """
 
     __metaclass__ = abc.ABCMeta
