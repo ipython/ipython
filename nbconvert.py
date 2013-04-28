499e7245533b7de5048626e8a9053e79a41f3bbf
Jonathan Frederic <jdfreder@calpoly.edu>
2013-04-28 10:24:47 -0700

    Refactoring of entrypoint

--- nbconvert.py
+++ nbconvert.py
@@ -1,116 +1,138 @@
 #!/usr/bin/env python
+
+"""NBConvert is a utility for conversion of IPYNB files.
+
+Commandline interface for the NBConvert conversion utility.  Read the
+readme.rst for usage information
 """
-================================================================================
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, the IPython Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
-|,---.     |    |                 ,   .|                                   |
-||---',   .|--- |---.,---.,---.   |\  ||---.,---.,---.,---..    ,,---.,---.|---
-||    |   ||    |   ||   ||   |   | \ ||   ||    |   ||   | \  / |---'|    |
-``    `---|`---'`   '`---'`   '   `  `'`---'`---'`---'`   '  `'  `---'`    `---'
-      `---'
-================================================================================
-
-Highly experimental for now
-
-"""
 #-----------------------------------------------------------------------------
 # Imports
 #-----------------------------------------------------------------------------
+
+# Stdlib imports
 from __future__ import print_function
 import sys
 import io
 import os
 
-from converters.template import ConverterTemplate
 # From IPython
-
 # All the stuff needed for the configurable things
 from IPython.config.application import Application
 from IPython.config.loader import ConfigFileNotFound
 from IPython.utils.traitlets import Unicode, Bool
 
-from converters.transformers import (ExtractFigureTransformer)
+# Local imports
+from converters.template import ConverterTemplate #TODO
+from converters.config import GlobalConfigurable #TODO
+from converters.transformers import (ExtractFigureTransformer) #TODO
 
-from converters.config import GlobalConfigurable
-
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
 NBCONVERT_DIR = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
-
+ERROR_CONFIG_NOT_FOUND = "Config file for profile '%s' not found, giving up."
+#-----------------------------------------------------------------------------
+# Classes and functions
+#-----------------------------------------------------------------------------
 class NbconvertApp(Application):
-    """A basic application to convert ipynb files
+    """A basic application to convert ipynb files"""
 
-    """
+    stdout = Bool(
+        True, config=True,
+        help="""Whether to print the converted ipynb file to stdout
+        "use full do diff files without actually writing a new file"""
+        )
 
-    stdout = Bool(True, config=True,
-            help="""
-            Wether to print the converted ipynb file to stdout
-            use full do diff files without actually writing a new file
-            """)
+    write = Bool(
+        False, config=True,
+        help="""Should the converted notebook file be written to disk
+        along with potential extracted resources."""
+        )
 
-    write = Bool(False, config=True,
-            help="""Shoudl the converted notebook file be written to disk
-            along with potential extracted resources.
-            """
-            )
-
-    fileext = Unicode('txt', config=True,
-            help="""Extension of the file that should be written to disk"""
-            )
+    fileext = Unicode(
+        'txt', config=True, 
+        help="Extension of the file that should be written to disk"
+        )
 
     aliases = {
-            'stdout':'NbconvertApp.stdout',
-            'write':'NbconvertApp.write',
-            }
+        'stdout':'NbconvertApp.stdout',
+        'write':'NbconvertApp.write'
+        }
 
     flags = {}
     flags['no-stdout'] = (
-        {'NbconvertApp' : {'stdout' : False}},
-    """Do not print converted file to stdout, equivalent to --stdout=False
-    """
-    )
+        {'NbconvertApp' : {'stdout' : False}}, 
+        """Do not print converted file to stdout, equivalent to 
+        --stdout=False"""
+        )
 
     def __init__(self, **kwargs):
+        """Public constructor"""
+
+        #Call base class
         super(NbconvertApp, self).__init__(**kwargs)
+
+        #Register class here to have help with help all
         self.classes.insert(0, ConverterTemplate)
-        # register class here to have help with help all
         self.classes.insert(0, ExtractFigureTransformer)
         self.classes.insert(0, GlobalConfigurable)
 
-    def load_config_file(self, profile_name):
-        """load a config file from the config file dir
 
-        profile_name : {string} name of the profile file to load without file extension.
+    def load_config_file(self, profile_name):
+        """Load a config file from the config file dir
+
+        profile_name : {string} name of the profile file to load without file 
+        extension.
         """
+
+        #Try to load the config file.  If the file isn't found, catch the 
+        #exception.
         try:
             Application.load_config_file(
                 self,
-                profile_name+'.py',
+                profile_name + '.py',
                 path=[os.path.join(NBCONVERT_DIR, 'profile')]
-            )
+                )
             return True
+
         except ConfigFileNotFound:
-            self.log.warn("Config file for profile '%s' not found, giving up ", profile_name)
+            self.log.warn(ERROR_CONFIG_NOT_FOUND, profile_name)
             return False
 
 
-    def initialize(self, argv=None):
-        """parse command line and load config"""
+    def start(self, argv=None):
+        """Convert a notebook in one step"""
+
+        #Parse the commandline options.
         self.parse_command_line(argv)
+
+        #Load an addition config file if specified by the user via the 
+        #commandline.
         cl_config = self.config
         profile_file = argv[1]
         if not self.load_config_file(profile_file):
             exit(1)
         self.update_config(cl_config)
 
+        #Call base
+        super(NbconvertApp, self).start()
 
-
-    def run(self):
-        """Convert a notebook in one step"""
+        #The last arguments in chain of arguments will be used as conversion
         ipynb_file = (self.extra_args or [None])[2]
 
         # If you are writting a custom transformer, append it to the dictionary
         # below.
         userpreprocessors = {}
-        
-        # Create the converter
+
+        # Create the Jinja template converter TODO, refactor this
         C = ConverterTemplate(config=self.config, preprocessors=userpreprocessors)
 
         output, resources = C.from_filename(ipynb_file)
@@ -147,16 +169,15 @@ they need to be.
 ===========================================================================
                   """)
 
-def main():
-    """Convert a notebook to html in one step"""
-    app = NbconvertApp.instance()
-    app.description = __doc__
-    app.initialize(argv=sys.argv)
-    app.start()
-    app.run()
 #-----------------------------------------------------------------------------
 # Script main
 #-----------------------------------------------------------------------------
+def main():
+    """Convert a notebook in one step"""
+    
+    app = NbconvertApp.instance()
+    app.description = __doc__
+    app.run(argv=sys.argv)
 
 if __name__ == '__main__':
     main()
