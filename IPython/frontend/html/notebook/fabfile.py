2571b05781ddb612a25ab1b054c0bfd679404bbe
Brian E. Granger <ellisonbg@gmail.com>
2013-04-27 17:53:41 -0700

    Removing tests from fabfile that are no longer needed.

--- IPython/frontend/html/notebook/fabfile.py
+++ IPython/frontend/html/notebook/fabfile.py
@@ -7,19 +7,9 @@ import os
 static_dir = 'static'
 components_dir = os.path.join(static_dir,'components')
 
-def test_component(name):
-    if not os.path.exists(os.path.join(components_dir,name)):
-        components()
-
-def components():
-    """install components with bower"""
-    with lcd(static_dir):
-        local('bower install')
 
 def css(minify=True):
     """generate the css from less files"""
-    test_component('bootstrap')
-    test_component('less.js')
     if minify not in ['True', 'False', True, False]:
         abort('minify must be Boolean')
     minify = (minify in ['True',True])
