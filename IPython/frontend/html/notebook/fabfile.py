6e2935fcbd36b4f23417581a1bdb13d2cc83b716
Brian E. Granger <ellisonbg@gmail.com>
2013-05-16 11:46:54 -0700

    Updating fabfile and templates for global style.min.css.

--- IPython/frontend/html/notebook/fabfile.py
+++ IPython/frontend/html/notebook/fabfile.py
@@ -6,28 +6,20 @@ import os
 
 static_dir = 'static'
 components_dir = os.path.join(static_dir, 'components')
-to_compile = [
-    ('notebooks', 'style.less'),
-    ('tree', 'style.less'),
-    ('auth', 'style.less')
-]
 
 def css(minify=True):
     """generate the css from less files"""
-    for subdir, filename in to_compile:
-        _compile_less(subdir, filename, minify=minify)
+    source = os.path.join('style', 'style.less')
+    target = os.path.join('style', 'style.min.css')
+    _compile_less(source, target, minify)
 
-
-def _compile_less(subdir, filename, minify=True):
+def _compile_less(source, target, minify=True):
+    """Complie a less file by source and target relative to static_dir"""
     if minify not in ['True', 'False', True, False]:
         abort('minify must be Boolean')
     minify = (minify in ['True',True])
     min_flag= '-x' if minify is True else ''
     lessc = os.path.join('components', 'less.js', 'bin', 'lessc')
-
-    source = os.path.join(subdir, 'less', filename)
-    target = os.path.join(subdir, 'css', filename.replace('.less','.min.css'))
-    subdir = os.path.join(static_dir, subdir)
     with lcd(static_dir):
         local('{lessc} {min_flag} {source} {target}'.format(**locals()))
 
