fa36e98f00812064d63627d48c48d84d42ea492e
MinRK <benjaminrk@gmail.com>
2013-06-20 10:17:08 -0400

    mv IPython.html.notebook -> IPython.html

---
+++ IPython/html/__init__.py
@@ -0,0 +1,7 @@
+"""The IPython HTML Notebook"""
+
+import os
+# Packagers: modify this line if you store the notebook static files elsewhere
+DEFAULT_STATIC_FILES_PATH = os.path.join(os.path.dirname(__file__), "static")
+
+del os
