6d571e1d0c3a6090c8ee9d1e7b57b6dfe6867984
MinRK <benjaminrk@gmail.com>
2013-06-20 11:35:29 -0400

    note notebook move in frontend.py docstring

--- IPython/frontend.py
+++ IPython/frontend.py
@@ -2,7 +2,8 @@
 Shim to maintain backwards compatibility with old frontend imports.
 
 We have moved all contents of the old `frontend` subpackage into top-level
-subpackages (`html`, `qt` and `terminal`).  
+subpackages (`html`, `qt` and `terminal`), and flattened the notebook into
+just `IPython.html`, formerly `IPython.frontend.html.notebook`.
 
 This will let code that was making `from IPython.frontend...` calls continue
 working, though a warning will be printed.
