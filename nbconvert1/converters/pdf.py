7fbc6d5f6182bb8a913cc4b7a89bf46735616a6c
MinRK <benjaminrk@gmail.com>
2013-06-30 18:02:42 -0700

    remove nbconvert1

--- nbconvert1/converters/pdf.py
+++
@@ -1,7 +0,0 @@
-from .latex import ConverterLaTeX
-import subprocess
-
-class ConverterLaTeXToPDF(ConverterLaTeX):
-    def render(self):
-        super(ConverterLaTeXToPDF, self).render()
-        subprocess.check_call(['pdflatex', self.outbase + '.tex'])
