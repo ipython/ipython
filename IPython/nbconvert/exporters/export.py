6c4c9f83b6e22e5928231e423d1738f559c63794
Paul Ivanov <pi@berkeley.edu>
2013-07-08 10:45:43 -0700

    more informative "template not found" message

--- IPython/nbconvert/exporters/export.py
+++ IPython/nbconvert/exporters/export.py
@@ -221,5 +221,5 @@ def export_by_name(template_name, nb, config=None, transformers=None, filters=No
     if function_name in globals():
         return globals()[function_name](nb, config, transformers, filters)
     else:
-        raise NameError("template not found")
+        raise NameError("template for `%s` not found" % function_name)
 
