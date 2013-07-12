9efb1a52584ce7228bce4b2e23df53b6ca4ddb63
Jonathan Frederic <jdfreder@calpoly.edu>
2013-07-12 13:08:51 -0700

    Added missing transformer in init

--- IPython/nbconvert/transformers/__init__.py
+++ IPython/nbconvert/transformers/__init__.py
@@ -6,6 +6,7 @@ from .extractfigure import ExtractFigureTransformer
 from .revealhelp import RevealHelpTransformer
 from .latex import LatexTransformer
 from .sphinx import SphinxTransformer
+from .csshtmlheader import CSSHTMLHeaderTransformer
 
 # decorated function Transformers
 from .coalescestreams import coalesce_streams
