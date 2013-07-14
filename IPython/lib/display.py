7cc195e00dac3119a6e3b2180a761b71f350b8a7
Brian E. Granger <ellisonbg@gmail.com>
2013-07-13 17:40:52 -0700

    Add xkcdify function to display.
    This enables the xkcd insipred font Humor Sans for the notebook.

--- IPython/lib/display.py
+++ IPython/lib/display.py
@@ -341,3 +341,32 @@ class FileLinks(FileLink):
         for dirname, subdirs, fnames in walked_dir:
             result_lines += self.terminal_display_formatter(dirname, fnames, self.included_suffixes)
         return '\n'.join(result_lines)
+
+
+def xkcdify():
+    """Turn on xkcd mode for the notebook.
+    
+    Run this in your notebook and it will enable the xkcd inspired font
+    Humor Sans from http://antiyawn.com/uploads/Humor-Sans.ttf. Your browser
+    will load this font from the internet so this only works if you are online.
+
+    This mode can be used with Matplotlib's new xkcd mode to great effect. However,
+    you will have to enable that separately by calling matplotlibs xkcd function.
+    """
+    from IPython.display import display, HTML
+    display(HTML("""
+    <style>
+    @font-face {
+        font-family: "xkcd";
+        src: url('http://antiyawn.com/uploads/Humor-Sans.ttf');
+    }
+
+    .rendered_html {font-family: xkcd; font-size: 120%}
+    .CodeMirror {font-family: xkcd; font-size: 120%}
+    div.output_area pre {font-family: xkcd; font-size: 120%}
+    pre, code {font-family: xkcd; font-size: 120%}
+    div.prompt {font-family: xkcd; font-size: 120%}
+
+
+    </style>
+    """))
