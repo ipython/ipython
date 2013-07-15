9f15eb132ce63c286500144a2fb84f7b1997dc57
MinRK <benjaminrk@gmail.com>
2013-07-15 11:43:09 -0700

    use `python -m IPython` in test_console as well

--- IPython/terminal/console/tests/test_console.py
+++ IPython/terminal/console/tests/test_console.py
@@ -1,6 +1,6 @@
 """Tests for two-process terminal frontend
 
-Currenlty only has the most simple test possible, starting a console and running
+Currently only has the most simple test possible, starting a console and running
 a single command.
 
 Authors:
@@ -12,6 +12,7 @@ Authors:
 # Imports
 #-----------------------------------------------------------------------------
 
+import sys
 import time
 
 import nose.tools as nt
@@ -30,19 +31,7 @@ def test_console_starts():
     """test that `ipython console` starts a terminal"""
     from IPython.external import pexpect
     
-    # weird IOErrors prevent this from firing sometimes:
-    ipython_cmd = None
-    for i in range(5):
-        try:
-            ipython_cmd = find_cmd('ipython3' if py3compat.PY3 else 'ipython')
-        except IOError:
-            time.sleep(0.1)
-        else:
-            break
-    if ipython_cmd is None:
-        raise SkipTest("Could not determine ipython command")
-    
-    p = pexpect.spawn(ipython_cmd, args=['console', '--colors=NoColor'])
+    p = pexpect.spawn(sys.executable, args=['-m', 'IPython', 'console', '--colors=NoColor'])
     idx = p.expect([r'In \[\d+\]', pexpect.EOF], timeout=15)
     nt.assert_equal(idx, 0, "expected in prompt")
     p.sendline('5')
