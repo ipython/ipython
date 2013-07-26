b0514d67355b2a44b79b81ece43a3db3edc19a96
Kyle Kelley <kyle.kelley@rackspace.com>
2013-07-26 12:02:49 -0500

    Added reading and writing notebooks

--- IPython/html/services/notebooks/openstacknbmanager.py
+++ IPython/html/services/notebooks/openstacknbmanager.py
@@ -23,6 +23,7 @@ Authors:
 import datetime
 
 import pyrax
+from pyrax import pyrax.exceptions.NoSuchContainer as NoSuchContainer
 
 from tornado import web
 
@@ -40,7 +41,7 @@ class OpenStackNotebookManager(NotebookManager):
     account_name = Unicode('', config=True, help='OpenStack account name.')
     account_key = Unicode('', config=True, help='OpenStack account key.')
     identity_type = Unicode('', config=True, help='OpenStack Identity type (e.g. rackspace)')
-    container = Unicode('', config=True, help='Container name for notebooks.')
+    container_name = Unicode('', config=True, help='Container name for notebooks.')
 
     def __init__(self, **kwargs):
         super(OpenStackNotebookManager, self).__init__(**kwargs)
@@ -48,7 +49,8 @@ class OpenStackNotebookManager(NotebookManager):
         pyrax.set_credentials(username=account_name, api_key=account_key)
         # Set the region, optionally
         # pyrax.set_setting("region", region) # e.g. "LON"
-        pass
+
+        self.cf = pyrax.cloudfiles
 
     def load_notebook_names(self):
         """On startup load the notebook ids and names from OpenStack Swift.
@@ -56,6 +58,15 @@ class OpenStackNotebookManager(NotebookManager):
         The object names are the notebook ids and the notebook names are stored
         as object metadata.
         """
+        # Cached version of the mapping of notebook IDs to notebook names
+        self.mapping = {}
+
+        try:
+            container = self.cf.get_container(self.container_name)
+        except NoSuchContainer:
+            container = self.cf.create_container(self.container_name)
+        objects = container.get_objects()
+
         pass
 
     def list_notebooks(self):
@@ -67,11 +78,50 @@ class OpenStackNotebookManager(NotebookManager):
 
     def read_notebook_object(self, notebook_id):
         """Get the object representation of a notebook by notebook_id."""
-        pass
+        if not self.notebook_exists(notebook_id):
+            raise web.HTTPError(404, u'Notebook does not exist: %s' % notebook_id)
+        try:
+            obj = self.container.get_object(notebook_id)
+            s = obj.get() # Read the file into s
+        except:
+            raise web.HTTPError(500, u'Notebook cannot be read.')
+        try:
+            nb = current.reads(s, u'json')
+        except:
+            raise web.HTTPError(500, u'Unreadable JSON notebook.')
+        # Todo: The last modified should actually be saved in the notebook document.
+        # We are just using the current datetime until that is implemented.
+        last_modified = tz.utcnow()
+        return last_modified, nb
 
     def write_notebook_object(self, nb, notebook_id=None):
         """Save an existing notebook object by notebook_id."""
-        pass
+
+        try:
+            new_name = nb.metadata.name
+        except AttributeError:
+            raise web.HTTPError(400, u'Missing notebook name')
+
+        if notebook_id is None:
+            notebook_id = self.new_notebook_id(new_name)
+
+        if notebook_id not in self.mapping:
+            raise web.HTTPError(404, u'Notebook does not exist: %s' % notebook_id)
+
+        try:
+            data = current.writes(nb, u'json')
+        except Exception as e:
+            raise web.HTTPError(400, u'Unexpected error while saving notebook: %s' % e)
+
+        metadata = {'nbname': new_name}
+        try:
+            obj = self.container.store_object(notebook_id, data)
+            obj.set_metadata(metadata)
+        except Exception as e:
+            raise web.HTTPError(400, u'Unexpected error while saving notebook: %s' % e)
+
+        self.mapping[notebook_id] = new_name
+        return notebook_id
 
     def delete_notebook(self, notebook_id):
         """Delete notebook by notebook_id."""
