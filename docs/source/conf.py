ec8ea8ebd2c220f62c71cc014879d2d183c5315c
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-07-29 22:25:36 +0200

    fix rtfd by removing  sphinxext import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,10 +30,6 @@ if ON_RTD:
 # absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../sphinxext'))
 
-# Import support for ipython console session syntax highlighting
-# (lives IPython's sphinxext subpackage)
-from IPython.sphinxext import ipython_console_highlighting
-
 # We load the ipython release info into a dict by explicit execution
 iprelease = {}
 execfile('../../IPython/core/release.py',iprelease)
@@ -62,6 +58,7 @@ if ON_RTD:
     extensions.remove('matplotlib.sphinxext.mathmpl')
     extensions.remove('matplotlib.sphinxext.plot_directive')
     extensions.remove('IPython.sphinxext.ipython_directive')
+    extensions.remove('IPython.sphinxext.ipython_console_highlighting')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
