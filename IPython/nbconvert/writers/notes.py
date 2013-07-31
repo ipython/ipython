693211ae9b10f70c71556cc33893208589fa2d46
damianavila <damianavila82@yahoo.com.ar>
2013-07-31 02:09:47 -0300

    Fixing docstrings and imports not used.

--- IPython/nbconvert/writers/notes.py
+++ IPython/nbconvert/writers/notes.py
@@ -1,5 +1,5 @@
 """
-Contains writer for writing nbconvert output to filesystem.
+Contains writer for writing speaker notes plugin for reveal slides.
 """
 #-----------------------------------------------------------------------------
 #Copyright (c) 2013, the IPython Development Team.
@@ -13,16 +13,10 @@ Contains writer for writing nbconvert output to filesystem.
 # Imports
 #-----------------------------------------------------------------------------
 
-import io
 import os
-import glob
 import urllib2
 import codecs
 
-from IPython.utils.traitlets import Unicode
-from IPython.utils.path import link_or_copy
-
-from .base import WriterBase
 from .files import FilesWriter
 
 #-----------------------------------------------------------------------------
@@ -30,12 +24,12 @@ from .files import FilesWriter
 #-----------------------------------------------------------------------------
 
 class NotesWriter(FilesWriter):
-    """Consumes nbconvert output and produces files."""
+    """Build the reveal speaker notes plugin infrastructure 
+       to support the speaker notes functionality."""
 
 
     def write_notes(self):
-            """
-            """
+            """Writes the necessary html and js files."""
 
             html_infile = 'http://cdn.jsdelivr.net/reveal.js/2.4.0/plugin/notes/notes.html'
             html_outfile = "notes.html"    
@@ -43,19 +37,18 @@ class NotesWriter(FilesWriter):
             
             js_infile = 'http://cdn.jsdelivr.net/reveal.js/2.4.0/plugin/notes/notes.js'
             js_outfile = "notes.js"
-            self.write_helper(js_infile, js_outfile) 
+            self.write_helper(js_infile, js_outfile)
             
 
     def write_helper(self, infile, outfile):
-            """
-            """
-                      
+            """Helper function to write the content of a url to a file."""
+            
             notes_path = "reveal.js/plugin/notes"
             dir_dest = os.path.join(self.build_directory, notes_path)
             if not os.path.isdir(dir_dest):
                 os.makedirs(dir_dest)
             
             file_dest = os.path.join(dir_dest, outfile)
-
+            
             with codecs.open(file_dest, 'w', 'utf-8') as f:
                 f.write(urllib2.urlopen(infile).read())
