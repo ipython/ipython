50185a9f6a08365896541308d91bc67f889fe524
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-08-10 16:41:51 +0200

    add detaild info for most common error no prompt number

--- IPython/nbformat/v3/validator.py
+++ IPython/nbformat/v3/validator.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+"""
+methods to validate json notebook agains a schema.
+
+Instead of having a deep schema, top level of the schema contain subscheme
+that describe individually each type of cell and can be referenced with
+json references.
+
+Thus when asking to validate a json object agains a schema you need
+to specify the key
+
+"""
+
 from jsonschema import  Draft3Validator, validate, ValidationError
 import jsonpointer
 from IPython.external import argparse
@@ -9,7 +21,13 @@ import jsonref
 import json
 import os.path
 
-def nbvalidate(nbjson, schema, key=None, verbose=False):
+def nbvalidate(nbjson, schema, key='/notebook', verbose=False):
+    """validate notebook versus a json schema
+    
+    key: json pointer to a subkey of the schema
+    use if you only want to validate a sub-part of a notebook
+    like a code_cell or worksheet.
+    """
     if key :
         schema = jsonpointer.resolve_pointer(schema,key)
     errors = 0
@@ -20,7 +38,29 @@ def nbvalidate(nbjson, schema, key=None, verbose=False):
             print(error)
     return errors
 
+def detailed_error(nbjson):
+    """try to describe errors as much as possible"""
+    try :
+        # loop through each cell and if code cell check
+        # for prompt number
+        for cell in nbjson['worksheets'][0]['cells']:
+            if cell['cell_type'] == 'code':
+                if "prompt_number" not in cell.keys():
+                    print "          - code cell has no prompt number...."
+                else :
+                    if type(cell["prompt_number"]) is not int:
+                        print "          - prompt number is not int"
+    except Exception as e:
+        print " "*8,"unknown error",e
+
+
+@property
 def v3schema():
+    return getV3schema()
+
+def getV3schema():
+    """schema to validate v3 notebook
+    """
     with open(os.path.join(os.path.dirname(__file__),'v3.withref.json')) as f:
         schema=jsonref.load(f)
     return schema
@@ -28,7 +68,7 @@ def v3schema():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--schema',
-                    type=str, default='v3.withref.json')
+                    type=str, default='')
 
     parser.add_argument('-k', '--key',
                     type=str, default='/notebook',
@@ -45,12 +85,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    with open(args.schema) as f:
-        schema=jsonref.load(f)
+    if args.schema :
+        with open(args.schema) as f:
+            schema=jsonref.load(f)
+    else:
+        schema = getV3schema()
 
     for name in args.filename :
         with open(name) as notebook:
-            nerror = nbvalidate(json.load(notebook),
+            nb = json.load(notebook)
+            nerror = nbvalidate(nb,
                                 schema,
                                 key=args.key,
                                 verbose=args.verbose)
@@ -58,6 +102,7 @@ if __name__ == '__main__':
             print u"[Pass]",name
         else :
             print u"[    ]",name,'(%d)'%(nerror)
+            detailed_error(nb)
         if args.verbose :
             print '=================================================='
 
