8730be2e8159d454f438ee1275c7d3f9bb389adc
Zachary Sailer <zachsailer@gmail.com>
2013-08-14 19:04:58 -0700

    session manager restructuring
    Redid the sessionmanager, added tests for the session manager,
    and added a sqlite database to hold kernel,session,nb mapping.

--- IPython/html/services/sessions/sessionmanager.py
+++ IPython/html/services/sessions/sessionmanager.py
@@ -18,6 +18,7 @@ Authors:
 
 import os
 import uuid
+import sqlite3
 
 from tornado import web
 
@@ -31,67 +32,139 @@ from IPython.utils.traitlets import List, Dict, Unicode, TraitError
 
 class SessionManager(LoggingConfigurable):
     
-    # Use session_ids to map notebook names to kernel_ids
-    sessions = List()
+    # Session database initialized below
+    _cursor = None
+    _connection = None
     
-    def get_session(self, nb_name, nb_path=None):
-        """Get an existing session or create a new one"""
-        model = None
-        for session in self.sessions:
-            if session['name'] == nb_name and session['path'] == nb_path:
-                session_id = session['id']
-                model = session
-        if model != None:
-            return session_id, model
-        else:
-            session_id = unicode(uuid.uuid4())
-            return session_id, model
-    
-    def session_model(self, session_id, notebook_name=None, notebook_path=None, kernel=None):
-        """ Create a session that links notebooks with kernels """
-        model = dict(id=session_id,
-                name=notebook_name, 
-                path=notebook_path, 
-                kernel=kernel)
-        if notebook_path == None:
-            model['path']=""
-        self.sessions.append(model)
-        return model
-    
-    def list_sessions(self):
-        """List all sessions and their information"""
-        return self.sessions
-    
-    def set_kernel_for_sessions(self, session_id, kernel_id):
-        """Maps the kernel_ids to the session_id in session_mapping"""
-        for session in self.sessions:
-            if session['id'] == session_id:
-                session['kernel']['id'] = kernel_id
-        return self.sessions
-        
-    def delete_mapping_for_session(self, session_id):
-        """Delete the session from session_mapping with the given session_id"""
-        i = 0
-        for session in self.sessions:
-            if session['id'] == session_id:
-                del self.sessions[i]
-            i = i + 1
-        return self.sessions
-        
-    def get_session_from_id(self, session_id):
-        for session in self.sessions:
-            if session['id'] == session_id:
-                return session
+    @property
+    def cursor(self):
+        """Start a cursor and create a database called 'session'"""
+        if self._cursor is None:
+            self._cursor = self.connection.cursor()
+            self._cursor.execute("""CREATE TABLE session 
+                (id, name, path, kernel)""")
+        return self._cursor
 
-    def get_notebook_from_session(self, session_id):
-        """Returns the notebook_path for the given session_id"""
-        for session in self.sessions:
-            if session['id'] == session_id:
-                return session['name']
-    
-    def get_kernel_from_session(self, session_id):
-        """Returns the kernel_id for the given session_id"""
-        for session in self.sessions:
-            if session['id'] == session_id:
-                return session['kernel']['id']
+    @property
+    def connection(self):
+        """Start a database connection"""
+        if self._connection is None:
+            self._connection = sqlite3.connect(':memory:')
+            self._connection.row_factory = sqlite3.Row
+        return self._connection
         
+    def __del__(self):
+        """Close connection once SessionManager closes"""
+        self.cursor.close()
+
+    def session_exists(self, name, path):
+        """Check to see if the session for the given notebook exists"""
+        self.cursor.execute("SELECT * FROM session WHERE name=? AND path=?", (name,path))
+        reply = self.cursor.fetchone()
+        if reply is None:
+            return False
+        else:
+            return True
+
+    def get_session_id(self):
+        "Create a uuid for a new session"
+        return unicode(uuid.uuid4())
+
+    def save_session(self, session_id, name=None, path=None, kernel=None):
+        """ Given a session_id (and any other of the arguments), this method
+        creates a row in the sqlite session database that holds the information
+        for a session.
+        
+        Parameters
+        ----------
+        session_id : str
+            uuid for the session; this method must be given a session_id
+        name : str
+            the .ipynb notebook name that started the session
+        path : str
+            the path to the named notebook
+        kernel : str
+            a uuid for the kernel associated with this session
+        """
+        self.cursor.execute("""INSERT INTO session VALUES 
+            (?,?,?,?)""", (session_id, name, path, kernel))
+        self.connection.commit()
+
+    def get_session(self, **kwargs):
+        """ Takes a keyword argument and searches for the value in the session
+        database, then returns the rest of the session's info.
+
+        Parameters
+        ----------
+        **kwargs : keyword argument
+            must be given one of the keywords and values from the session database
+            (i.e. session_id, name, path, kernel)
+
+        Returns
+        -------
+        model : dict
+            returns a dictionary that includes all the information from the 
+            session described by the kwarg.
+        """
+        column = kwargs.keys()[0] # uses only the first kwarg that is entered
+        value = kwargs.values()[0]
+        try:
+            self.cursor.execute("SELECT * FROM session WHERE %s=?" %column, (value,))
+        except sqlite3.OperationalError:
+            raise TraitError("The session database has no column: %s" %column)
+        reply = self.cursor.fetchone()
+        if reply is not None:
+            model = self.reply_to_dictionary_model(reply)
+        else:
+            model = None
+        return model
+
+    def update_session(self, session_id, **kwargs):
+        """Updates the values in the session with the given session_id
+        with the values from the keyword arguments. 
+        
+        Parameters
+        ----------
+        session_id : str
+            a uuid that identifies a session in the sqlite3 database
+        **kwargs : str
+            the key must correspond to a column title in session database,
+            and the value replaces the current value in the session 
+            with session_id.
+        """
+        column = kwargs.keys()[0] # uses only the first kwarg that is entered
+        value = kwargs.values()[0]
+        try:
+            self.cursor.execute("UPDATE session SET %s=? WHERE id=?" %column, (value, session_id))
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            raise TraitError("No session exists with ID: %s" %session_id)
+
+    def reply_to_dictionary_model(self, reply):
+        """Takes sqlite database session row and turns it into a dictionary"""
+        model = {'id': reply['id'],
+                'name' : reply['name'],
+                'path' : reply['path'],
+                'kernel' : {'id':reply['kernel'], 'ws_url': ''}}
+        return model
+        
+    def list_sessions(self):
+        """Returns a list of dictionaries containing all the information from
+        the session database"""
+        session_list=[]
+        self.cursor.execute("SELECT * FROM session")
+        sessions = self.cursor.fetchall()
+        for session in sessions:
+            model = self.reply_to_dictionary_model(session)
+            session_list.append(model)
+        return session_list
+
+    def delete_session(self, session_id):
+        """Deletes the row in the session database with given session_id"""
+        # Check that session exists before deleting
+        model = self.get_session(id=session_id)
+        if model is None:
+            raise TraitError("The session does not exist: %s" %session_id)
+        else: 
+            self.cursor.execute("DELETE FROM session WHERE id=?", (session_id,))
+            self.connection.commit()
