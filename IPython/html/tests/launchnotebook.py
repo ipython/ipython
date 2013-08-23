cc15133f9582caf7c0e750ee5558582eecfa18ce
Zachary Sailer <zachsailer@gmail.com>
2013-08-23 10:10:24 -0700

    review fixes on tests, add extra kernel api test

--- IPython/html/tests/launchnotebook.py
+++ IPython/html/tests/launchnotebook.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import requests
 from subprocess import Popen, PIPE
 from unittest import TestCase
 
@@ -17,6 +18,26 @@ class NotebookTestBase(TestCase):
 
     port = 1234
 
+    def wait_till_alive(self):
+        url = 'http://localhost:%i/' % self.port
+        while True:
+            time.sleep(.1)
+            try:
+                r = requests.get(url + 'api/notebooks')
+                break
+            except requests.exceptions.ConnectionError:
+                pass
+        
+    def wait_till_dead(self):
+        url = 'http://localhost:%i/' % self.port
+        while True:
+            time.sleep(.1)
+            try:
+                r = requests.get(url + 'api/notebooks')
+                continue
+            except requests.exceptions.ConnectionError:
+                break
+
     def setUp(self):
         self.ipython_dir = TemporaryDirectory()
         self.notebook_dir = TemporaryDirectory()
@@ -27,15 +48,16 @@ class NotebookTestBase(TestCase):
             '--no-browser',
             '--ipython-dir=%s' % self.ipython_dir.name,
             '--notebook-dir=%s' % self.notebook_dir.name
-        ]        
+        ]  
         self.notebook = Popen(notebook_args, stdout=PIPE, stderr=PIPE)
-        time.sleep(3.0)
+        self.wait_till_alive()
+        #time.sleep(3.0)
 
     def tearDown(self):
         self.notebook.terminate()
         self.ipython_dir.cleanup()
         self.notebook_dir.cleanup()
-        time.sleep(3.0)
-
+        self.wait_till_dead()
+        
     def base_url(self):
         return 'http://localhost:%i/' % self.port
