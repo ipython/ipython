7ca3e03fdf802748d17eb9afcf10678533d9952a
Zachary Sailer <zachsailer@gmail.com>
2013-08-26 11:18:34 -0700

    refactoring of nbmanager and filenbmanager
    major clean up of the two managers. We make sure to follow
    the standard models described in IPEP 16

--- IPython/html/services/notebooks/nbmanager.py
+++ IPython/html/services/notebooks/nbmanager.py
@@ -3,6 +3,7 @@
 Authors:
 
 * Brian Granger
+* Zach Sailer
 """
 
 #-----------------------------------------------------------------------------
@@ -18,10 +19,11 @@ Authors:
 
 import os
 import uuid
-
-from tornado import web
 from urllib import quote, unquote
 
+from tornado import web
+
+from IPython.html.utils import url_path_join
 from IPython.config.configurable import LoggingConfigurable
 from IPython.nbformat import current
 from IPython.utils.traitlets import List, Dict, Unicode, TraitError
@@ -42,10 +44,13 @@ class NotebookManager(LoggingConfigurable):
             The directory to use for notebooks.
             """)
 
+    filename_ext = Unicode(u'.ipynb')
+
     def named_notebook_path(self, notebook_path):
-        """Given a notebook_path name, returns a (name, path) tuple, where
-        name is a .ipynb file, and path is the directory for the file, which
-        *always* starts *and* ends with a '/' character.
+        """Given notebook_path (*always* a URL path to notebook), returns a 
+        (name, path) tuple, where name is a .ipynb file, and path is the 
+        URL path that describes the file system path for the file. 
+        It *always* starts *and* ends with a '/' character.
 
         Parameters
         ----------
@@ -73,7 +78,7 @@ class NotebookManager(LoggingConfigurable):
         return name, path
         
     def get_os_path(self, fname=None, path='/'):
-        """Given a notebook name and a server URL path, return its file system
+        """Given a notebook name and a URL path, return its file system
         path.
 
         Parameters
@@ -99,21 +104,23 @@ class NotebookManager(LoggingConfigurable):
         return path
 
     def url_encode(self, path):
-        """Returns the path with all special characters URL encoded"""
-        parts = os.path.split(path)
-        return os.path.join(*[quote(p) for p in parts])
+        """Takes a URL path with special characters and returns 
+        the path with all these characters URL encoded"""
+        parts = path.split('/')
+        return '/'.join([quote(p) for p in parts])
 
     def url_decode(self, path):
-        """Returns the URL with special characters decoded"""
-        parts = os.path.split(path)
-        return os.path.join(*[unquote(p) for p in parts])
+        """Takes a URL path with encoded special characters and 
+        returns the URL with special characters decoded"""
+        parts = path.split('/')
+        return '/'.join([unquote(p) for p in parts])
 
-    def _notebook_dir_changed(self, new):
-        """do a bit of validation of the notebook dir"""
+    def _notebook_dir_changed(self, name, old, new):
+        """Do a bit of validation of the notebook dir."""
         if not os.path.isabs(new):
             # If we receive a non-absolute path, make it absolute.
             abs_new = os.path.abspath(new)
-            #self.notebook_dir = os.path.dirname(abs_new)
+            self.notebook_dir = os.path.dirname(abs_new)
             return
         if os.path.exists(new) and not os.path.isdir(new):
             raise TraitError("notebook dir %r is not a directory" % new)
@@ -123,27 +130,23 @@ class NotebookManager(LoggingConfigurable):
                 os.mkdir(new)
             except:
                 raise TraitError("Couldn't create notebook dir %r" % new)
-                
-    allowed_formats = List([u'json',u'py'])
 
-    def add_new_folder(self, path=None):
-        new_path = os.path.join(self.notebook_dir, path)
-        if not os.path.exists(new_path):
-            os.makedirs(new_path) 
-        else:
-            raise web.HTTPError(409, u'Directory already exists or creation permission not allowed.')
+    # Main notebook API
 
-    def load_notebook_names(self, path):
-        """Load the notebook names into memory.
-
-        This should be called once immediately after the notebook manager
-        is created to load the existing notebooks into the mapping in
-        memory.
+    def increment_filename(self, basename, path='/'):
+        """Increment a notebook filename without the .ipynb to make it unique.
+        
+        Parameters
+        ----------
+        basename : unicode
+            The name of a notebook without the ``.ipynb`` file extension.
+        path : unicode
+            The URL path of the notebooks directory
         """
-        self.list_notebooks(path)
+        return basename
 
     def list_notebooks(self):
-        """List all notebooks.
+        """Return a list of notebook dicts without content.
 
         This returns a list of dicts, each of the form::
 
@@ -155,142 +158,62 @@ class NotebookManager(LoggingConfigurable):
         """
         raise NotImplementedError('must be implemented in a subclass')
 
-    def notebook_model(self, name, path='/', content=True):
-        """ Creates the standard notebook model """
-        last_modified, contents = self.read_notebook_model(name, path)
-        model = {"name": name, 
-                    "path": path,
-                    "last_modified": last_modified.ctime()}
-        if content is True:
-            model['content'] = contents
+    def get_notebook_model(self, name, path='/', content=True):
+        """Get the notebook model with or without content."""
+        raise NotImplementedError('must be implemented in a subclass')
+
+    def save_notebook_model(self, model, name, path='/'):
+        """Save the notebook model and return the model with no content."""
+        raise NotImplementedError('must be implemented in a subclass')
+
+    def update_notebook_model(self, model, name, path='/'):
+        """Update the notebook model and return the model with no content."""
+        raise NotImplementedError('must be implemented in a subclass')
+
+    def delete_notebook_model(self, name, path):
+        """Delete notebook by name and path."""
+        raise NotImplementedError('must be implemented in a subclass')
+
+    def create_notebook_model(self, model=None, path='/'):
+        """Create a new untitled notebook and return its model with no content."""
+        name = self.increment_filename('Untitled', path)
+        if model is None:
+            model = {}
+            metadata = current.new_metadata(name=u'')
+            nb = current.new_notebook(metadata=metadata)
+            model['content'] = nb
+            model['name'] = name
+            model['path'] = path
+        model = self.save_notebook_model(model, name, path)
         return model
 
-    def get_notebook(self, notebook_name, notebook_path='/', format=u'json'):
-        """Get the representation of a notebook in format by notebook_name."""
-        format = unicode(format)
-        if format not in self.allowed_formats:
-            raise web.HTTPError(415, u'Invalid notebook format: %s' % format)
-        kwargs = {}
-        last_mod, nb = self.read_notebook_object(notebook_name, notebook_path)
-        if format == 'json':
-            # don't split lines for sending over the wire, because it
-            # should match the Python in-memory format.
-            kwargs['split_lines'] = False
-        representation = current.writes(nb, format, **kwargs)
-        name = nb.metadata.get('name', 'notebook')
-        return last_mod, representation, name
-
-    def read_notebook_model(self, notebook_name, notebook_path='/'):
-        """Get the object representation of a notebook by notebook_id."""
-        raise NotImplementedError('must be implemented in a subclass')
-
-    def save_notebook(self, model, name=None, path='/'):
-        """Save the Notebook"""
-        if name is None:
-            name = self.increment_filename('Untitled', path)
-        if 'content' not in model:
-            metadata = current.new_metadata(name=name)
-            nb = current.new_notebook(metadata=metadata)
-        else:
-            nb = model['content']
-        self.write_notebook_object()
-            
-
-    def save_new_notebook(self, data, notebook_path='/', name=None, format=u'json'):
-        """Save a new notebook and return its name.
-
-        If a name is passed in, it overrides any values in the notebook data
-        and the value in the data is updated to use that value.
-        """
-        if format not in self.allowed_formats:
-            raise web.HTTPError(415, u'Invalid notebook format: %s' % format)
-
-        try:
-            nb = current.reads(data.decode('utf-8'), format)
-        except:
-            raise web.HTTPError(400, u'Invalid JSON data')
-
-        if name is None:
-           try:
-               name = nb.metadata.name
-           except AttributeError:
-               raise web.HTTPError(400, u'Missing notebook name')
-        nb.metadata.name = name
-
-        notebook_name = self.write_notebook_object(nb, notebook_path=notebook_path)
-        return notebook_name
-
-    def save_notebook(self, data, notebook_path='/', name=None, format=u'json'):
-        """Save an existing notebook by notebook_name."""
-        if format not in self.allowed_formats:
-            raise web.HTTPError(415, u'Invalid notebook format: %s' % format)
-
-        try:
-            nb = current.reads(data.decode('utf-8'), format)
-        except:
-            raise web.HTTPError(400, u'Invalid JSON data')
-
-        if name is not None:
-            nb.metadata.name = name
-        self.write_notebook_object(nb, name, notebook_path, new_name)
-
-    def write_notebook_model(self, model):
-        """Write a notebook object and return its notebook_name.
-
-        If notebook_name is None, this method should create a new notebook_name.
-        If notebook_name is not None, this method should check to make sure it
-        exists and is valid.
-        """
-        raise NotImplementedError('must be implemented in a subclass')
-
-    def delete_notebook(self, notebook_name, notebook_path):
-        """Delete notebook by notebook_id."""
-        raise NotImplementedError('must be implemented in a subclass')
-
-    def increment_filename(self, name):
-        """Increment a filename to make it unique.
-
-        This exists for notebook stores that must have unique names. When a notebook
-        is created or copied this method constructs a unique filename, typically
-        by appending an integer to the name.
-        """
-        return name
-
-    def new_notebook(self, notebook_path='/'):
-        """Create a new notebook and return its notebook_name."""
-        name = self.increment_filename('Untitled', notebook_path)
-        metadata = current.new_metadata(name=name)
-        nb = current.new_notebook(metadata=metadata)
-        notebook_name = self.write_notebook_object(nb, notebook_path=notebook_path)
-        return notebook_name
-
-    def copy_notebook(self, name, path='/'):
-        """Copy an existing notebook and return its new notebook_name."""
-        last_mod, nb = self.read_notebook_object(name, path)
-        name = nb.metadata.name + '-Copy'
-        name = self.increment_filename(name, path)
-        nb.metadata.name = name
-        notebook_name = self.write_notebook_object(nb, notebook_path = path)
-        return notebook_name  
+    def copy_notebook(self, name, path='/', content=False):
+        """Copy an existing notebook and return its new model."""
+        model = self.get_notebook_model(name, path)
+        name = os.path.splitext(name)[0] + '-Copy'
+        name = self.increment_filename(name, path) + self.filename_ext
+        model['name'] = name
+        model = self.save_notebook_model(model, name, path, content=content)
+        return model
     
     # Checkpoint-related
     
-    def create_checkpoint(self, notebook_name, notebook_path='/'):
+    def create_checkpoint(self, name, path='/'):
         """Create a checkpoint of the current state of a notebook
         
         Returns a checkpoint_id for the new checkpoint.
         """
         raise NotImplementedError("must be implemented in a subclass")
     
-    def list_checkpoints(self, notebook_name, notebook_path='/'):
+    def list_checkpoints(self, name, path='/'):
         """Return a list of checkpoints for a given notebook"""
         return []
     
-    def restore_checkpoint(self, notebook_name, checkpoint_id, notebook_path='/'):
+    def restore_checkpoint(self, checkpoint_id, name, path='/'):
         """Restore a notebook from one of its checkpoints"""
         raise NotImplementedError("must be implemented in a subclass")
 
-    def delete_checkpoint(self, notebook_name, checkpoint_id, notebook_path='/'):
+    def delete_checkpoint(self, checkpoint_id, name, path='/'):
         """delete a checkpoint for a notebook"""
         raise NotImplementedError("must be implemented in a subclass")
     
--- IPython/html/services/notebooks/tests/test_nbmanager.py
+++ IPython/html/services/notebooks/tests/test_nbmanager.py
@@ -1,11 +1,14 @@
 """Tests for the notebook manager."""
 
 import os
+
+from tornado.web import HTTPError
 from unittest import TestCase
 from tempfile import NamedTemporaryFile
 
 from IPython.utils.tempdir import TemporaryDirectory
 from IPython.utils.traitlets import TraitError
+from IPython.html.utils import url_path_join
 
 from ..filenbmanager import FileNotebookManager
 from ..nbmanager import NotebookManager
@@ -54,6 +57,16 @@ class TestFileNotebookManager(TestCase):
             self.assertEqual(path, fs_path)
 
 class TestNotebookManager(TestCase):
+    
+    def make_dir(self, abs_path, rel_path):
+        """make subdirectory, rel_path is the relative path
+        to that directory from the location where the server started"""
+        os_path = os.path.join(abs_path, rel_path)
+        try:
+            os.makedirs(os_path)
+        except OSError:
+            print "Directory already exists."
+
     def test_named_notebook_path(self):
         nm = NotebookManager()
 
@@ -98,14 +111,156 @@ class TestNotebookManager(TestCase):
 
     def test_url_decode(self):
         nm = NotebookManager()
-        
+
         # decodes a url string to a plain string
         # these tests decode paths with spaces
         path = nm.url_decode('/this%20is%20a%20test/for%20spaces/')
         self.assertEqual(path, '/this is a test/for spaces/')
-        
+
         path = nm.url_decode('notebook%20with%20space.ipynb')
         self.assertEqual(path, 'notebook with space.ipynb')
-        
+
         path = nm.url_decode('/path%20with%20a/notebook%20and%20space.ipynb')
         self.assertEqual(path, '/path with a/notebook and space.ipynb')
+
+    def test_create_notebook_model(self):
+        with TemporaryDirectory() as td:
+            # Test in root directory
+            nm = FileNotebookManager(notebook_dir=td)
+            model = nm.create_notebook_model()
+            assert isinstance(model, dict)
+            self.assertIn('name', model)
+            self.assertIn('path', model)
+            self.assertEqual(model['name'], 'Untitled0.ipynb')
+            self.assertEqual(model['path'], '/')
+
+            # Test in sub-directory
+            sub_dir = '/foo/'
+            self.make_dir(nm.notebook_dir, 'foo')
+            model = nm.create_notebook_model(None, sub_dir)
+            assert isinstance(model, dict)
+            self.assertIn('name', model)
+            self.assertIn('path', model)
+            self.assertEqual(model['name'], 'Untitled0.ipynb')
+            self.assertEqual(model['path'], sub_dir)
+
+    def test_get_notebook_model(self):
+        with TemporaryDirectory() as td:
+            # Test in root directory
+            # Create a notebook
+            nm = FileNotebookManager(notebook_dir=td)
+            model = nm.create_notebook_model()
+            name = model['name']
+            path = model['path']
+
+            # Check that we 'get' on the notebook we just created
+            model2 = nm.get_notebook_model(name, path)
+            assert isinstance(model2, dict)
+            self.assertIn('name', model2)
+            self.assertIn('path', model2)
+            self.assertEqual(model['name'], name)
+            self.assertEqual(model['path'], path)
+
+            # Test in sub-directory
+            sub_dir = '/foo/'
+            self.make_dir(nm.notebook_dir, 'foo')
+            model = nm.create_notebook_model(None, sub_dir)
+            model2 = nm.get_notebook_model(name, sub_dir)
+            assert isinstance(model2, dict)
+            self.assertIn('name', model2)
+            self.assertIn('path', model2)
+            self.assertIn('content', model2)
+            self.assertEqual(model2['name'], 'Untitled0.ipynb')
+            self.assertEqual(model2['path'], sub_dir)
+            
+    def test_update_notebook_model(self):
+        with TemporaryDirectory() as td:
+            # Test in root directory
+            # Create a notebook
+            nm = FileNotebookManager(notebook_dir=td)
+            model = nm.create_notebook_model()
+            name = model['name']
+            path = model['path']
+
+            # Change the name in the model for rename
+            model['name'] = 'test.ipynb'
+            model = nm.update_notebook_model(model, name, path)
+            assert isinstance(model, dict)
+            self.assertIn('name', model)
+            self.assertIn('path', model)
+            self.assertEqual(model['name'], 'test.ipynb')
+
+            # Make sure the old name is gone
+            self.assertRaises(HTTPError, nm.get_notebook_model, name, path)
+
+            # Test in sub-directory
+            # Create a directory and notebook in that directory
+            sub_dir = '/foo/'
+            self.make_dir(nm.notebook_dir, 'foo')
+            model = nm.create_notebook_model(None, sub_dir)
+            name = model['name']
+            path = model['path']
+            
+            # Change the name in the model for rename
+            model['name'] = 'test_in_sub.ipynb'
+            model = nm.update_notebook_model(model, name, path)
+            assert isinstance(model, dict)
+            self.assertIn('name', model)
+            self.assertIn('path', model)
+            self.assertEqual(model['name'], 'test_in_sub.ipynb')
+            self.assertEqual(model['path'], sub_dir)
+            
+            # Make sure the old name is gone
+            self.assertRaises(HTTPError, nm.get_notebook_model, name, path)
+
+    def test_save_notebook_model(self):
+        with TemporaryDirectory() as td:
+            # Test in the root directory
+            # Create a notebook
+            nm = FileNotebookManager(notebook_dir=td)
+            model = nm.create_notebook_model()
+            name = model['name']
+            path = model['path']
+
+            # Get the model with 'content'
+            full_model = nm.get_notebook_model(name, path)
+
+            # Save the notebook
+            model = nm.save_notebook_model(full_model, name, path)
+            assert isinstance(model, dict)
+            self.assertIn('name', model)
+            self.assertIn('path', model)
+            self.assertEqual(model['name'], name)
+            self.assertEqual(model['path'], path)
+
+            # Test in sub-directory
+            # Create a directory and notebook in that directory
+            sub_dir = '/foo/'
+            self.make_dir(nm.notebook_dir, 'foo')
+            model = nm.create_notebook_model(None, sub_dir)
+            name = model['name']
+            path = model['path']
+            model = nm.get_notebook_model(name, path)
+
+            # Change the name in the model for rename
+            model = nm.save_notebook_model(model, name, path)
+            assert isinstance(model, dict)
+            self.assertIn('name', model)
+            self.assertIn('path', model)
+            self.assertEqual(model['name'], 'Untitled0.ipynb')
+            self.assertEqual(model['path'], sub_dir)          
+
+    def test_delete_notebook_model(self):
+        with TemporaryDirectory() as td:
+            # Test in the root directory
+            # Create a notebook
+            nm = FileNotebookManager(notebook_dir=td)
+            model = nm.create_notebook_model()
+            name = model['name']
+            path = model['path']
+            
+            # Delete the notebook
+            nm.delete_notebook_model(name, path)
+            
+            # Check that a 'get' on the deleted notebook raises and error
+            self.assertRaises(HTTPError, nm.get_notebook_model, name, path)
