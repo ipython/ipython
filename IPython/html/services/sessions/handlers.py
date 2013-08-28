15b62b6e57666d79b64cdcbead1067d5eae521d3
Zachary Sailer <zachsailer@gmail.com>
2013-08-28 16:08:55 -0700

    changes after session manager code review

--- IPython/html/services/sessions/handlers.py
+++ IPython/html/services/sessions/handlers.py
@@ -16,12 +16,11 @@ Authors:
 # Imports
 #-----------------------------------------------------------------------------
 
+import json
+
 from tornado import web
-
-from zmq.utils import jsonapi
-
 from IPython.utils.jsonutil import date_default
-from ...base.handlers import IPythonHandler
+from ...base.handlers import IPythonHandler, json_errors
 
 #-----------------------------------------------------------------------------
 # Session web service handlers
@@ -31,63 +30,76 @@ from ...base.handlers import IPythonHandler
 class SessionRootHandler(IPythonHandler):
 
     @web.authenticated
+    @json_errors
     def get(self):
         # Return a list of running sessions
         sm = self.session_manager
-        nbm = self.notebook_manager
-        km = self.kernel_manager
         sessions = sm.list_sessions()
-        self.finish(jsonapi.dumps(sessions))
+        self.finish(json.dumps(sessions, default=date_default))
 
     @web.authenticated
+    @json_errors
     def post(self):
         # Creates a new session 
         #(unless a session already exists for the named nb)
         sm = self.session_manager
         nbm = self.notebook_manager
         km = self.kernel_manager
-        notebook_path = self.get_argument('notebook_path', default=None)
-        name, path = nbm.named_notebook_path(notebook_path)
+        model = self.get_json_body()
+        if model is None:
+            raise HTTPError(400, "No JSON data provided")
+        try:
+            name = model['notebook']['name']
+        except KeyError:
+            raise HTTPError(400, "Missing field in JSON data: name")
+        try:
+            path = model['notebook']['path']
+        except KeyError:
+            raise HTTPError(400, "Missing field in JSON data: path")
         # Check to see if session exists
         if sm.session_exists(name=name, path=path):
             model = sm.get_session(name=name, path=path)
-            kernel_id = model['kernel']['id']
-            km.start_kernel(kernel_id, cwd=nbm.notebook_dir)
         else:
-            session_id = sm.get_session_id()
-            sm.save_session(session_id=session_id, name=name, path=path)
             kernel_id = km.start_kernel(cwd=nbm.notebook_dir)
-            kernel = km.kernel_model(kernel_id, self.ws_url)
-            sm.update_session(session_id, kernel=kernel_id)
-            model = sm.get_session(id=session_id)
-        self.set_header('Location', '{0}kernels/{1}'.format(self.base_kernel_url, kernel_id))
-        self.finish(jsonapi.dumps(model))
+            model = sm.create_session(name=name, path=path, kernel_id=kernel_id, ws_url=self.ws_url)
+        self.set_header('Location', '{0}/api/sessions/{1}'.format(self.base_project_url, model['id']))
+        self.finish(json.dumps(model, default=date_default))
 
 class SessionHandler(IPythonHandler):
 
     SUPPORTED_METHODS = ('GET', 'PATCH', 'DELETE')
 
     @web.authenticated
+    @json_errors
     def get(self, session_id):
         # Returns the JSON model for a single session
         sm = self.session_manager
         model = sm.get_session(id=session_id)
-        self.finish(jsonapi.dumps(model))
+        self.finish(json.dumps(model, default=date_default))
 
     @web.authenticated
+    @json_errors
     def patch(self, session_id):
         # Currently, this handler is strictly for renaming notebooks
         sm = self.session_manager
         nbm = self.notebook_manager
         km = self.kernel_manager
-        data = self.request.body
-        data = jsonapi.loads(self.request.body)
-        name, path = nbm.named_notebook_path(data['notebook_path'])
-        sm.update_session(session_id, name=name)
+        model = self.get_json_body()
+        if model is None:
+            raise HTTPError(400, "No JSON data provided")
+        changes = {}
+        if 'notebook' in model:
+            notebook = model['notebook']
+            if 'name' in notebook:
+                changes['name'] = notebook['name']
+            if 'path' in notebook:
+                changes['path'] = notebook['path']
+        sm.update_session(session_id, **changes)
         model = sm.get_session(id=session_id)
-        self.finish(jsonapi.dumps(model))
+        self.finish(json.dumps(model, default=date_default))
 
     @web.authenticated
+    @json_errors
     def delete(self, session_id):
         # Deletes the session with given session_id
         sm = self.session_manager
@@ -107,9 +119,7 @@ class SessionHandler(IPythonHandler):
 _session_id_regex = r"(?P<session_id>\w+-\w+-\w+-\w+-\w+)"
 
 default_handlers = [
-    (r"api/sessions/%s/" % _session_id_regex, SessionHandler),
     (r"api/sessions/%s" % _session_id_regex, SessionHandler),
-    (r"api/sessions/",  SessionRootHandler),
     (r"api/sessions",  SessionRootHandler)
 ]
 
