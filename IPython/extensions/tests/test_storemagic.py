d88f5f3a9463617f34ef1b3138aa4cb751bacc9d
MinRK <benjaminrk@gmail.com>
2013-08-29 14:59:35 -0700

    Backport PR #4137: Restore autorestore option for storemagic
    It is now StoreMagics.autorestore rather than StoreMagic.autorestore (note the extra s).
    
    Closes #4078

--- IPython/extensions/tests/test_storemagic.py
+++ IPython/extensions/tests/test_storemagic.py
@@ -1,5 +1,6 @@
 import tempfile, os
 
+from IPython.config.loader import Config
 import nose.tools as nt
 
 ip = get_ipython()
@@ -30,3 +31,20 @@ def test_store_restore():
     nt.assert_in(os.path.realpath(tmpd), ip.user_ns['_dh'])
     
     os.rmdir(tmpd)
+
+def test_autorestore():
+    ip.user_ns['foo'] = 95
+    ip.magic('store foo')
+    del ip.user_ns['foo']
+    c = Config()
+    c.StoreMagics.autorestore = False
+    orig_config = ip.config
+    try:
+        ip.config = c
+        ip.extension_manager.reload_extension('storemagic')
+        nt.assert_not_in('foo', ip.user_ns)
+        c.StoreMagics.autorestore = True
+        ip.extension_manager.reload_extension('storemagic')
+        nt.assert_equal(ip.user_ns['foo'], 95)
+    finally:
+        ip.config = orig_config
