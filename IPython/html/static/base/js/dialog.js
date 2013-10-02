3ca82a7e84eaa5b23dadad48a57479e152936a8a
MinRK <benjaminrk@gmail.com>
2013-10-02 15:26:19 -0700

    move edit_metadata to IPython.dialog

--- IPython/html/static/base/js/dialog.js
+++ IPython/html/static/base/js/dialog.js
@@ -60,7 +60,7 @@ IPython.dialog = (function (IPython) {
         });
         
         // destroy dialog on hide, unless explicitly asked not to
-        if (options.destroy == undefined || options.destroy) {
+        if (options.destroy === undefined || options.destroy) {
             dialog.on("hidden", function () {
                 dialog.remove();
             });
@@ -75,10 +75,69 @@ IPython.dialog = (function (IPython) {
         }
         
         return dialog.modal(options);
-    }
+    };
+
+    var edit_metadata = function (md, callback, name) {
+        name = name || "Cell";
+        var error_div = $('<div/>').css('color', 'red');
+        var message = 
+            "Manually edit the JSON below to manipulate the metadata for this " + name + "." +
+            " We recommend putting custom metadata attributes in an appropriately named sub-structure," +
+            " so they don't conflict with those of others.";
+
+        var textarea = $('<textarea/>')
+            .attr('rows', '13')
+            .attr('cols', '80')
+            .attr('name', 'metadata')
+            .text(JSON.stringify(md || {}, null, 2));
+        
+        var dialogform = $('<div/>').attr('title', 'Edit the metadata')
+            .append(
+                $('<form/>').append(
+                    $('<fieldset/>').append(
+                        $('<label/>')
+                        .attr('for','metadata')
+                        .text(message)
+                        )
+                        .append(error_div)
+                        .append($('<br/>'))
+                        .append(textarea)
+                    )
+            );
+        var editor = CodeMirror.fromTextArea(textarea[0], {
+            lineNumbers: true,
+            matchBrackets: true,
+            indentUnit: 2,
+            autoIndent: true,
+            mode: 'application/json',
+        });
+        IPython.dialog.modal({
+            title: "Edit " + name + " Metadata",
+            body: dialogform,
+            buttons: {
+                OK: { class : "btn-primary",
+                    click: function() {
+                        // validate json and set it
+                        var new_md;
+                        try {
+                            new_md = JSON.parse(editor.getValue());
+                        } catch(e) {
+                            console.log(e);
+                            error_div.text('WARNING: Could not save invalid JSON.');
+                            return false;
+                        }
+                        callback(new_md);
+                    }
+                },
+                Cancel: {}
+            }
+        });
+        editor.refresh();
+    };
     
     return {
         modal : modal,
+        edit_metadata : edit_metadata,
     };
 
 }(IPython));
