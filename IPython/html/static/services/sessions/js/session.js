9d9156bbb43c837cda492985ec053466d5bef212
MinRK <benjaminrk@gmail.com>
2013-10-08 17:17:56 -0700

    review pass on multidir js

--- IPython/html/static/services/sessions/js/session.js
+++ IPython/html/static/services/sessions/js/session.js
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------
-//  Copyright (C) 2008-2011  The IPython Development Team
+//  Copyright (C) 2013  The IPython Development Team
 //
 //  Distributed under the terms of the BSD License.  The full license is in
 //  the file COPYING, distributed as part of this software.
@@ -10,45 +10,58 @@
 //============================================================================
 
 var IPython = (function (IPython) {
+    "use strict";
     
-    var Session = function(notebook_name, notebook_path, Notebook){
+    var utils = IPython.utils;
+    
+    var Session = function(notebook_name, notebook_path, notebook){
         this.kernel = null;
         this.id = null;
         this.name = notebook_name;
         this.path = notebook_path;
-        this.notebook = Notebook;
-        this._baseProjectUrl = Notebook.baseProjectUrl() 
+        this.notebook = notebook;
+        this._baseProjectUrl = notebook.baseProjectUrl();
     };
     
     Session.prototype.start = function() {
-        var that = this
-        var notebook = {'notebook':{'name': this.name, 'path': this.path}}
+        var that = this;
+        var model = {
+            notebook : {
+                name : this.name,
+                path : this.path
+            }
+        };
         var settings = {
             processData : false,
             cache : false,
             type : "POST",
-            data: JSON.stringify(notebook),
+            data: JSON.stringify(model),
             dataType : "json",
             success : $.proxy(this.start_kernel, that),
         };
-        var url = this._baseProjectUrl + 'api/sessions';
+        var url = utils.url_path_join(this._baseProjectUrl, 'api/sessions');
         $.ajax(url, settings);
     };
     
     Session.prototype.notebook_rename = function (name, path) {
         this.name = name;
         this.path = path;
-        var notebook = {'notebook':{'name':name, 'path': path}};
+        var model = {
+            notebook : {
+                name : this.name,
+                path : this.path
+            }
+        };
         var settings = {
             processData : false,
             cache : false,
             type : "PATCH",
-            data: JSON.stringify(notebook),
+            data: JSON.stringify(model),
             dataType : "json",
         };
-        var url = this._baseProjectUrl + 'api/sessions/' + this.session_id;
+        var url = utils.url_path_join(this._baseProjectUrl, 'api/sessions', this.session_id);
         $.ajax(url, settings);
-    }
+    };
     
     Session.prototype.delete_session = function() {
         var settings = {
@@ -57,7 +70,7 @@ var IPython = (function (IPython) {
             type : "DELETE",
             dataType : "json",
         };
-        var url = this._baseProjectUrl + 'api/sessions/' + this.session_id;
+        var url = utils.url_path_join(this._baseProjectUrl, 'api/sessions', this.session_id);
         $.ajax(url, settings);
     };
     
@@ -70,7 +83,7 @@ var IPython = (function (IPython) {
     Session.prototype.start_kernel = function (json) {
         this.id = json.id;
         this.kernel_content = json.kernel;
-        var base_url = $('body').data('baseKernelUrl') + "api/kernels";
+        var base_url = utils.url_path_join($('body').data('baseKernelUrl'), "api/kernels");
         this.kernel = new IPython.Kernel(base_url, this.session_id);
         this.kernel._kernel_started(this.kernel_content);
     };
@@ -90,11 +103,10 @@ var IPython = (function (IPython) {
     
 
     Session.prototype.kill_kernel = function() {
-        this.kernel.kill();        
+        this.kernel.kill();
     };
     
     IPython.Session = Session;
-
 
     return IPython;
 
