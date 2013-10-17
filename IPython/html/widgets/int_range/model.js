6e057c18b7c0d5af39b91d7a47a5feb4755b332e
Jonathan Frederic <jdfreder@calpoly.edu>
2013-10-17 06:15:48 +0000

    Add int range widget

---
+++ IPython/html/widgets/int_range/model.js
@@ -0,0 +1,2 @@
+var IntRangeWidgetModel = IPython.WidgetModel.extend({});
+IPython.notebook.widget_manager.register_widget_model('IntRangeWidgetModel', IntRangeWidgetModel);
---
+++ IPython/html/widgets/int_range/widget.py
@@ -0,0 +1,17 @@
+import os
+
+from ..widget import Widget
+from IPython.utils.traitlets import Unicode, Int, Bool
+from IPython.utils.javascript import display_all_js
+
+class IntRangeWidget(Widget):
+    target_name = Unicode('IntRangeWidgetModel')
+    default_view_name = Unicode('IntSliderView')
+    _keys = ['value', 'step', 'max', 'min', 'disabled', 'orientation']
+
+    value = Int(0) 
+    max = Int(100) # Max value
+    min = Int(0) # Min value
+    disabled = Bool(False) # Enable or disable user changes
+    step = Int(1) # Minimum step that the value can take (ignored by some views)
+    orientation = Unicode(u'horizontal') # Vertical or horizontal (ignored by some views)
