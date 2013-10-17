e737024e72b6f6e0a5ab534546f400ec62a00633
Paul Ivanov <pi@berkeley.edu>
2013-10-17 11:04:41 -0700

    repeat test with keyboard shortcut

--- IPython/html/tests/casperjs/test_cases/execute_code_cell.js
+++ IPython/html/tests/casperjs/test_cases/execute_code_cell.js
@@ -21,6 +21,31 @@ casper.notebook_test(function () {
             var output = cell.output_area.outputs[0].text;
             return output;
         })
-        this.test.assertEquals(result, '10\n', 'stdout output matches')
+        this.test.assertEquals(result, '10\n', 'cell execute (using js)')
+    });
+
+
+    // do it again with the keyboard shortcut
+    this.thenEvaluate(function () {
+        var cell = IPython.notebook.get_cell(0);
+        cell.set_text('a=11; print(a)');
+        cell.clear_output()
+        $(document).trigger($.Event('keydown', {which: 13, ctrlKey: true}))
+    });
+
+    this.waitFor(function () {
+        return this.evaluate(function get_output() {
+            var cell = IPython.notebook.get_cell(0);
+            return cell.output_area.outputs.length != 0;
+        })
+    });
+
+    this.then(function () {
+        var result = this.evaluate(function () {
+            var cell = IPython.notebook.get_cell(0);
+            var output = cell.output_area.outputs[0].text;
+            return output;
+        })
+        this.test.assertEquals(result, '11\n', 'cell execute (using ctrl-enter)')
     });
 });
