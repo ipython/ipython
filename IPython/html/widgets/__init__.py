6963d31d947c1d881f43def10d75ce876bc45d39
Jonathan Frederic <jdfreder@calpoly.edu>
2013-10-18 20:37:36 +0000

    Added missing import reference for bool widget

--- IPython/html/widgets/__init__.py
+++ IPython/html/widgets/__init__.py
@@ -1,5 +1,6 @@
 from base import Widget, init_widget_js
 
+from bool import BoolWidget
 from container import ContainerWidget
 from float_range import FloatRangeWidget
 from int_range import IntRangeWidget
