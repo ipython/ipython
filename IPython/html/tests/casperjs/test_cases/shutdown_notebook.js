d18e40bd6dabe4610a161ce831052b82c72e482d
Paul Ivanov <pi@berkeley.edu>
2013-10-22 22:31:01 -0700

    add test for shutdown notebook

---
+++ IPython/html/tests/casperjs/test_cases/shutdown_notebook.js
@@ -0,0 +1,25 @@
+//
+// Test shutdown of a kernel.
+//
+casper.notebook_test(function () {
+    this.test.begin("shutdown tests", 2, function(test) {
+    casper.evaluate(function () {
+        $('#kill_and_exit').click();
+    });
+
+    casper.thenEvaluate(function () {
+        var cell = IPython.notebook.get_cell(0);
+        cell.set_text('a=10; print(a)');
+        cell.execute();
+    });
+
+    // refactor this into  just a get_output(0)
+    casper.then(function () {
+        var result = this.get_output_cell(0);
+        test.assertFalsy(result, "after shutdown: no execution results");
+        test.assertNot(this.kernel_running(), 
+            'after shutdown: IPython.notebook.kernel is false ');
+    });
+});
+
+});
