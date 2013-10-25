7ca41d1336b91f7ff94e3997058a7f20b75fb9d2
Thomas Kluyver <takowl@gmail.com>
2013-10-24 17:09:35 -0700

    Fix kernel.inprocess tests

--- IPython/kernel/inprocess/socket.py
+++ IPython/kernel/inprocess/socket.py
@@ -58,7 +58,7 @@ class DummySocket(HasTraits):
         return self.queue.get_nowait()
 
     def send_multipart(self, msg_parts, flags=0, copy=True, track=False):
-        msg_parts = map(zmq.Message, msg_parts)
+        msg_parts = list(map(zmq.Message, msg_parts))
         self.queue.put_nowait(msg_parts)
         self.message_sent += 1
 
