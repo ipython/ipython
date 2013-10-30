6f019b30997bdf9ae54d19a33bbebb10d71894ed
MinRK <benjaminrk@gmail.com>
2013-10-29 17:48:18 -0700

    add `python -m` entry points for everything

---
+++ IPython/kernel/__main__.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    from IPython.kernel.zmq import kernelapp as app
+    app.launch_new_instance()
---
+++ IPython/terminal/console/__main__.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    from IPython.terminal.console import app
+    app.launch_new_instance()
