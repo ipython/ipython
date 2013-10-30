b5e15cbbfbeaff2840a3dd7a12f11c7f5d0f449b
Jonathan Frederic <jdfreder@calpoly.edu>
2013-10-30 13:48:00 -0700

    Fixed widget imports for Python3

--- IPython/html/widgets/__init__.py
+++ IPython/html/widgets/__init__.py
@@ -1,12 +1,12 @@
-from widget import Widget, init_widget_js
+from .widget import Widget, init_widget_js
 
-from widget_bool import BoolWidget
-from widget_button import ButtonWidget
-from widget_container import ContainerWidget
-from widget_float import FloatWidget
-from widget_float_range import FloatRangeWidget
-from widget_int import IntWidget
-from widget_int_range import IntRangeWidget
-from widget_multicontainer import MulticontainerWidget
-from widget_selection import SelectionWidget
-from widget_string import StringWidget
+from .widget_bool import BoolWidget
+from .widget_button import ButtonWidget
+from .widget_container import ContainerWidget
+from .widget_float import FloatWidget
+from .widget_float_range import FloatRangeWidget
+from .widget_int import IntWidget
+from .widget_int_range import IntRangeWidget
+from .widget_multicontainer import MulticontainerWidget
+from .widget_selection import SelectionWidget
+from .widget_string import StringWidget
