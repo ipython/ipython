414a88dc56626f0edf9a181df81c48e33c8ab632
Thomas Kluyver <takowl@gmail.com>
2013-10-29 17:40:52 -0700

    Better clipboard handling, esp. with pywin32
    Ensure that we get unicode from the clipboard when using pywin32.
    Closes gh-3386
    
    Also catches the empty clipboard case and displays a short error
    message, instead of a huge traceback.

--- IPython/lib/clipboard.py
+++ IPython/lib/clipboard.py
@@ -6,6 +6,9 @@ import subprocess
 from IPython.core.error import TryNext
 import IPython.utils.py3compat as py3compat
 
+class ClipboardEmpty(ValueError):
+    pass
+
 def win32_clipboard_get():
     """ Get the current clipboard's text on Windows.
 
@@ -17,9 +20,16 @@ def win32_clipboard_get():
         raise TryNext("Getting text from the clipboard requires the pywin32 "
                       "extensions: http://sourceforge.net/projects/pywin32/")
     win32clipboard.OpenClipboard()
-    text = win32clipboard.GetClipboardData(win32clipboard.CF_TEXT)
-    # FIXME: convert \r\n to \n?
-    win32clipboard.CloseClipboard()
+    try:
+        text = win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)
+    except TypeError:
+        try:
+            text = win32clipboard.GetClipboardData(win32clipboard.CF_TEXT)
+            text = py3compat.cast_unicode(text, py3compat.DEFAULT_ENCODING)
+        except TypeError:
+            raise ClipboardEmpty
+    finally:
+        win32clipboard.CloseClipboard()
     return text
 
 def osx_clipboard_get():
@@ -41,17 +51,21 @@ def tkinter_clipboard_get():
     implementation that uses that toolkit.
     """
     try:
-        from tkinter import Tk  # Py 3
+        from tkinter import Tk, TclError  # Py 3
     except ImportError:
         try:
-            from Tkinter import Tk  # Py 2
+            from Tkinter import Tk, TclError  # Py 2
         except ImportError:
             raise TryNext("Getting text from the clipboard on this platform "
                           "requires Tkinter.")
     root = Tk()
     root.withdraw()
-    text = root.clipboard_get()
-    root.destroy()
+    try:
+        text = root.clipboard_get()
+    except TclError:
+        raise ClipboardEmpty
+    finally:
+        root.destroy()
     text = py3compat.cast_unicode(text, py3compat.DEFAULT_ENCODING)
     return text
 
