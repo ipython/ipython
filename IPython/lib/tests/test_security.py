2768f7ac50a7b91d984f0f872b647e647d768e93
Thomas Kluyver <takowl@gmail.com>
2013-11-12 10:51:04 -0800

    Add failing (on Py 2) test for passwd_check with unicode arguments

--- IPython/lib/tests/test_security.py
+++ IPython/lib/tests/test_security.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from IPython.lib import passwd
 from IPython.lib.security import passwd_check, salt_len
 import nose.tools as nt
@@ -19,3 +20,7 @@ def test_bad():
     nt.assert_equal(passwd_check(p, 'a:b:c:d'), False)
     nt.assert_equal(passwd_check(p, 'a:b'), False)
 
+def test_passwd_check_unicode():
+    # GH issue #4524
+    phash = u'sha1:9dc18846ca26:6bb62badc41fde529c258a8a7fbe259a91313df8'
+    assert passwd_check(phash, u'mypassword³')
