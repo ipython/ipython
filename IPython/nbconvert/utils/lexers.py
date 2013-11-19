623f73462859c7a6b416a0ad87d7ce07f7263561
chebee7i <chebee7i@gmail.com>
2013-11-18 20:35:04 -0600

    Insert copyright again.

--- IPython/nbconvert/utils/lexers.py
+++ IPython/nbconvert/utils/lexers.py
@@ -24,6 +24,13 @@ This includes:
         to Pygments.
 
 """
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, the IPython Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
 # Standard library
 import re
