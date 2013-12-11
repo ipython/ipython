0d5e3f9a2acdebf5ebce3b4f49641c9481ea3225
westurner <wes.turner@gmail.com>
2013-12-11 07:07:33 -0600

    Fewer string copies, factored time.strftime calls out to self.time
    Also removed extra newlines

--- IPython/extensions/version_information.py
+++ IPython/extensions/version_information.py
@@ -67,7 +67,6 @@ except ImportError:
 
 @magics_class
 class VersionInformation(Magics):
-
     @line_magic
     def version_information(self, line=''):
         """Show information about versions of modules.
@@ -77,12 +76,12 @@ class VersionInformation(Magics):
             %version_information [optional comma-separated list of modules]
 
         """
+        self.time = time.strftime('%a %b %d %H:%M:%S %Y %Z')
+
         self.packages = [("Python", sys.version.replace("\n", "")),
                          ("IPython", IPython.__version__),
                          ("OS", "%s [%s]" % (os.name, sys.platform))]
-
         modules = line.replace(' ', '').split(",")
-
         for module in modules:
             if len(module) > 0:
                 try:
@@ -98,9 +97,7 @@ class VersionInformation(Magics):
                         self.packages.append((module, version))
                     except Exception as e:
                         self.packages.append((module, str(e)))
-
         return self
-
 
     def _repr_json_(self):
         obj = {
@@ -109,21 +106,16 @@ class VersionInformation(Magics):
                 (name, version) in self.packages]}
         return json.dumps(obj)
 
-
     def _repr_html_(self):
-
-        html = "<table>"
-        html += "<tr><th>Software</th><th>Version</th></tr>"
-        for name, version in self.packages:
-            _version = cgi.escape(version)
-            html += "<tr><td>%s</td><td>%s</td></tr>" % (name, _version)
-
-        html += "<tr><td colspan='2'>%s</td></tr>" % \
-                    time.strftime('%a %b %d %H:%M:%S %Y %Z')
-        html += "</table>"
-
-        return html
-
+        def __repr_html(self):
+            yield "<table>"
+            yield "<tr><th>Software</th><th>Version</th></tr>"
+            for name, version in self.packages:
+                _version = cgi.escape(version)
+                yield "<tr><td>%s</td><td>%s</td></tr>" % (name, _version)
+            yield "<tr><td colspan='2'>%s</td></tr>" % self.time
+            yield "</table>"
+        return u''.join(__repr_html(self))
 
     @staticmethod
     def _latex_escape(str_):
@@ -143,32 +135,26 @@ class VersionInformation(Magics):
         }
         return u"".join([CHARS.get(c, c) for c in str_])
 
-
     def _repr_latex_(self):
-
-        latex = r"\begin{tabular}{|l|l|}\hline" + "\n"
-        latex += r"{\bf Software} & {\bf Version} \\ \hline\hline" + "\n"
-        for name, version in self.packages:
-            _version = self._latex_escape(version)
-            latex += r"%s & %s \\ \hline" % (name, _version) + "\n"
-
-        latex += r"\hline \multicolumn{2}{|l|}{%s} \\ \hline" % \
-                    time.strftime('%a %b %d %H:%M:%S %Y %Z') + "\n"
-        latex += r"\end{tabular}" + "\n"
-
-        return latex
-
+        def __repr_latex(self):
+            yield r"\begin{tabular}{|l|l|}\hline\n"
+            yield r"{\bf Software} & {\bf Version} \\ \hline\hline\n"
+            for name, version in self.packages:
+                _version = self._latex_escape(version)
+                yield r"%s & %s \\ \hline\n" % (name, _version)
+            yield r"\hline \multicolumn{2}{|l|}{%s} \\ \hline\n" % self.time
+            yield r"\end{tabular}\n"
+        return r''.join(__repr_latex(self))
 
     def _repr_pretty_(self, pp, cycle):
         def __repr_pretty(self):
             yield ("# Software versions\n"
                    "# -----------------\n")
-            yield "# %s\n" % time.strftime('%a %b %d %H:%M:%S %Y %Z')
+            yield "# %s\n" % self.time
             for name, version in self.packages[:3]:
                 yield u"# %s: %s\n" % (name, version)
             for name, version in self.packages[3:]:
                 yield u"%s==%s\n" % (name, version)
-
         pp.text(u''.join(__repr_pretty(self)))
 
 
