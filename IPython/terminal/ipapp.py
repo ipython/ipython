d4fb1c9b5fa5c598a76b77e6dbc3941868d89ad8
Wieland Hoffmann <themineo@gmail.com>
2013-12-21 14:48:28 +0100

    Complete the docstring for the force_interactive flag

--- IPython/terminal/ipapp.py
+++ IPython/terminal/ipapp.py
@@ -273,7 +273,8 @@ class TerminalIPythonApp(BaseIPythonApplication, InteractiveShellApp):
     # unless the --i flag (App.force_interact) is true.
     force_interact = Bool(False, config=True,
         help="""If a command or file is given via the command-line,
-        e.g. 'ipython foo.py"""
+        e.g. 'ipython foo.py', start an interactive shell after executing the
+        file or command."""
     )
     def _force_interact_changed(self, name, old, new):
         if new:
