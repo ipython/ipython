51396c70caee85deab57d2aae846c20a983b88b0
MinRK <benjaminrk@gmail.com>
2013-12-27 22:31:14 -0800

    fix typo in PR #4744

--- IPython/parallel/tests/test_client.py
+++ IPython/parallel/tests/test_client.py
@@ -333,7 +333,7 @@ class TestClient(ClusterTestCase):
         
         # ensure Hub up to date:
         self.assertEqual(qs['unassigned'], 0)
-        for eid in qs if eid != 'unassigned':
+        for eid in [ eid for eid in qs if eid != 'unassigned' ]:
             self.assertEqual(qs[eid]['tasks'], 0)
             self.assertEqual(qs[eid]['queue'], 0)
     
