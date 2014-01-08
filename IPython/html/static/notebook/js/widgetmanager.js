592da7db387d2baf55b1522b6945c6c39941ae68
Jonathan Frederic <jdfreder@calpoly.edu>
2014-01-08 10:31:04 +0000

    Removed get_kernel method.

--- IPython/html/static/notebook/js/widgetmanager.js
+++ IPython/html/static/notebook/js/widgetmanager.js
@@ -185,7 +185,10 @@
             // for the message.  get_cell callbacks are registered for
             // widget messages, so this block is actually checking to see if the
             // message was triggered by a widget.
-            var kernel = this.get_kernel();
+            var kernel = null;
+            if (this.comm_manager !== null) {
+                kernel = this.comm_manager.kernel;
+            }
             if (kernel !== undefined && kernel !== null) {
                 var callbacks = kernel.get_callbacks_for_msg(msg_id);
                 if (callbacks !== undefined && 
@@ -244,15 +247,6 @@
                 return model;
             }
             return null;
-        };
-
-
-        WidgetManager.prototype.get_kernel = function () {
-            if (this.comm_manager === null) {
-                return null;
-            } else {
-                return this.comm_manager.kernel;
-            }
         };
 
 
