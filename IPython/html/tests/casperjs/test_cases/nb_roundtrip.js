c47d3c9899254671ff52938a9301d04d6476e81c
Paul Ivanov <pi@berkeley.edu>
2014-01-09 14:18:44 -0800

    added svg to/fromJSON tests

--- IPython/html/tests/casperjs/test_cases/nb_roundtrip.js
+++ IPython/html/tests/casperjs/test_cases/nb_roundtrip.js
@@ -17,6 +17,7 @@ mime =  {
     
 var black_dot_jpeg="\"\"\"/9j/4AAQSkZJRgABAQEASABIAAD/2wBDACodICUgGiolIiUvLSoyP2lEPzo6P4FcYUxpmYagnpaG\nk5GovfLNqLPltZGT0v/V5fr/////o8v///////L/////2wBDAS0vLz83P3xERHz/rpOu////////\n////////////////////////////////////////////////////////////wgARCAABAAEDAREA\nAhEBAxEB/8QAFAABAAAAAAAAAAAAAAAAAAAABP/EABQBAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEA\nAhADEAAAARn/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/9oACAEBAAEFAn//xAAUEQEAAAAAAAAAAAAA\nAAAAAAAA/9oACAEDAQE/AX//xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oACAECAQE/AX//xAAUEAEA\nAAAAAAAAAAAAAAAAAAAA/9oACAEBAAY/An//xAAUEAEAAAAAAAAAAAAAAAAAAAAA/9oACAEBAAE/\nIX//2gAMAwEAAgADAAAAEB//xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oACAEDAQE/EH//xAAUEQEA\nAAAAAAAAAAAAAAAAAAAA/9oACAECAQE/EH//xAAUEAEAAAAAAAAAAAAAAAAAAAAA/9oACAEBAAE/\nEH//2Q==\"\"\"";
 var black_dot_png = '\"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAWJLR0QA\\niAUdSAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB94BCRQnOqNu0b4AAAAKSURBVAjXY2AA\\nAAACAAHiIbwzAAAAAElFTkSuQmCC\"';
+var svg = "\"<svg width='1cm' height='1cm' viewBox='0 0 1000 500'><defs><style>rect {fill:red;}; </style></defs><rect id='r1' x='200' y='100' width='600' height='300' /></svg>\""
 
 // helper function to ensure that the short_name is found in the toJSON
 // represetnation, while the original in-memory cell retains its long mimetype
@@ -186,4 +187,25 @@ casper.notebook_test(function () {
     this.then(function ( ) {
         check_output_area.apply(this, ['display_data', ['text', 'jpeg']]);
     });
+    
+    this.then(function() {
+        clear_and_execute(this,
+            "from IPython.core.display import SVG; SVG(" + svg + ")");
+    });
+    
+    this.then(function ( ) {
+        check_output_area.apply(this, ['pyout', ['text', 'svg']]);
+    });
+
+    this.then(function() {
+        clear_and_execute(this,
+            "from IPython.core.display import SVG, display; display(SVG(" + svg + "))");
+    });
+    
+    this.then(function ( ) {
+        check_output_area.apply(this, ['display_data', ['text', 'svg']]);
+    });
+    
+    this.thenEvaluate(function() { IPython.notebook.save_notebook(); });
+ 
 });
