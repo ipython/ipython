de6601a2c21cc6e89751a4a84d8100fc02eb313b
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2014-01-23 19:31:35 +0100

    fix js formatting

--- IPython/html/tests/casperjs/test_cases/kerneljs_test.js
+++ IPython/html/tests/casperjs/test_cases/kerneljs_test.js
@@ -17,13 +17,12 @@ casper.notebook_test(function () {
         });
     });
 
-    this.then(
-        function () {
-            var kernel_info_response=  this.evaluate(function(){
-                return IPython._kernel_info_response;
+    this.then(function () {
+        var kernel_info_response=  this.evaluate(function(){
+            return IPython._kernel_info_response;
         });
         this.test.assertTrue( kernel_info_response.msg_type === 'kernel_info_reply', 'Kernel info request return kernel_info_reply');
         this.test.assertTrue( kernel_info_response.content !== undefined, 'Kernel_info_reply is not undefined');
-    );
+    });
 
 });
