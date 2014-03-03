850d4671acf5a946f66bbaa6ac5f2ce488484d72
Brian E. Granger <ellisonbg@gmail.com>
2014-03-03 14:50:06 -0800

    Adding comment about this fix.

--- IPython/html/tests/launchnotebook.py
+++ IPython/html/tests/launchnotebook.py
@@ -16,6 +16,8 @@ from IPython.utils.tempdir import TemporaryDirectory
 MAX_WAITTIME = 30   # seconds to wait for notebook server to start
 POLL_INTERVAL = 0.1 # time between attempts
 
+# TimeoutError is a builtin on Python 3. This can be removed when we stop
+# supporting Python 2.
 class TimeoutError(Exception):
     pass
 
