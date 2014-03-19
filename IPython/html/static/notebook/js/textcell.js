c916c8beb1de7871edd4d374f4c7cc3507a33465
Paul Ivanov <pi@berkeley.edu>
2014-03-19 10:42:11 -0700

    raw cell placeholder text as one line

--- IPython/html/static/notebook/js/textcell.js
+++ IPython/html/static/notebook/js/textcell.js
@@ -376,8 +376,8 @@ var IPython = (function (IPython) {
     };
 
     RawCell.options_default = {
-        placeholder : "Write raw LaTeX or other formats here, for use with nbconvert.\n" +
-            "It will not be rendered in the notebook.\n" + 
+        placeholder : "Write raw LaTeX or other formats here, for use with nbconvert. " +
+            "It will not be rendered in the notebook. " + 
             "When passing through nbconvert, a Raw Cell's content is added to the output unmodified."
     };
 
