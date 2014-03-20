bfa67c7c411b369b2be9cb78874566b35a08fee2
Jonathan Frederic <jdfreder@calpoly.edu>
2014-03-20 15:46:04 -0700

    Note to self

--- IPython/html/tests/notebook/dualmode_arrows.js
+++ IPython/html/tests/notebook/dualmode_arrows.js
@@ -37,12 +37,14 @@ casper.notebook_test(function () {
         this.validate_notebook_state('click cell 3', 'edit', 3);
         this.trigger_keydown('down');
         this.validate_notebook_state('down at end of notebook', 'edit', 3);
+        // cursor
         this.trigger_keydown('up');
         this.validate_notebook_state('up', 'edit', 2);
         this.click_cell_editor(0);
         this.validate_notebook_state('click 0', 'edit', 0);
         this.trigger_keydown('up');
         this.validate_notebook_state('up at top of notebook', 'edit', 0);
+        // cursor
         this.trigger_keydown('down');
         this.validate_notebook_state('down', 'edit', 1);
     });
