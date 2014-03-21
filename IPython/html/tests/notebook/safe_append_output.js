4212239ef0ea9f78e3710734fdcf7d307e93aad1
Jonathan Frederic <jdfreder@calpoly.edu>
2014-03-21 13:21:07 -0700

    Revert accidental checkin of files from SlimerJS branch

--- IPython/html/tests/notebook/safe_append_output.js
+++ IPython/html/tests/notebook/safe_append_output.js
@@ -25,7 +25,7 @@ casper.notebook_test(function () {
     this.then(function () {
         var output = this.get_output_cell(0);
         this.test.assert(messages.length > 0, "Captured log message");
-        this.test.assertEquals(messages[messages.length-1].splice(0, 26), "Invalid type for image/png", "Logged Invalid type message");
+        this.test.assertEquals(messages[messages.length-1], "Invalid type for image/png 5", "Logged Invalid type message");
         this.test.assertEquals(output['image/png'], undefined, "Non-string png data was stripped");
         this.test.assertEquals(output['text/plain'], '5', "text data is fine");
     });
