a3af51cc16168b6b6d04ab36c2fb82a66ed2d134
Nile Geisinger <nilegeisinger@yahoo.com>
2014-04-14 16:35:46 -0400

    Test for very large cells
    https://github.com/ipython/ipython/pull/5616#issuecomment-40513680

--- IPython/nbconvert/tests/test_nbconvertapp.py
+++ IPython/nbconvert/tests/test_nbconvertapp.py
@@ -15,6 +15,14 @@
 import os
 import glob
 import sys
+import textwrap
+from IPython.nbformat import current
+from IPython.utils import py3compat
+
+if py3compat.PY3:
+    compat_unichar = chr 
+else:
+    compat_unichar = unichr 
 
 from .base import TestsBase
 
@@ -201,6 +209,7 @@ class TestNbConvertApp(TestsBase):
             assert os.path.isfile(u'nb1_análisis.tex')
             assert os.path.isfile(u'nb1_análisis.pdf')
 
+
     def test_cwd_plugin(self):
         """
         Verify that an extension in the cwd can be imported.
@@ -209,3 +218,42 @@ class TestNbConvertApp(TestsBase):
             self.create_empty_notebook(u'empty.ipynb')
             self.call('nbconvert empty --to html --NbConvertApp.writer_class=\'hello.HelloWriter\'')
             assert os.path.isfile(u'hello.txt')
+
+
+    def test_very_long_cells(self):
+        """
+        Torture test that long cells do not cause issues 
+        """
+        lorem_ipsum_text = textwrap.dedent("""\
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec 
+          dignissim, ipsum non facilisis tempus, dui felis tincidunt metus,
+          nec pulvinar neque odio eget risus. Nulla nisi lectus, cursus
+          suscipit interdum at, ultrices sit amet orci. Mauris facilisis
+          imperdiet elit, vitae scelerisque ipsum dignissim non. Integer
+          consequat malesuada neque sit amet pulvinar. Curabitur pretium
+          ut turpis eget aliquet. Maecenas sagittis lacus sed lectus
+          volutpat, eu adipiscing purus pulvinar. Maecenas consequat 
+          luctus urna, eget cursus quam mollis a. Aliquam vitae ornare
+          erat, non hendrerit urna. Sed eu diam nec massa egestas pharetra
+          at nec tellus. Fusce feugiat lacus quis urna sollicitudin volutpat. 
+          Quisque at sapien non nibh feugiat tempus ac ultricies purus.
+           """)
+        lorem_ipsum_text = lorem_ipsum_text.replace("\n"," ") + "\n\n"
+        large_lorem_ipsum_text = "".join([lorem_ipsum_text]*3000)
+
+        notebook_name = "lorem_ipsum_long.ipynb"
+        tex_name = "lorem_ipsum_long.tex"
+        with self.create_temp_cwd([]):
+            nb = current.new_notebook(
+                worksheets=[
+                    current.new_worksheet(cells=[
+                        current.new_text_cell('markdown',source=large_lorem_ipsum_text)
+                    ])
+                ]
+            )
+            with open(notebook_name, 'w') as f: current.write(nb, f, 'ipynb')
+            self.call('nbconvert --to latex --log-level 0 ' + 
+                      os.path.join(notebook_name))
+            assert os.path.isfile(tex_name)
+
+
