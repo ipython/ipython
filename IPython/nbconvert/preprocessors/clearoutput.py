a9860b18e49991b14375370a45f441dde2978e05
Julia Evans <julia@jvns.ca>
2014-04-16 10:46:54 -0400

    Fix copyright on clearoutput.py

--- IPython/nbconvert/preprocessors/clearoutput.py
+++ IPython/nbconvert/preprocessors/clearoutput.py
@@ -1,12 +1,7 @@
 """Module containing a preprocessor that removes the outputs from code cells"""
 
-#-----------------------------------------------------------------------------
-# Copyright (c) 2013, the IPython Development Team.
-#
+# Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
 # Imports
