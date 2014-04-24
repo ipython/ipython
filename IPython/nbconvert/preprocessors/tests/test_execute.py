8df1a1233e13a80d3e0ef2dfec8af15394e77b66
Julia Evans <julia@stripe.com>
2014-04-24 16:18:17 -0400

    Start adding tests from runipy

--- IPython/nbconvert/preprocessors/tests/test_execute.py
+++ IPython/nbconvert/preprocessors/tests/test_execute.py
@@ -9,6 +9,8 @@ Module with tests for the execute preprocessor.
 # Imports
 #-----------------------------------------------------------------------------
 import copy
+import os
+import re
 
 from IPython.nbformat import current as nbformat
 
@@ -23,6 +25,28 @@ from ..execute import ExecutePreprocessor
 class TestExecute(PreprocessorTestsBase):
     """Contains test functions for execute.py"""
 
+    def prepare_cell(self, cell):
+        cell = dict(cell)
+        if 'metadata' in cell:
+            del cell['metadata']
+        if 'text' in cell:
+            cell['text'] = re.sub('0x[0-9a-f]{7,9}', '<HEXADDR>', cell['text'])
+        return cell
+
+
+    def assert_notebooks_equal(self, expected, actual):
+        expected_cells = expected['worksheets'][0]['cells']
+        actual_cells = actual['worksheets'][0]['cells']
+        assert len(expected_cells) == len(actual_cells)
+
+        # TODO: what does this code do?
+        for expected_out, actual_out in zip(expected_cells, actual_cells):
+            for k in set(expected_out).union(actual_out):
+                if k == 'outputs':
+                    self.assertEquals(len(expected_out[k]), len(actual_out[k]))
+                    for e, a in zip(expected_out[k], actual_out[k]):
+                        assert self.prepare_cell(e) == self.prepare_cell(a)
+
 
     def build_preprocessor(self):
         """Make an instance of a preprocessor"""
@@ -30,16 +54,25 @@ class TestExecute(PreprocessorTestsBase):
         preprocessor.enabled = True
         return preprocessor
 
+
     def test_constructor(self):
         """Can a ExecutePreprocessor be constructed?"""
         self.build_preprocessor()
 
-    def test_correct_output(self):
-        """Test that ExecutePreprocessor evaluates a cell to the right thing"""
-        nb = self.build_notebook()
-        res = self.build_resources()
-        nb.worksheets[0].cells[0].input = "print 'hi!'"
-        preprocessor = self.build_preprocessor()
-        nb, res = preprocessor(nb, res)
-        expected_outputs = [{'output_type': 'stream', 'stream': 'stdout', 'text': 'hi!\n'}] 
-        assert nb.worksheets[0].cells[0].outputs == expected_outputs
+
+    def test_run_notebooks(self):
+        """Runs a series of test notebooks and compares them to their actual output"""
+        current_dir = os.path.dirname(__file__)
+        input_files = os.listdir(os.path.join(current_dir, 'input'))
+        for filename in input_files:
+            if not filename.endswith(".ipynb"):
+                continue
+            with open(os.path.join(current_dir, 'input', filename)) as f:
+                input_nb = nbformat.read(f, 'ipynb')
+            with open(os.path.join(current_dir, 'expected', filename)) as f:
+                expected_nb = nbformat.read(f, 'ipynb')
+            res = self.build_resources()
+            preprocessor = self.build_preprocessor()
+            output_nb, _ = preprocessor(input_nb, res)
+            self.assert_notebooks_equal(output_nb, expected_nb)
+
