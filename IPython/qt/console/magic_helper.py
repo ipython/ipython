fa6ad35daea1a9ec7951d04c14920263d208db6d
Dimitry Kloper <kloper@users.sf.net>
2014-05-01 14:15:42 +0300

    Updated file headers according to IPython Copyright Policy

--- IPython/qt/console/magic_helper.py
+++ IPython/qt/console/magic_helper.py
@@ -4,8 +4,10 @@
 Authors:
 
 * Dimitry Kloper
-
 """
+
+# Copyright (c) IPython Development Team.
+# Distributed under the terms of the Modified BSD License. 
 
 #-----------------------------------------------------------------------------
 # Imports
--- IPython/qt/console/mainwindow.py
+++ IPython/qt/console/mainwindow.py
@@ -15,6 +15,10 @@ Authors:
 
 """
 
+# Copyright (c) IPython Development Team.
+# Distributed under the terms of the Modified BSD License. 
+
+
 #-----------------------------------------------------------------------------
 # Imports
 #-----------------------------------------------------------------------------
