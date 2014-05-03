ff9c8db9a526fb3ba64c48741e0834a3c999fe5b
scottsievert <sieve121@umn.edu>
2014-05-02 21:17:01 -0500

    MATLAB-like draw now.

--- IPython/core/pylabtools.py
+++ IPython/core/pylabtools.py
@@ -379,3 +379,50 @@ def configure_inline_support(shell, backend):
     # Setup the default figure format
     select_figure_formats(shell, cfg.figure_formats, **cfg.print_figure_kwargs)
 
+def drawnow(draw_fig, show_once=False, *argv, **kwargs):
+    """A function to refresh the current figure.
+
+    Parameters
+    ----------
+    draw_fig : callable
+               the function that draws the figure you want to update
+    *argv    : any
+               the list of parameters to pass ``draw_fig()``
+    **kwargs : any
+               the keywords to pass to ``draw_fig()``
+    show_once, optional : bool, default: False. 
+               If True, will call show() instead of draw(). 
+
+    Limitations
+    -----------
+    - If two figures open and focus moved between figures, then other figure
+      gets cleared.
+    - Occaisonally ignores Ctrl-C.
+
+    Usage Example
+    -------------
+      >>> def draw_fig_real():
+      >>>     imshow(z, interpolation='nearest')
+      >>>     colorbar()
+      >>> N = 16
+      >>> z = zeros((N,N))
+
+      >>> ion()
+      >>> for i in arange(N*N):
+      >>>     z.flat[i] = 0
+      >>>     drawnow(draw_fig_real)
+    """
+    from matplotlib.pyplot import clf, show, draw
+    # get the kwargs
+    kw = dict()
+    kw.update(kwargs)
+
+    # draw current figure
+    clf()
+    draw_fig(*argv, **kw)
+
+    # should we show once? show() causes many windows to open, draw() doesn't
+    if show_once:
+        show()
+    else:
+        draw()
