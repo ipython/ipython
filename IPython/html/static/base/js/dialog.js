76b82228e06979f33149958c27065e3b849d9968
Jonathan Frederic <jon.freder@gmail.com>
2014-05-15 12:35:26 -0700

    Rebase cleanup, comments and nesting

--- IPython/html/static/base/js/dialog.js
+++ IPython/html/static/base/js/dialog.js
@@ -15,24 +15,6 @@ IPython.dialog = (function (IPython) {
     "use strict";
     
     var modal = function (options) {
-        // <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        //   <div class="modal-dialog">
-        //     <div class="modal-content">
-        //       <div class="modal-header">
-        //         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        //         <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-        //       </div>
-        //       <div class="modal-body">
-        //         ...
-        //       </div>
-        //       <div class="modal-footer">
-        //         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        //         <button type="button" class="btn btn-primary">Save changes</button>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
-
         var modal = $("<div/>")
             .addClass("modal")
             .addClass("fade")
