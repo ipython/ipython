2778607f29b6e0b06770943d4c47a7c5736356e5
Jonathan Frederic <jdfreder@calpoly.edu>
2014-06-11 14:34:32 -0700

    Fixed global IPython namespace.

--- IPython/html/static/base/js/namespace.js
+++ IPython/html/static/base/js/namespace.js
@@ -1,8 +1,8 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+var IPython = IPython || {};
 define([], function(){
-    var IPython = IPython || {};
     IPython.version = "3.0.0-dev";
     return IPython;
 });
