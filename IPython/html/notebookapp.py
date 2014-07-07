8449bcd6aec3b04e2283e17d3cfeef5f5f518014
MinRK <benjaminrk@gmail.com>
2014-07-06 23:52:57 -0500

    Backport PR #6061: make CORS configurable
    allows setting CORS headers.
    
    - allow_origin sets Access-Control-Allow-Origin directly
    - allow_origin_pat allows setting Access-Control-Allow-Origin via regular expression, since the header spec itself doesn’t support nontrivial rules.
    - allow_credentials sets Access-Control-Allow-Credentials: true
    ...

--- IPython/html/notebookapp.py
+++ IPython/html/notebookapp.py
@@ -24,6 +24,7 @@ import json
 import logging
 import os
 import random
+import re
 import select
 import signal
 import socket
@@ -340,7 +341,33 @@ class NotebookApp(BaseIPythonApplication):
             self.file_to_run = base
             self.notebook_dir = path
 
-    # Network related information.
+    # Network related information
+
+    allow_origin = Unicode('', config=True,
+        help="""Set the Access-Control-Allow-Origin header
+
+        Use '*' to allow any origin to access your server.
+
+        Takes precedence over allow_origin_pat.
+        """
+    )
+
+    allow_origin_pat = Unicode('', config=True,
+        help="""Use a regular expression for the Access-Control-Allow-Origin header
+
+        Requests from an origin matching the expression will get replies with:
+
+            Access-Control-Allow-Origin: origin
+
+        where `origin` is the origin of the request.
+
+        Ignored if allow_origin is set.
+        """
+    )
+
+    allow_credentials = Bool(False, config=True,
+        help="Set the Access-Control-Allow-Credentials: true header"
+    )
 
     ip = Unicode('localhost', config=True,
         help="The IP address the notebook server will listen on."
@@ -600,11 +627,15 @@ class NotebookApp(BaseIPythonApplication):
             logger = logging.getLogger('tornado.%s' % name)
             logger.parent = self.log
             logger.setLevel(self.log.level)
-    
+
     def init_webapp(self):
         """initialize tornado webapp and httpserver"""
+        self.webapp_settings['allow_origin'] = self.allow_origin
+        self.webapp_settings['allow_origin_pat'] = re.compile(self.allow_origin_pat)
+        self.webapp_settings['allow_credentials'] = self.allow_credentials
+
         self.web_app = NotebookWebApplication(
-            self, self.kernel_manager, self.notebook_manager, 
+            self, self.kernel_manager, self.notebook_manager,
             self.cluster_manager, self.session_manager,
             self.log, self.base_url, self.webapp_settings,
             self.jinja_environment_options
