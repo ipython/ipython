9eddd8307dc536c56c850922ee6153da8c7a2d93
Thomas Kluyver <takowl@gmail.com>
2014-07-12 17:28:55 -0500

    Start kernel according to loaded notebook metadata

--- IPython/html/static/notebook/js/notebook.js
+++ IPython/html/static/notebook/js/notebook.js
@@ -2231,7 +2231,16 @@ define([
         // Create the session after the notebook is completely loaded to prevent
         // code execution upon loading, which is a security risk.
         if (this.session === null) {
-            this.start_session();
+            var kernelspec = this.metadata.kernelspec || {};
+            var kernel_name = kernelspec.name || this.default_kernel_name;
+            
+            // If we don't already know about this kernel spec, store its info
+            var kernelspecs = IPython.kernelselector.kernelspecs;
+            if (kernelspec !== {} && !(kernel_name in kernelspecs)) {
+                kernelspecs[kernel_name] = kernelspec;
+            }
+
+            this.start_session(kernel_name);
         }
         // load our checkpoint list
         this.list_checkpoints();
