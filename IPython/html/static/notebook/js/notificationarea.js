e7631b44e702c71d833821415133c3c88a0303f5
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2014-07-16 19:35:03 +0200

    remove uselessrandom id, and move styling to css

--- IPython/html/static/notebook/js/notificationarea.js
+++ IPython/html/static/notebook/js/notificationarea.js
@@ -32,13 +32,10 @@ define([
     };
 
     NotificationArea.prototype.temp_message = function (msg, timeout, css_class) {
-        var uuid = utils.uuid();
         if( css_class == 'danger') {css_class = 'ui-state-error';}
         if( css_class == 'warning') {css_class = 'ui-state-highlight';}
         var tdiv = $('<div>')
-            .attr('id',uuid)
             .addClass('notification_widget ui-widget ui-widget-content ui-corner-all')
-            .addClass('border-box-sizing')
             .addClass(css_class)
             .hide()
             .text(msg);
