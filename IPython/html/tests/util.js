226af0ea5ecc84f0e841020a5149192dc1ca067e
MinRK <benjaminrk@gmail.com>
2014-07-22 15:43:39 -0700

    use global events in js tests

--- IPython/html/tests/util.js
+++ IPython/html/tests/util.js
@@ -26,11 +26,14 @@ casper.open_new_notebook = function () {
     this.waitFor(this.kernel_running);
     // track the IPython busy/idle state
     this.thenEvaluate(function () {
-        IPython.events.on('status_idle.Kernel',function () {
-            IPython._status = 'idle';
-        });
-        IPython.events.on('status_busy.Kernel',function () {
-            IPython._status = 'busy';
+        require(['base/js/namespace', 'base/js/events'], function (IPython, events) {
+        
+            events.on('status_idle.Kernel',function () {
+                IPython._status = 'idle';
+            });
+            events.on('status_busy.Kernel',function () {
+                IPython._status = 'busy';
+            });
         });
     });
 
