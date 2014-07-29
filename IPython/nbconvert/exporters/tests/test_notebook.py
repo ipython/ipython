d7aa64bd9bb60bb2f02e3cc6886d93b2c261968b
MinRK <benjaminrk@gmail.com>
2014-07-28 21:04:30 -0700

    support downgrading notebooks with nbconvert
    Use:
    
        ipython nbconvert --to notebook --nbformat 3 notebook.ipynb
    
    to write `notebook.v3.ipynb`
    
    Adds `output_suffix` resource, inserted before file ext.
    Suffix is used if `--output` is not specified.
    
    Default is `.nbconvert`, to prevent `--to notebook` from clobbering existing notebooks by default.

--- IPython/nbconvert/exporters/tests/test_notebook.py
+++ IPython/nbconvert/exporters/tests/test_notebook.py
@@ -3,6 +3,8 @@
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import json
+
 from .base import ExportersTestsBase
 from ..notebook import NotebookExporter
 
@@ -17,6 +19,18 @@ class TestNotebookExporter(ExportersTestsBase):
         """
         with open(self._get_notebook()) as f:
             file_contents = f.read()
-        (output, resources) = NotebookExporter().from_filename(self._get_notebook())
+        (output, resources) = self.exporter_class().from_filename(self._get_notebook())
         assert len(output) > 0
-        assert output == file_contents
+        self.assertEqual(output, file_contents)
+
+    def test_downgrade_3(self):
+        exporter = self.exporter_class(nbformat_version=3)
+        (output, resources) = exporter.from_filename(self._get_notebook())
+        nb = json.loads(output)
+        self.assertEqual(nb['nbformat'], 3)
+
+    def test_downgrade_2(self):
+        exporter = self.exporter_class(nbformat_version=2)
+        (output, resources) = exporter.from_filename(self._get_notebook())
+        nb = json.loads(output)
+        self.assertEqual(nb['nbformat'], 2)
