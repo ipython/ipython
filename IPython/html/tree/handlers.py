be57e111ba4a360b5974aa8a0d1cd4b87d18f135
MinRK <benjaminrk@gmail.com>
2014-08-18 11:24:18 -0700

    Backport PR #6333: allow slash in root->tree redirect
    - before #6117, only `/base_url/` worked
    - after #6117, only `/base_url` worked
    - after this PR, both should work.
    
    closes #6329

--- IPython/html/tree/handlers.py
+++ IPython/html/tree/handlers.py
@@ -99,5 +99,5 @@ default_handlers = [
     (r"/tree%s" % notebook_path_regex, TreeHandler),
     (r"/tree%s" % path_regex, TreeHandler),
     (r"/tree", TreeHandler),
-    (r"", TreeRedirectHandler),
+    (r"/?", TreeRedirectHandler),
     ]
