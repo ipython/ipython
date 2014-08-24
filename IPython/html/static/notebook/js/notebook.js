4fb32b436d6d3a4be8a2cd8f6e35b01f2c38f971
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2014-08-24 12:24:58 +0200

    load the per kernel custom.js and .css

--- IPython/html/static/notebook/js/notebook.js
+++ IPython/html/static/notebook/js/notebook.js
@@ -1808,7 +1808,14 @@ define([
         // Trigger an event changing the kernel spec - this will set the default
         // codemirror mode
         if (this.metadata.kernelspec !== undefined) {
-            this.events.trigger('spec_changed.Kernel', this.metadata.kernelspec);
+            // TODO shoudl probably not trigger here, 
+            // should call the kernel selector, or custom.{js|css} not loaded.
+            if(this.kernel_selector){
+                // technically not perfect, we should check that the kernelspec matches
+                this.kernel_selector.change_kernel(this.metadata.kernelspec.name);
+            } else {
+                console.log('do not have handle on kernnel_selector');
+            }
         }
         
         // Only handle 1 worksheet for now.
@@ -2392,6 +2399,10 @@ define([
         // now that we're fully loaded, it is safe to restore save functionality
         delete(this.save_notebook);
         this.events.trigger('notebook_loaded.Notebook');
+    };
+
+    Notebook.prototype.set_kernelselector = function(k_selector){
+        this.kernel_selector = k_selector;
     };
 
     /**
