e5927b9bdf88ab4401977c0bbebcd2d6a5ea3907
MinRK <benjaminrk@gmail.com>
2014-08-27 11:19:18 -0700

    add assert_big_text_equal
    for better output when large strings differ

--- IPython/nbconvert/exporters/tests/test_notebook.py
+++ IPython/nbconvert/exporters/tests/test_notebook.py
@@ -8,6 +8,8 @@ import json
 from .base import ExportersTestsBase
 from ..notebook import NotebookExporter
 
+from IPython.testing.tools import assert_big_text_equal
+
 class TestNotebookExporter(ExportersTestsBase):
     """Contains test functions for notebook.py"""
 
@@ -21,7 +23,7 @@ class TestNotebookExporter(ExportersTestsBase):
             file_contents = f.read()
         (output, resources) = self.exporter_class().from_filename(self._get_notebook())
         assert len(output) > 0
-        self.assertEqual(output, file_contents)
+        assert_big_text_equal(output, file_contents)
 
     def test_downgrade_3(self):
         exporter = self.exporter_class(nbformat_version=3)
