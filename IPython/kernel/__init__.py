e34e96ed0e7fa7f7d97fc4206880945d2d80891e
Thomas Kluyver <takowl@gmail.com>
2014-09-10 16:40:47 -0700

    Create run_kernel context manager

--- IPython/kernel/__init__.py
+++ IPython/kernel/__init__.py
@@ -6,6 +6,6 @@ from . import zmq
 from .connect import *
 from .launcher import *
 from .client import KernelClient
-from .manager import KernelManager
+from .manager import KernelManager, run_kernel
 from .blocking import BlockingKernelClient
 from .multikernelmanager import MultiKernelManager
