93ff19c4346962c3d8c8d16149f128eede8fd8fd
Thomas Kluyver <takowl@gmail.com>
2014-10-03 16:20:13 -0700

    Initial REST API for terminals

--- IPython/html/terminal/__init__.py
+++ IPython/html/terminal/__init__.py
@@ -1,6 +1,7 @@
 import os
 from terminado import NamedTermManager
 from .handlers import TerminalHandler, NewTerminalHandler, TermSocket
+from . import api_handlers
 
 def initialize(webapp):
     shell = os.environ.get('SHELL', 'sh')
@@ -10,5 +11,7 @@ def initialize(webapp):
         (r"/terminals/(\w+)", TerminalHandler),
         (r"/terminals/websocket/(\w+)", TermSocket,
              {'term_manager': webapp.terminal_manager}),
+        (r"/api/terminals", api_handlers.TerminalRootHandler),
+        (r"/api/terminals/(\w+)", api_handlers.TerminalHandler),
     ]
     webapp.add_handlers(".*$", handlers)
