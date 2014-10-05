beded84b43464f8ec338f89949fb4a722a02b49d
bsvh <me@bsvh.me>
2014-10-05 10:58:12 -0400

    fix ls_alias for NetBSD as well

--- IPython/core/alias.py
+++ IPython/core/alias.py
@@ -69,8 +69,8 @@ def default_aliases():
                           # things which are executable
                           ('lx', 'ls -F -o --color %l | grep ^-..x'),
                           ]
-        elif sys.platform.startswith('openbsd'):
-            # OpenBSD 
+        elif sys.platform.startswith('openbsd') or sys.platform.startswith('netbsd'):
+            # OpenBSD, NetBSD 
             ls_aliases = [('ls', 'ls -F'),
                           # long ls
                           ('ll', 'ls -F -l'),
