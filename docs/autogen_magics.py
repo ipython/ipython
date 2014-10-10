800fd305273b32cce5a7e827604f907e69a4a546
Thomas Kluyver <takowl@gmail.com>
2014-10-10 14:58:22 -0700

    Various improvements to docs infrastructure for magics

--- docs/autogen_magics.py
+++ docs/autogen_magics.py
@@ -11,34 +11,54 @@ def isalias(name):
 line_magics = magic_docs['line']
 cell_magics = magic_docs['cell']
 
+def _strip_underline(line):
+    chars = set(line.strip())
+    if len(chars) == 1 and ('-' in chars or '=' in chars):
+        return ""
+    else:
+        return line
+
+def format_docstring(docstring):
+    docstring = indent(dedent(docstring))
+    # Sphinx complains if indented bits have rst headings in, so strip out
+    # any underlines in the docstring.
+    lines = [_strip_underline(l) for l in docstring.splitlines()]
+    return "\n".join(lines)
+
 output = [
 "Line magics",
 "===========",
 "",
 ]
 
-for name, docstring in sorted(line_magics.items()):
+# Case insensitive sort by name
+def sortkey(s): return s[0].lower()
+
+for name, docstring in sorted(line_magics.items(), key=sortkey):
     if isalias(name):
         # Aliases are magics, but shouldn't be documented here
         continue
-    output.extend([".. magic:: %{}".format(name),
+    output.extend([".. magic:: {}".format(name),
                    "",
-                   indent(dedent(docstring)),
+                   format_docstring(docstring),
                    ""])
 
 output.extend([
-"Cell magics"
-"==========="
+"Cell magics",
+"===========",
 "",
 ])
 
-for name, docstring in sorted(cell_magics.items()):
+for name, docstring in sorted(cell_magics.items(), key=sortkey):
+    if name == "!":
+        # Special case - don't encourage people to use %%!
+        continue
     if docstring == line_magics.get(name, 'QQQP'):
         # Don't redocument line magics that double as cell magics
         continue
-    output.extend([".. cellmagic:: %%{}".format(name),
+    output.extend([".. cellmagic:: {}".format(name),
                    "",
-                   indent(dedent(docstring)),
+                   format_docstring(docstring),
                    ""])
 
 with open("source/interactive/magics-generated.txt", "w") as f:
