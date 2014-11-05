e448416b7f5328f779a3282ef254514e05682e2f
Scott Sanderson <scoutoss@gmail.com>
2014-11-05 00:19:17 -0500

    DEV: Support for configurable list of extra jinja template directories.

--- IPython/html/__init__.py
+++ IPython/html/__init__.py
@@ -3,6 +3,8 @@
 import os
 # Packagers: modify this line if you store the notebook static files elsewhere
 DEFAULT_STATIC_FILES_PATH = os.path.join(os.path.dirname(__file__), "static")
+# Packagers: modify this line if you store the notebook template files elsewhere
+DEFAULT_TEMPLATE_FILES_PATH = os.path.join(os.path.dirname(__file__), "templates")
 
 del os
 
