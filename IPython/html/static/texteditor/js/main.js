82765a7db97b614b5c78e6edeb2a2b9b61f2f8e9
Thomas Kluyver <takowl@gmail.com>
2014-11-05 16:11:29 -0800

    Loading a file works

--- IPython/html/static/texteditor/js/main.js
+++ IPython/html/static/texteditor/js/main.js
@@ -5,19 +5,56 @@ require([
     'jquery',
     'base/js/utils',
     'base/js/page',
+    'contents',
     'codemirror/lib/codemirror',
+    'codemirror/mode/meta',
     'custom/custom',
 ], function(
     $, 
     utils,
     page,
+    contents,
     CodeMirror
     ){
     page = new page.Page();
 
     var base_url = utils.get_body_data('baseUrl');
-    var cm_instance = CodeMirror($('#texteditor-container')[0]);
-    
-    page.show();
+    contents = new contents.Contents({base_url: base_url});
 
+    var file_path = utils.get_body_data('filePath');
+    var ix = file_path.lastIndexOf("/");
+    var dir_path, basename;
+    if (ix == -1) {
+        dir_path = '';
+        basename = file_path;
+    } else {
+        dir_path = file_path.substring(0, ix);
+        basename = file_path.substring(ix);
+    }
+    contents.load(dir_path, basename, {
+        success: function(model) {
+            page.show();
+            if (model.type === "file" && model.format === "text") {
+                console.log(modeinfo);
+                var cm = CodeMirror($('#texteditor-container')[0], {
+                    value: model.content,
+                });
+                
+                // Find and load the highlighting mode
+                var modeinfo = CodeMirror.findModeByMIME(model.mimetype);
+                if (modeinfo) {
+                    utils.requireCodeMirrorMode(modeinfo.mode, function() {
+                        cm.setOption('mode', modeinfo.mode);
+                    });
+                }
+                
+                // Attach to document for debugging
+                document.cm_instance = cm;
+            } else {
+                $('#texteditor-container').append(
+                    $('<p/>').text(dir_path + " is not a text file")
+                );
+            }
+        }
+    });
 });
--- IPython/html/texteditor/handlers.py
+++ IPython/html/texteditor/handlers.py
@@ -6,12 +6,21 @@
 
 from tornado import web
 from ..base.handlers import IPythonHandler, file_path_regex
+from ..utils import url_escape
 
 class EditorHandler(IPythonHandler):
     """Render the terminal interface."""
     @web.authenticated
     def get(self, path, name):
-        self.write(self.render_template('texteditor.html'))
+        path = path.strip('/')
+        if not self.contents_manager.file_exists(name, path):
+            raise web.HTTPError(404, u'File does not exist: %s/%s' % (path, name))
+
+        file_path = url_escape(path) + "/" + url_escape(name)
+        self.write(self.render_template('texteditor.html',
+            file_path=file_path,
+            )
+        )
 
 default_handlers = [
     (r"/texteditor%s" % file_path_regex, EditorHandler),
