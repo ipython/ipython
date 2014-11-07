5d06103df7b0c5b21fa4601d5d6ccedfa117d04f
AnneTheAgile <annemoroney@alum.mit.edu>
2014-11-07 16:04:44 -0500

    [NBConvert] Bug Fix: PDF from Latex for Windows/Py2 change to or from and.

--- IPython/nbconvert/exporters/pdf.py
+++ IPython/nbconvert/exporters/pdf.py
@@ -59,7 +59,7 @@ class PDFExporter(LatexExporter):
         command = [c.format(filename=filename) for c in command_list]
         #In windows and python 2.x there is a bug in subprocess.Popen and
         # unicode commands are not supported
-        if sys.platform == 'win32' and sys.version_info < (3,0):
+        if sys.platform == 'win32' or sys.version_info < (3,0):
             #We must use cp1252 encoding for calling subprocess.Popen
             #Note that sys.stdin.encoding and encoding.DEFAULT_ENCODING
             # could be different (cp437 in case of dos console)
