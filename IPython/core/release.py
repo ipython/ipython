af67011952ba70a5c385743e47d22e032b799197
Min RK <benjaminrk@gmail.com>
2014-11-12 14:54:19 -0800

    make 2.3.1-maint

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -20,8 +20,8 @@ name = 'ipython'
 # release.  'dev' as a _version_extra string means this is a development
 # version
 _version_major = 2
-_version_minor = 4
-_version_patch = 0
+_version_minor = 3
+_version_patch = 1
 _version_extra = 'maint'
 # _version_extra = 'rc1'
 #_version_extra = ''  # Uncomment this for full releases
