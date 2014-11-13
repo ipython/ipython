e5af162da3101783aae45963ad6be7a93e87233d
Thomas Kluyver <takowl@gmail.com>
2014-11-13 11:59:15 -0800

    Add es6-promise to package_data

--- setupbase.py
+++ setupbase.py
@@ -150,6 +150,7 @@ def find_package_data():
         pjoin(components, "bootstrap", "js", "bootstrap.min.js"),
         pjoin(components, "bootstrap-tour", "build", "css", "bootstrap-tour.min.css"),
         pjoin(components, "bootstrap-tour", "build", "js", "bootstrap-tour.min.js"),
+        pjoin(components, "es6-promise", "*.js"),
         pjoin(components, "font-awesome", "fonts", "*.*"),
         pjoin(components, "google-caja", "html-css-sanitizer-minified.js"),
         pjoin(components, "highlight.js", "build", "highlight.pack.js"),
