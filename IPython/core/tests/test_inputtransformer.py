4f186a3a9f6d378ea4693e53fa00e450a774c4e0
Pascal Bugnion <pascal@bugnion.org>
2014-11-22 17:01:39 +0000

    Fix input transformer bug when pasting ipython promts.
    Fixes a SyntaxError that arose when pasting code containing the IPython
    continuation prompt without preceding spaces, eg.:
    
    In [1]: if True:
    ...:     print "No spaces before the '...:' prompt."
    
    This issue was brought up in
    "http://stackoverflow.com/questions/26297356/how-can-i-paste-the-ipython-output-in-ipython#comment41270724_26297356"
    
    This commit adds a test for the bug, and fixes the regular expression
    that strips continuation prompts.

--- IPython/core/tests/test_inputtransformer.py
+++ IPython/core/tests/test_inputtransformer.py
@@ -228,6 +228,11 @@ syntax_ml = \
           ('    ...:     print i','    print i'),
           ('    ...: ', ''),
           ],
+         [('In [24]: for i in range(10):','for i in range(10):'),
+          # Sometimes whitespace preceding '...' has been removed (issue #6674)
+          ('...:     print i','    print i'),
+          ('...: ', ''),
+          ],
          [('In [2]: a="""','a="""'),
           ('   ...: 123"""','123"""'),
           ],
