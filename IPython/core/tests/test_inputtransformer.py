1ea1881226851477460cb436dee51d4b515015ca
Pascal Bugnion <pascal@bugnion.org>
2014-11-22 20:17:02 +0000

    Fixed whitespace bug when pasting IPython continuation prompt.
    Fixes a SyntaxError that arose when pasting code containing the IPython
    continuation prompt without a trailing white space, such as:
    
    In[1]: if True:
       ...:    print "No space after ':' on next line."
       ...:
    
    This might arise if the user only copies up to the end of the last
    prompt, without copying the trailing whitespace.
    
    This commit adds a regression test and fixes the regular expression that
    strips continuation prompts, making the whitespace optional.

--- IPython/core/tests/test_inputtransformer.py
+++ IPython/core/tests/test_inputtransformer.py
@@ -233,6 +233,11 @@ syntax_ml = \
           ('...:     print i','    print i'),
           ('...: ', ''),
           ],
+         [('In [24]: for i in range(10):','for i in range(10):'),
+          # Space after last continuation prompt has been removed (issue #6674)
+          ('...:     print i','    print i'),
+          ('...:', ''),
+          ],
          [('In [2]: a="""','a="""'),
           ('   ...: 123"""','123"""'),
           ],
