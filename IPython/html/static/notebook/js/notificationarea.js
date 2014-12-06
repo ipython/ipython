97985a76e0d39051666cb9c451d1217fdedd011b
Nathan Heijermans <nathan@heijermans.us>
2014-12-06 10:28:16 -0500

    Increased notification message display time.

--- IPython/html/static/notebook/js/notificationarea.js
+++ IPython/html/static/notebook/js/notificationarea.js
@@ -279,9 +279,9 @@ define([
             nnw.warning(error.message || "Notebook copy failed");
         });
         
-            nnw.set_message(msg, 1000);
         this.events.on('toggle_other_client_output.Notebook', function(evt, ignored) {
             var msg = (ignored? "Ignoring": "Showing") + " output from other clients";
+            nnw.set_message(msg, 2000);
         });
 
         // Checkpoint events
