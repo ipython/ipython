e01e1301658cbe52ec11e0ff7711487f88ccd920
Jonathan Frederic <jdfreder@calpoly.edu>
2014-12-09 10:35:02 -0800

    Explicit 2.5px

--- IPython/html/widgets/widget.py
+++ IPython/html/widgets/widget.py
@@ -427,7 +427,8 @@ class DOMWidget(Widget):
     
     width = CUnicode(sync=True)
     height = CUnicode(sync=True)
-    padding = CUnicode(2.5, sync=True)
+    # A default padding of 2.5 px makes the widgets look nice when displayed inline.
+    padding = CUnicode("2.5px", sync=True)
     margin = CUnicode(sync=True)
 
     color = Unicode(sync=True)
