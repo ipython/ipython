30763f2fd5a710558365de663c7e3b0fad19831e
Jonathan Frederic <jon.freder@gmail.com>
2014-12-23 12:48:23 -0800

    New Terminal into new new button

--- IPython/html/static/tree/js/terminallist.js
+++ IPython/html/static/tree/js/terminallist.js
@@ -38,7 +38,7 @@ define([
         $('#refresh_' + this.element_name + '_list').click(function () {
             that.load_terminals();
         });
-        $('#new_terminal').click($.proxy(this.new_terminal, this));
+        $('#new-terminal').click($.proxy(this.new_terminal, this));
     };
 
     TerminalList.prototype.new_terminal = function () {
