d0310092aaa85bc810f0a15fb7c89740f3f3492f
Min RK <benjaminrk@gmail.com>
2015-01-13 11:14:31 -0800

    add shutil.which backport to py3compat

--- IPython/utils/py3compat.py
+++ IPython/utils/py3compat.py
@@ -4,6 +4,7 @@ import functools
 import os
 import sys
 import re
+import shutil
 import types
 
 from .encoding import DEFAULT_ENCODING
@@ -75,6 +76,70 @@ def safe_unicode(e):
 
     return u'Unrecoverably corrupt evalue'
 
+# shutil.which from Python 3.4
+def _shutil_which(cmd, mode=os.F_OK | os.X_OK, path=None):
+    """Given a command, mode, and a PATH string, return the path which
+    conforms to the given mode on the PATH, or None if there is no such
+    file.
+
+    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+    of os.environ.get("PATH"), or can be overridden with a custom search
+    path.
+    
+    This is a backport of shutil.which from Python 3.4
+    """
+    # Check that a given file can be accessed with the correct mode.
+    # Additionally check that `file` is not a directory, as on Windows
+    # directories pass the os.access check.
+    def _access_check(fn, mode):
+        return (os.path.exists(fn) and os.access(fn, mode)
+                and not os.path.isdir(fn))
+
+    # If we're given a path with a directory part, look it up directly rather
+    # than referring to PATH directories. This includes checking relative to the
+    # current directory, e.g. ./script
+    if os.path.dirname(cmd):
+        if _access_check(cmd, mode):
+            return cmd
+        return None
+
+    if path is None:
+        path = os.environ.get("PATH", os.defpath)
+    if not path:
+        return None
+    path = path.split(os.pathsep)
+
+    if sys.platform == "win32":
+        # The current directory takes precedence on Windows.
+        if not os.curdir in path:
+            path.insert(0, os.curdir)
+
+        # PATHEXT is necessary to check on Windows.
+        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+        # See if the given file matches any of the expected path extensions.
+        # This will allow us to short circuit when given "python.exe".
+        # If it does match, only test that one, otherwise we have to try
+        # others.
+        if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+            files = [cmd]
+        else:
+            files = [cmd + ext for ext in pathext]
+    else:
+        # On other platforms you don't have things like PATHEXT to tell you
+        # what file suffixes are executable, so just pass on cmd as-is.
+        files = [cmd]
+
+    seen = set()
+    for dir in path:
+        normdir = os.path.normcase(dir)
+        if not normdir in seen:
+            seen.add(normdir)
+            for thefile in files:
+                name = os.path.join(dir, thefile)
+                if _access_check(name, mode):
+                    return name
+    return None
+
 if sys.version_info[0] >= 3:
     PY3 = True
     
@@ -96,6 +161,8 @@ if sys.version_info[0] >= 3:
     
     string_types = (str,)
     unicode_type = str
+    
+    which = shutil.which
     
     def isidentifier(s, dotted=False):
         if dotted:
@@ -184,6 +251,8 @@ else:
     def get_closure(f):
         """Get a function's closure attribute"""
         return f.func_closure
+    
+    which = _shutil_which
 
     # Abstract u'abc' syntax:
     @_modify_str_or_docstring
