6ef7d21c32a23489b96d5e883c38376ab0c25716
Min RK <benjaminrk@gmail.com>
2015-01-27 10:39:50 -0800

    use default_url for logo link

--- IPython/html/base/handlers.py
+++ IPython/html/base/handlers.py
@@ -138,6 +138,10 @@ class IPythonHandler(AuthenticatedHandler):
         return self.settings.get('base_url', '/')
 
     @property
+    def default_url(self):
+        return self.settings.get('default_url', '/')
+
+    @property
     def ws_url(self):
         return self.settings.get('websocket_url', '')
 
@@ -238,6 +242,7 @@ class IPythonHandler(AuthenticatedHandler):
     def template_namespace(self):
         return dict(
             base_url=self.base_url,
+            default_url=self.default_url,
             ws_url=self.ws_url,
             logged_in=self.logged_in,
             login_available=self.login_available,
