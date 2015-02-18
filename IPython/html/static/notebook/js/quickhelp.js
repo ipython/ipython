a971f7a5b0c78adc7b89d3eb9250855b7d04db3c
abalkin <abalkin@enlnt.com>
2015-02-17 20:50:36 -0500

    Update quickhelp.js

--- IPython/html/static/notebook/js/quickhelp.js
+++ IPython/html/static/notebook/js/quickhelp.js
@@ -38,8 +38,8 @@ define([
             { shortcut: "Cmd-Down",   help:"go to cell end"  },
             { shortcut: "Alt-Left",   help:"go one word left"  },
             { shortcut: "Alt-Right",  help:"go one word right"  },
-            { shortcut: "Alt-Backspace",      help:"delete word to the left"  },
-            { shortcut: "Alt-Delete",         help:"delete word to the right"  },
+            { shortcut: "Alt-Backspace",      help:"delete word before"  },
+            { shortcut: "Alt-Delete",         help:"delete word after"  },
         ];
     } else {
         // PC specific
