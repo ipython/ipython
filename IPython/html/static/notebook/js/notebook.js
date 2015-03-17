dbfc1fda861764292e4e3dbf2d7f121e01cc5690
Alexander Belopolsky <a@enlnt.com>
2015-03-16 21:34:33 -0400

    First attempt to adjust end-space.

--- IPython/html/static/notebook/js/notebook.js
+++ IPython/html/static/notebook/js/notebook.js
@@ -344,6 +344,14 @@ define(function (require) {
         this.events.trigger('set_dirty.Notebook', {value: value});
     };
 
+    Notebook.prototype._adjust_end_space = function (scroll_value) {
+        var end_space =  $('.end_space');
+        var end = end_space.offset().top + end_space.height();
+        if (end < scroll_value + this.scroll_manager.element.height()) {
+            end_space.css('height', scroll_value - end);
+        }
+     };
+
     /**
      * Scroll the top of the page to a given cell.
      * 
@@ -356,7 +364,8 @@ define(function (require) {
         time = time || 0;
         index = Math.min(cells.length-1,index);
         index = Math.max(0             ,index);
-        var scroll_value = cells[index].element.position().top-cells[0].element.position().top ;
+        var scroll_value = cells[index].element.position().top-cells[0].element.position().top;
+        this._adjust_end_space(scroll_value);
         this.scroll_manager.element.animate({scrollTop:scroll_value}, time);
         return scroll_value;
     };
@@ -373,13 +382,14 @@ define(function (require) {
         time = time || 0;
         index = Math.min(cells.length-1,index);
         index = Math.max(0             ,index);
-        // var scroll_value = cells[index].element.position().top-cells[0].element.position().top ;
         var sme = this.scroll_manager.element;
         var h = sme.height();
         var st = sme.scrollTop();
         var t = sme.offset().top;
         var ct = cells[index].element.offset().top;
         var scroll_value =  st + ct - (t + h/2);
+        this._adjust_end_space(scroll_value);
+        console.log('scroll_value = ' + scroll_value);
         this.scroll_manager.element.animate({scrollTop:scroll_value}, time);
         return scroll_value;
     };
