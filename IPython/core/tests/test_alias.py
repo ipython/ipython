afaf3ad0a667f74c48c8e207ee4255729f0155a6
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-03-26 12:21:00 -0400

    Wrote test to ensure that number of nargs is calculated correctly.

--- IPython/core/tests/test_alias.py
+++ IPython/core/tests/test_alias.py
@@ -48,3 +48,15 @@ def test_alias_args_commented():
         _ip.run_cell('commetarg')
     
     nt.assert_equal(cap.stdout, 'this is %s a "commented out" arg')
+
+def test_alias_args_commented_nargs():
+    """Check that alias correctly counts args, excluding those commented out"""
+    am = _ip.alias_manager
+    alias_name = 'comargcount'
+    cmd = 'echo this is %%s a "commented out" arg and this is not %s'
+    
+    am.define_alias(alias_name, cmd)
+    assert am.is_alias(alias_name)
+    
+    thealias = am.get_alias(alias_name)
+    nt.assert_equal(thealias.nargs, 1)
