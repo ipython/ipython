e99390bf48dae18c14a63ada3286c8931177004b
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-03-26 13:02:51 -0400

    Removed decorative quotes in cmd causing 'commented_nargs' test to fail

--- IPython/core/tests/test_alias.py
+++ IPython/core/tests/test_alias.py
@@ -53,7 +53,7 @@ def test_alias_args_commented_nargs():
     """Check that alias correctly counts args, excluding those commented out"""
     am = _ip.alias_manager
     alias_name = 'comargcount'
-    cmd = 'echo this is %%s a "commented out" arg and this is not %s'
+    cmd = 'echo this is %%s a commented out arg and this is not %s'
     
     am.define_alias(alias_name, cmd)
     assert am.is_alias(alias_name)
