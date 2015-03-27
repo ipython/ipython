a02ad4329e855b6fe5af0460ba5c20e666dbdc1d
Min RK <benjaminrk@gmail.com>
2015-03-26 17:12:07 -0700

    finish kernel shims

--- IPython/kernel/__init__.py
+++ IPython/kernel/__init__.py
@@ -1,12 +1,8 @@
-"""IPython kernels and associated utilities
-
-For connecting to kernels, use jupyter_client
 """
-
+Shim to maintain backwards compatibility with old IPython.kernel imports.
+"""
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
-
-# Shim to maintain backwards compatibility with old IPython.kernel imports.
 
 import sys
 from warnings import warn
@@ -14,55 +10,20 @@ from warnings import warn
 warn("The `IPython.kernel` package has been deprecated. "
      "You should import from ipython_kernel or jupyter_client instead.")
 
+
 from IPython.utils.shimmodule import ShimModule
 
-# Shims for jupyter_client
-# Can't do a single shim, because the package didn't move all together
+# session moved relative to top-level
+sys.modules['IPython.kernel.zmq.session'] = ShimModule('session', mirror='jupyter_client.session')
 
-for name in (
-    'adapter',
-    'blocking',
-    'channels',
-    'channelsabc',
-    'client',
-    'clientabc',
-    'connect',
-    'ioloop',
-    'kernelspec',
-    'kernelspecapp',
-    'launcher',
-    'manager',
-    'managerabc',
-    'multikernelmanager',
-    'restarter',
-    'threaded',
-    'tests.test_adapter',
-    'tests.test_connect',
-    'tests.test_kernelmanager',
-    'tests.test_kernelspec',
-    'tests.test_launcher',
-    'tests.test_multikernelmanager',
-    'tests.test_public_api',
-):
-    sys.modules['IPython.kernel.%s' % name] = \
-        ShimModule(name, mirror='jupyter_client.%s' % name)
+for pkg in ('comm', 'inprocess', 'resources', 'zmq'):
+    sys.modules['IPython.kernel.%s' % pkg] = ShimModule(pkg, mirror='ipython_kernel.%s' % pkg)
+for pkg in ('ioloop', 'blocking'):
+    sys.modules['IPython.kernel.%s' % pkg] = ShimModule(pkg, mirror='jupyter_client.%s' % pkg)
 
-# some files moved out of the zmq prefix
-for name in (
-    'session',
-    'tests.test_session',
-):
-    sys.modules['IPython.kernel.zmq.%s' % name] = \
-        ShimModule(name, mirror='jupyter_client.%s' % name)
-# preserve top-level API modules, all from jupyter_client
-
-# just for friendlier zmq version check
-from . import zmq
-
-from jupyter_client.connect import *
-from jupyter_client.launcher import *
-from jupyter_client.client import KernelClient
-from jupyter_client.manager import KernelManager, run_kernel
-from jupyter_client.blocking import BlockingKernelClient
-from jupyter_client.multikernelmanager import MultiKernelManager
-
+# required for `from IPython.kernel import PKG`
+from ipython_kernel import comm, inprocess, resources, zmq
+from jupyter_client import ioloop, blocking
+# public API
+from ipython_kernel.connect import *
+from jupyter_client import *
