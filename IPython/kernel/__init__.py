8aead6535226414188ae38f2fddcd6a2efcfe10e
Min RK <benjaminrk@gmail.com>
2015-03-26 17:49:35 -0700

    kernel.resources isn't a package

--- IPython/kernel/__init__.py
+++ IPython/kernel/__init__.py
@@ -17,14 +17,14 @@ from IPython.utils.shimmodule import ShimModule
 sys.modules['IPython.kernel.zmq.session'] = ShimModule('session', mirror='jupyter_client.session')
 sys.modules['IPython.kernel.zmq'] = ShimModule('zmq', mirror='ipython_kernel')
 
-for pkg in ('comm', 'inprocess', 'resources'):
+for pkg in ('comm', 'inprocess'):
     sys.modules['IPython.kernel.%s' % pkg] = ShimModule(pkg, mirror='ipython_kernel.%s' % pkg)
 
 for pkg in ('ioloop', 'blocking'):
     sys.modules['IPython.kernel.%s' % pkg] = ShimModule(pkg, mirror='jupyter_client.%s' % pkg)
 
 # required for `from IPython.kernel import PKG`
-from ipython_kernel import comm, inprocess, resources
+from ipython_kernel import comm, inprocess
 from jupyter_client import ioloop, blocking
 # public API
 from ipython_kernel.connect import *
