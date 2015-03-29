152a462a3b85669261bd8af20c3c3ef0f09ac164
Min RK <benjaminrk@gmail.com>
2015-03-29 13:44:54 -0700

    use import_item in ShimModule
    The problem described in the comment does not appear to exist anymore

--- IPython/utils/shimmodule.py
+++ IPython/utils/shimmodule.py
@@ -6,6 +6,8 @@
 import sys
 import types
 
+from .importstring import import_item
+
 
 class ShimImporter(object):
     """Import hook for a shim.
@@ -36,10 +38,7 @@ class ShimImporter(object):
     def load_module(self, fullname):
         """Import the mirrored module, and insert it into sys.modules"""
         mirror_name = self._mirror_name(fullname)
-        mod = __import__(mirror_name)
-        if '.' in mirror_name:
-            name = mirror_name.rsplit('.', 1)[-1]
-            mod = getattr(mod, name)
+        mod = import_item(mirror_name)
         sys.modules[fullname] = mod
         return mod
 
@@ -70,26 +69,7 @@ class ShimModule(types.ModuleType):
     def __getattr__(self, key):
         # Use the equivalent of import_item(name), see below
         name = "%s.%s" % (self._mirror, key)
-
-        # NOTE: the code below was copied *verbatim* from
-        # importstring.import_item. For some very strange reason that makes no
-        # sense to me, if we call it *as a function*, it doesn't work.  This
-        # has something to do with the deep bowels of the import machinery and
-        # I couldn't find a way to make the code work as a standard function
-        # call.  But at least since it's an unmodified copy of import_item,
-        # which is used extensively and has a test suite, we can be reasonably
-        # confident this is OK.  If anyone finds how to call the function, all
-        # the below could be replaced simply with:
-        #
-        # from IPython.utils.importstring import import_item
-        # return import_item('MIRROR.' + key)
-
-        parts = name.rsplit('.', 1)
-        if len(parts) == 2:
-            # called with 'foo.bar....'
-            package, obj = parts
-            module = __import__(package, fromlist=[obj])
-            return getattr(module, obj)
-        else:
-            # called with un-dotted string
-            return __import__(parts[0])
+        try:
+            return import_item(name)
+        except ImportError:
+            raise AttributeError(key)
