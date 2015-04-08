a4d2c4fd0b8285fdaea4bc01b0758129a44e0f5d
Min RK <benjaminrk@gmail.com>
2015-04-08 12:40:09 -0700

    shim `python -m IPython.html`

--- IPython/html.py
+++ IPython/html.py
@@ -15,3 +15,6 @@ from IPython.utils.shimmodule import ShimModule
 sys.modules['IPython.html'] = ShimModule(
     src='IPython.html', mirror='jupyter_notebook')
 
+if __name__ == '__main__':
+    from jupyter_notebook import notebookapp as app
+    app.launch_new_instance()
