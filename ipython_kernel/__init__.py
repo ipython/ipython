dc3dae38e4ba6719d055bca7f2681b9a3d230b90
Min RK <benjaminrk@gmail.com>
2015-04-09 13:12:36 -0700

    remove ipython_kernel

--- ipython_kernel/__init__.py
+++
@@ -1,1 +0,0 @@
-from .connect import *
--- ipython_kernel/inprocess/__init__.py
+++
@@ -1,8 +0,0 @@
-from .channels import (
-    InProcessChannel,
-    InProcessHBChannel,
-)
-
-from .client import InProcessKernelClient
-from .manager import InProcessKernelManager
-from .blocking import BlockingInProcessKernelClient
