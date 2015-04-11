1f2abd8a2a1a2fc815689ac09c95659395b4ae39
Min RK <benjaminrk@gmail.com>
2015-04-10 18:00:15 -0700

    remove notebook-specific parts of setup, git-hooks

--- git-hooks/install-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DOTGIT=`git rev-parse --git-dir`
-TOPLEVEL=`git rev-parse --show-toplevel`
-TO=${DOTGIT}/hooks
-FROM=${TOPLEVEL}/git-hooks
-
-ln -s ${FROM}/post-checkout ${TO}/post-checkout
-ln -s ${FROM}/post-merge ${TO}/post-merge
