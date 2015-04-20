538f8e3382e274402f2f71ba79439fae0828b3cf
Jonathan Frederic <jdfreder@calpoly.edu>
2015-04-20 13:57:33 -0700

    Add shim to new widgets repository.

--- IPython/html.py
+++ IPython/html.py
@@ -8,12 +8,14 @@ import sys
 from warnings import warn
 
 warn("The `IPython.html` package has been deprecated. "
-     "You should import from jupyter_notebook instead.")
+     "You should import from `jupyter_notebook` and `jupyter_widgets` instead.")
 
 from IPython.utils.shimmodule import ShimModule
 
 sys.modules['IPython.html'] = ShimModule(
     src='IPython.html', mirror='jupyter_notebook')
+sys.modules['IPython.html.widgets'] = ShimModule(
+    src='IPython.html.widgets', mirror='jupyter_widgets')
 
 if __name__ == '__main__':
     from jupyter_notebook import notebookapp as app
