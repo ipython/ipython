4bf38cf24dc93569fee239c6109469e3e39864d6
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-05-31 17:56:19 -0400

    Reprising caught error

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -2713,6 +2713,8 @@ class InteractiveShell(SingletonConfigurable):
                     elif not result.success:
                         break
             except:
+                if raise_exceptions:
+                    raise
                 self.showtraceback()
                 warn('Unknown failure executing file: <%s>' % fname)
 
