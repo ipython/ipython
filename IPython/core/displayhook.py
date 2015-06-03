b61d88a990d7923e4a74df003dc70c4667186ee7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-06-03 11:03:30 -0700

    fix indentation by tabs

--- IPython/core/displayhook.py
+++ IPython/core/displayhook.py
@@ -84,19 +84,19 @@ class DisplayHook(Configurable):
     def quiet(self):
         """Should we silence the display hook because of ';'?"""
         # do not print output if input ends in ';'
- 
-	cell = self.shell.history_manager.input_hist_parsed[self.prompt_count]
+
+        cell = self.shell.history_manager.input_hist_parsed[self.prompt_count]
         sio = io.StringIO(cell)
         tokens = list(tokenize.generate_tokens(sio.readline))
- 
+
         try:
-    	    for token in reversed(tokens):
+            for token in reversed(tokens):
                 if token.type in (tokenize.ENDMARKER, tokenize.COMMENT):
-            	    continue
+                    continue
                 if (token.type == tokenize.OP) and (token.string == ';'):
-            	    return True
+                    return True
                 else:
-            	    return False
+                    return False
         except IndexError:
             # some uses of ipshellembed may fail here
             return False
