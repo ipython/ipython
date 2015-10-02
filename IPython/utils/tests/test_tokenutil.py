a85e1dcd48157003872dcc411478dba75a88bf0b
Min RK <benjaminrk@gmail.com>
2015-10-02 16:08:16 +0200

    handle multi-line tokens in token_at_cursor
    offset tracking was wrong in the case of tokens spanning multiple lines (e.g. docstrings)

--- IPython/utils/tests/test_tokenutil.py
+++ IPython/utils/tests/test_tokenutil.py
@@ -53,6 +53,25 @@ def test_multiline():
     for i in range(start, start + len(expected)):
         expect_token(expected, cell, i)
 
+def test_multiline_token():
+    cell = '\n'.join([
+        '"""\n\nxxxxxxxxxx\n\n"""',
+        '5, """',
+        'docstring',
+        'multiline token',
+        '""", [',
+        '2, 3, "complicated"]',
+        'b = hello("string", there)'
+    ])
+    expected = 'hello'
+    start = cell.index(expected) + 1
+    for i in range(start, start + len(expected)):
+        expect_token(expected, cell, i)
+    expected = 'hello'
+    start = cell.index(expected) + 1
+    for i in range(start, start + len(expected)):
+        expect_token(expected, cell, i)
+
 def test_nested_call():
     cell = "foo(bar(a=5), b=10)"
     expected = 'foo'
