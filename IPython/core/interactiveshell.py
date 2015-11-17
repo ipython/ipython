62850ab870c76969087fdca7a86629801a22d260
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-11-16 22:21:05 -0800

    Remove unused private method.
    Artifact of cell magics first implementation.
    Not used anymore.

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -2773,13 +2773,6 @@ class InteractiveShell(SingletonConfigurable):
             self.showtraceback()
             warn('Unknown failure executing module: <%s>' % mod_name)
 
-    def _run_cached_cell_magic(self, magic_name, line):
-        """Special method to call a cell magic with the data stored in self.
-        """
-        cell = self._current_cell_magic_body
-        self._current_cell_magic_body = None
-        return self.run_cell_magic(magic_name, line, cell)
-
     def run_cell(self, raw_cell, store_history=False, silent=False, shell_futures=True):
         """Run a complete IPython cell.
 
