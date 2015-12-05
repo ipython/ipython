51ca6ff8b21d12814528e4725a984c1179407c30
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-05 15:10:34 +0100

    Catch keyboard interrupt and quit

--- IPython/core/application.py
+++ IPython/core/application.py
@@ -201,8 +201,11 @@ class BaseIPythonApplication(Application):
             self.log.warning("Subcommand `ipython {sub}` is deprecated and will be removed "
                              "in future versions.".format(sub=subc))
             self.log.warning("You likely want to use `jupyter {sub}`... continue "
-                             "in 5 sec".format(sub=subc))
-            time.sleep(5)
+                             "in 5 sec. Press Ctrl-C to quit now.".format(sub=subc))
+            try:
+                time.sleep(5)
+            except KeyboardInterrupt:
+                sys.exit(1)
         return super(BaseIPythonApplication, self).initialize_subcommand(subc, argv)
 
     def init_crash_handler(self):
