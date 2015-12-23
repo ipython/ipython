a9b0c6ca598179c4c07291f3cc4111a021d92078
Thomas Kluyver <thomas@kluyver.me.uk>
2015-12-23 16:44:43 +0000

    Refine multiline behaviour

--- IPython/terminal/ptshell.py
+++ IPython/terminal/ptshell.py
@@ -1,9 +1,9 @@
 from IPython.core.interactiveshell import InteractiveShell
 
-from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.shortcuts import create_prompt_layout
-from prompt_toolkit.filters import Condition
-from prompt_toolkit.interface import AcceptAction, Application, CommandLineInterface
+from prompt_toolkit.shortcuts import create_prompt_application
+from prompt_toolkit.interface import CommandLineInterface
+from prompt_toolkit.key_binding.manager import KeyBindingManager
+from prompt_toolkit.keys import Keys
 from prompt_toolkit.layout.lexers import PygmentsLexer
 
 from pygments.lexers import Python3Lexer
@@ -11,17 +11,6 @@ from pygments.token import Token
 
 
 class PTInteractiveShell(InteractiveShell):
-    def _multiline(self, cli):
-        doc = cli.current_buffer.document
-        if not doc.on_last_line:
-            cli.run_in_terminal(lambda: print('Not on last line'))
-            return False
-        status, indent = self.input_splitter.check_complete(doc.text)
-        return status == 'incomplete'
-
-    def _multiline2(self):
-        return self._multiline(self.pt_cli)
-
     pt_cli = None
 
     def get_prompt_tokens(self, cli):
@@ -33,16 +22,27 @@ class PTInteractiveShell(InteractiveShell):
 
 
     def init_prompt_toolkit_cli(self):
-        layout = create_prompt_layout(
-            get_prompt_tokens=self.get_prompt_tokens,
-            lexer=PygmentsLexer(Python3Lexer),
-            multiline=Condition(self._multiline),
+        kbmanager = KeyBindingManager.for_prompt()
+        @kbmanager.registry.add_binding(Keys.ControlJ) # Ctrl+J == Enter, seemingly
+        def _(event):
+            b = event.current_buffer
+            if not b.document.on_last_line:
+                b.newline()
+                return
+
+            status, indent = self.input_splitter.check_complete(b.document.text)
+
+            if (status != 'incomplete') and b.accept_action.is_returnable:
+                b.accept_action.validate_and_handle(event.cli, b)
+            else:
+                b.insert_text('\n' + (' ' * indent))
+
+        app = create_prompt_application(multiline=True,
+                            lexer=PygmentsLexer(Python3Lexer),
+                            get_prompt_tokens=self.get_prompt_tokens,
+                            key_bindings_registry=kbmanager.registry,
         )
-        buffer = Buffer(
-            is_multiline=Condition(self._multiline2),
-            accept_action=AcceptAction.RETURN_DOCUMENT,
-        )
-        app = Application(layout=layout, buffer=buffer)
+
         self.pt_cli = CommandLineInterface(app)
 
     def __init__(self, *args, **kwargs):
