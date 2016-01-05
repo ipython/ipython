abd031405fd7db3f19db0641232b94f936ca729d
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-01-05 17:29:24 +0100

    Add missing space

--- IPython/lib/kernel.py
+++ IPython/lib/kernel.py
@@ -5,7 +5,7 @@ Moved to IPython.kernel.connect
 
 import warnings
 warnings.warn("IPython.lib.kernel moved to IPython.kernel.connect in IPython 1.0," 
-        "and will be removed in IPython 6.0.",
+        " and will be removed in IPython 6.0.",
     DeprecationWarning
 )
 
