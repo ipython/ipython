26d9f902fcd165753034074697596e111390a990
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-02-18 11:01:01 -0800

    remove lightbg style

--- IPython/utils/lightbg.py
+++ IPython/utils/lightbg.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, \
-     Number, Operator, Generic, Whitespace, Token
 
 from collections import defaultdict
 
@@ -21,57 +19,7 @@ class LightBGStyle(Style):
 
     background_color = "#f8f8f8"
     default_style = ""
+    
+    style = defaultdict(lambda:'bold #BBBBBB', {'':''})
 
-    styles = defaultdict(lambda:'italic #FF0000',{
-        Token.InPrompt.Number:     "bold #0000BB", 
-        Token.InPrompt.Color :     "bold #0000BB", 
-        Token.OutPrompt.Number:    "bold #BB0000",
-        Token.OutPrompt.Color:     "bold #bb0000",
 
-        Whitespace:                "#bbbbbb",
-        Comment:                   "italic #408080",
-        Comment.Preproc:           "noitalic #BC7A00",
-
-        Keyword:                   "bold #008000",
-        Keyword.Pseudo:            "nobold",
-        Keyword.Type:              "nobold #B00040",
-
-        Operator:                  "#666666",
-        Operator.Word:             "bold #AA22FF",
-
-        Name.Builtin:              "#008000",
-        Name.Function:             "#0000FF",
-        Name.Class:                "bold #0000FF",
-        Name.Namespace:            "bold #0000FF",
-        Name.Exception:            "bold #D2413A",
-        Name.Variable:             "#19177C",
-        Name.Constant:             "#880000",
-        Name.Label:                "#A0A000",
-        Name.Entity:               "bold #999999",
-        Name.Attribute:            "#7D9029",
-        Name.Tag:                  "bold #008000",
-        Name.Decorator:            "#AA22FF",
-
-        String:                    "#BA2121",
-        String.Doc:                "italic",
-        String.Interpol:           "bold #BB6688",
-        String.Escape:             "bold #BB6622",
-        String.Regex:              "#BB6688",
-        #String.Symbol:             "#B8860B",
-        String.Symbol:             "#19177C",
-        String.Other:              "#008000",
-        Number:                    "#666666",
-
-        Generic.Heading:           "bold #000080",
-        Generic.Subheading:        "bold #800080",
-        Generic.Deleted:           "#A00000",
-        Generic.Inserted:          "#00A000",
-        Generic.Error:             "#FF0000",
-        Generic.Emph:              "italic",
-        Generic.Strong:            "bold",
-        Generic.Prompt:            "bold #000080",
-        Generic.Output:            "#888",
-        Generic.Traceback:         "#04D",
-
-        Error:                     "border:#FF0000"
-    })
