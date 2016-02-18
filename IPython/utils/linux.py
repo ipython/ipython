9bfcc0351138fdaae1a504c3e013b805d3e2def2
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-02-18 11:01:41 -0800

    remove linux style

--- IPython/utils/linux.py
+++ IPython/utils/linux.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, \
-     Number, Operator, Generic, Whitespace, Token
 
 class LinuxStyle(Style):
     """
@@ -12,56 +10,4 @@ class LinuxStyle(Style):
     background_color = "#f8f8f8"
     default_style = ""
 
-    styles = {
-        Token.InPrompt.Number:     "#00FFFF", 
-        Token.InPrompt.Color :     "#0000FF", 
-        Token.OutPrompt.Number:    "#00FF00",
-        Token.OutPrompt.Color:     "#FFFF00",
-
-        Whitespace:                "#bbbbbb",
-        Comment:                   "italic #408080",
-        Comment.Preproc:           "noitalic #BC7A00",
-
-        #Keyword:                   "bold #AA22FF",
-        Keyword:                   "bold #008000",
-        Keyword.Pseudo:            "nobold",
-        Keyword.Type:              "nobold #B00040",
-
-        Operator:                  "#666666",
-        Operator.Word:             "bold #AA22FF",
-
-        Name.Builtin:              "#008000",
-        Name.Function:             "#0000FF",
-        Name.Class:                "bold #0000FF",
-        Name.Namespace:            "bold #0000FF",
-        Name.Exception:            "bold #D2413A",
-        Name.Variable:             "#19177C",
-        Name.Constant:             "#880000",
-        Name.Label:                "#A0A000",
-        Name.Entity:               "bold #999999",
-        Name.Attribute:            "#7D9029",
-        Name.Tag:                  "bold #008000",
-        Name.Decorator:            "#AA22FF",
-
-        String:                    "#BA2121",
-        String.Doc:                "italic",
-        String.Interpol:           "bold #BB6688",
-        String.Escape:             "bold #BB6622",
-        String.Regex:              "#BB6688",
-        String.Symbol:             "#19177C",
-        String.Other:              "#008000",
-        Number:                    "#666666",
-
-        Generic.Heading:           "bold #000080",
-        Generic.Subheading:        "bold #800080",
-        Generic.Deleted:           "#A00000",
-        Generic.Inserted:          "#00A000",
-        Generic.Error:             "#FF0000",
-        Generic.Emph:              "italic",
-        Generic.Strong:            "bold",
-        Generic.Prompt:            "bold #000080",
-        Generic.Output:            "#888",
-        Generic.Traceback:         "#04D",
-
-        Error:                     "border:#FF0000"
-    }
+    style = defaultdict(lambda:'bold #BB0000', {'':''})
