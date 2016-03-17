dad5f65facc5bf44dc3dab3ceae165a6c7e90d27
Thomas Kluyver <thomas@kluyver.me.uk>
2016-03-17 16:51:08 +0000

    Don't check for readline when setting autoindent
    Autoindent is not strictly tied to readline any more, and this warning
    was annoying me.

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -527,15 +527,9 @@ class InteractiveShell(SingletonConfigurable):
         ensure_dir_exists(new)
 
     def set_autoindent(self,value=None):
-        """Set the autoindent flag, checking for readline support.
+        """Set the autoindent flag.
 
         If called with no arguments, it acts as a toggle."""
-
-        if value != 0 and not self.has_readline:
-            if os.name == 'posix':
-                warn("The auto-indent feature requires the readline library")
-            self.autoindent = 0
-            return
         if value is None:
             self.autoindent = not self.autoindent
         else:
