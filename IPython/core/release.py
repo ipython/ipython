c963f6b4435fb150ebe995460649923b3ffc5e71
Min RK <benjaminrk@gmail.com>
2016-04-20 14:02:39 +0200

    release 4.2

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -20,8 +20,8 @@ name = 'ipython'
 # release.  'dev' as a _version_extra string means this is a development
 # version
 _version_major = 4
-_version_minor = 1
-_version_patch = 2
+_version_minor = 2
+_version_patch = 0
 # _version_extra = '.dev'
 # _version_extra = 'rc2'
 _version_extra = ''  # Uncomment this for full releases
