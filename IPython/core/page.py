422d42ee9639107fdc11ec9f5864ae63e1858b5f
Pierre Gerold <gerold@crans.org>
2016-04-20 21:03:32 +0200

    Thomas fix

--- IPython/core/page.py
+++ IPython/core/page.py
@@ -298,7 +298,7 @@ def get_pager_cmd(pager_cmd=None):
     Makes some attempts at finding an OS-correct one.
     """
     if os.name == 'posix':
-        default_pager_cmd = 'less -R'  # -r for color control sequences
+        default_pager_cmd = 'less -R'  # -R for color control sequences
     elif os.name in ['nt','dos']:
         default_pager_cmd = 'type'
 
@@ -308,7 +308,7 @@ def get_pager_cmd(pager_cmd=None):
         except:
             pager_cmd = default_pager_cmd
     
-    if pager_cmd == 'less' and not os.environ.get('LESS', ''):
+    if pager_cmd == 'less' and '-r' not in os.environ.get('LESS', ''):
         pager_cmd += ' -R'
     
     return pager_cmd
