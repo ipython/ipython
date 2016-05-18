c1d39c5055affd530afc7cbb1d7cc8dcd6f60fdf
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-05-18 15:03:12 -0700

    Reorder steps and group related action together.

--- .travis/nightly.sh
+++ .travis/nightly.sh
@@ -1,12 +1,20 @@
 #!/bin/bas
 
-if [ ("$TRAVIS_BRANCH" == "$master") && ("$TRAVIS_PULL_REQUEST" == 'false') && ("$TRAVIS_REPO_SLUG" == "ipython/ipython")]
+if (( $TRAVIS_BRANCH == "$master" )) && (( "$TRAVIS_PULL_REQUEST" == 'false' )) && (( "$TRAVIS_REPO_SLUG" == "ipython/ipython" ));
 then
-  pip install twine
-  # decrypt .pypirc with credential for projectJupyter on testpypi and pypi. 
-  openssl aes-256-cbc -K $encrypted_669152acb59a_key -iv $encrypted_669152acb59a_iv -in .pypirc.enc -out .pypirc -d
-  cp .pypirc ~/
+  echo 'Building nightly release'
   python tools/build_release
+
+  # decrypt .pypirc with credential for projectJupyter on testpypi and pypi. 
+  echo 'Decrypting .pypirc ...'
+  openssl aes-256-cbc -K $encrypted_669152acb59a_key -iv $encrypted_669152acb59a_iv -in .pypirc.enc -out .pypirc -d
+
+  cp .pypirc ~/
+  
+  echo 'Installing twine'
+  pip install twine
+
+  echo 'Uploading nightly'
   twine upload dist/* -r $PYPI_REPOSITORY
 else
   echo 'Not master,not ipython/ipython repo, or in a pull request: doing nothing.'
