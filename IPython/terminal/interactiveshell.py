2acc44a5b9c92768691e282198e3a705383d990f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-06-02 10:29:21 -0700

    Improve deprecation warning.
    Raise a warning no import, as it is common to have import that are
    unused.
    
    Also add since when this is deprecated.
    
    Close #9526

--- IPython/terminal/interactiveshell.py
+++ IPython/terminal/interactiveshell.py
@@ -9,10 +9,13 @@ from warnings import warn
 from IPython.utils.decorators import undoc
 from .ptshell import TerminalInteractiveShell as PromptToolkitShell
 
+warn("Since IPython 5.0 `IPython.terminal.interactiveshell` is deprecated in favor of `IPython.terminal.ptshell`.",
+      DeprecationWarning)
+
 @undoc
 class TerminalInteractiveShell(PromptToolkitShell):
     def __init__(self, *args, **kwargs):
-        warn("This is a deprecated alias for IPython.terminal.ptshell.TerminalInteractiveShell. "
+        warn("Since IPython 5.0 this is a deprecated alias for IPython.terminal.ptshell.TerminalInteractiveShell. "
              "The terminal interface of this class now uses prompt_toolkit instead of readline.",
              DeprecationWarning, stacklevel=2)
         PromptToolkitShell.__init__(self, *args, **kwargs)
