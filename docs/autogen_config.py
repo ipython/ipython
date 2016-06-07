32cc988e81bbbecf09f7e7a801e92c6cfc281e75
klonuo <klonuo@gmail.com>
2016-06-07 17:10:39 +0200

    Remove generation of unnecessary generated.rst file

--- docs/autogen_config.py
+++ docs/autogen_config.py
@@ -7,7 +7,6 @@ from ipykernel.kernelapp import IPKernelApp
 
 here = abspath(dirname(__file__))
 options = join(here, 'source', 'config', 'options')
-generated = join(options, 'generated.rst')
 
 def write_doc(name, title, app, preamble=None):
     filename = '%s.rst' % name
@@ -18,18 +17,12 @@ def write_doc(name, title, app, preamble=None):
         if preamble is not None:
             f.write(preamble + '\n\n')
         f.write(app.document_config_options())
-    with open(generated, 'a') as f:
-        f.write(filename + '\n')
 
 
 if __name__ == '__main__':
-    # create empty file
-    with open(generated, 'w'):
-        pass
 
     write_doc('terminal', 'Terminal IPython options', TerminalIPythonApp())
     write_doc('kernel', 'IPython kernel options', IPKernelApp(),
         preamble=("These options can be used in :file:`ipython_kernel_config.py`. "
                   "The kernel also respects any options in `ipython_config.py`"),
     )
-
