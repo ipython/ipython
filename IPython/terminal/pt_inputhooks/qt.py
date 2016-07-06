dc14b46f11f1c85a9a8936e5de9599234bc54c9c
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-07-05 18:07:41 -0700

    Try to get QT To work on windows.

--- IPython/terminal/pt_inputhooks/qt.py
+++ IPython/terminal/pt_inputhooks/qt.py
@@ -1,11 +1,39 @@
 from IPython.external.qt_for_kernel import QtCore, QtGui
 
+import threading
+import time
+
+import select
+watched_fd = set({})
+
+def _stop_qt_ev_on_redable(fd, ev):
+    """QtNotifier seem to not work on windows, we manually watch the file"""
+    while True:
+        ready, _, _ = select.select((fd,), (), (), .1)
+        time.sleep(0.05)
+        if ready:
+            watched_fd.remove(fd)
+            ev.exit()
+            break
+
+
 def inputhook(context):
     app = QtCore.QCoreApplication.instance()
     if not app:
         return
     event_loop = QtCore.QEventLoop(app)
-    notifier = QtCore.QSocketNotifier(context.fileno(), QtCore.QSocketNotifier.Read)
-    notifier.setEnabled(True)
-    notifier.activated.connect(event_loop.exit)
-    event_loop.exec_()
+    fd = context.fileno()
+
+    if sys.platform == 'win32':
+        if fd and (fd not in watched_fd):
+            watched_fd.add(fd)
+            t = threading.Thread(target=_stop_qt_ev_on_redable, args=(fd, event_loop))
+            t.start()
+    else:
+        notifier = QtCore.QSocketNotifier(fd, QtCore.QSocketNotifier.Read)
+        notifier.setEnabled(True)
+        notifier.activated.connect(event_loop.exit)
+        event_loop.exec_()
+
+    
+
