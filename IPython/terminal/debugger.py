3e812afa6e1f917695b517fc077ae54e5dab6c79
mbyt <random.seed@web.de>
2016-09-19 18:03:02 +0200

    fixing typo in comment

--- IPython/terminal/debugger.py
+++ IPython/terminal/debugger.py
@@ -87,7 +87,7 @@ def set_trace(frame=None):
 if __name__ == '__main__':
     import pdb
     # IPython.core.debugger.Pdb.trace_dispatch shall not catch
-    # bdb.BdbQuit. When started through __main__ and an exeption
+    # bdb.BdbQuit. When started through __main__ and an exception
     # happend after hitting "c", this is needed in order to
     # be able to quit the debugging session (see #9950).
     old_trace_dispatch = pdb.Pdb.trace_dispatch
