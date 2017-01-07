7d3a463efa1f64dd87addfd2d53163b762fc638d
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2017-01-07 19:52:20 +0530

    cleanup

--- IPython/extensions/tests/test_autoreload.py
+++ IPython/extensions/tests/test_autoreload.py
@@ -18,18 +18,13 @@ import tempfile
 import shutil
 import random
 import time
+from io import StringIO
 
 import nose.tools as nt
 import IPython.testing.tools as tt
 
 from IPython.extensions.autoreload import AutoreloadMagics
 from IPython.core.events import EventManager, pre_run_cell
-from IPython.utils.py3compat import PY3
-
-if PY3:
-    from io import StringIO
-else:
-    from StringIO import StringIO
 
 #-----------------------------------------------------------------------------
 # Test fixture
