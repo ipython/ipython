d40effda573e46c73549a8e7efaa918d19e93623
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2017-01-07 19:19:07 +0530

    clean up

--- IPython/core/tests/test_history.py
+++ IPython/core/tests/test_history.py
@@ -22,7 +22,7 @@ from IPython.core.history import HistoryManager, extract_hist_ranges
 from IPython.utils import py3compat
 
 def setUp():
-    nt.assert_equal(sys.getdefaultencoding(), "utf-8" if py3compat.PY3 else "ascii")
+    nt.assert_equal(sys.getdefaultencoding(), "utf-8")
 
 def test_history():
     ip = get_ipython()
