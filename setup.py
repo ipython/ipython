035d64301ddc65a3a38b379fd4b5383368b322da
Thomas Kluyver <thomas@kluyver.me.uk>
2017-04-12 09:42:28 +0100

    Remove unused code for building Windows installers

--- setup.py
+++ setup.py
@@ -239,23 +239,7 @@ if 'setuptools' in sys.modules:
         ],
     }
     setup_args['extras_require'] = extras_require
-    requires = setup_args['install_requires'] = install_requires
-
-    # Script to be run by the windows binary installer after the default setup
-    # routine, to add shortcuts and similar windows-only things.  Windows
-    # post-install scripts MUST reside in the scripts/ dir, otherwise distutils
-    # doesn't find them.
-    if 'bdist_wininst' in sys.argv:
-        if len(sys.argv) > 2 and \
-               ('sdist' in sys.argv or 'bdist_rpm' in sys.argv):
-            print("ERROR: bdist_wininst must be run alone. Exiting.", file=sys.stderr)
-            sys.exit(1)
-        setup_args['data_files'].append(
-            ['Scripts', ('scripts/ipython.ico', 'scripts/ipython_nb.ico')])
-        setup_args['scripts'] = [pjoin('scripts','ipython_win_post_install.py')]
-        setup_args['options'] = {"bdist_wininst":
-                                 {"install_script":
-                                  "ipython_win_post_install.py"}}
+    setup_args['install_requires'] = install_requires
 
 else:
     # scripts has to be a non-empty list, or install_scripts isn't called
