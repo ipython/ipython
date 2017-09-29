d73e5f74142b758267bee822059db92513a611e2
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-28 22:56:11 -0400

    Adding transient and update, change to dict for outputs

--- IPython/core/displaypub.py
+++ IPython/core/displaypub.py
@@ -115,10 +115,11 @@ class CapturingDisplayPublisher(DisplayPublisher):
     outputs = List()
 
     def publish(self, data, metadata=None, source=None, *, transient=None, update=False):
-        self.outputs.append((data, metadata))
+        self.outputs.append({'data':data, 'metadata':metadata,
+                             'transient':transient, 'update':update})
 
     def clear_output(self, wait=False):
         super(CapturingDisplayPublisher, self).clear_output(wait)
 
         # empty the list, *do not* reassign a new list
-        del self.outputs[:]
+        self.outputs.clear()
