fe198808e5d3221f7541edcb5ff33d31230927fd
Hugo <hugovk@users.noreply.github.com>
2017-10-20 13:46:19 +0300

    Update version classifiers

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -114,10 +114,6 @@ classifiers = [
     'License :: OSI Approved :: BSD License',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: Only',
-    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: 3 :: Only',
     'Topic :: System :: Shells'
     ]
