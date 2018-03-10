686e6c9efadedf8f6c1e3b05f98554d857121bed
Thomas Kluyver <thomas@kluyver.me.uk>
2018-03-10 20:28:23 +0000

    Start integrating new machinery for checking code completeness

--- IPython/core/tests/test_inputtransformer2.py
+++ IPython/core/tests/test_inputtransformer2.py
@@ -179,10 +179,11 @@ def test_transform_help():
     nt.assert_equal(tf.transform(HELP_MULTILINE[0]), HELP_MULTILINE[2])
 
 def test_check_complete():
-    tm = ipt2.TransformerManager()
-    nt.assert_equal(tm.check_complete("a = 1"), ('complete', None))
-    nt.assert_equal(tm.check_complete("for a in range(5):"), ('incomplete', 4))
-    nt.assert_equal(tm.check_complete("raise = 2"), ('invalid', None))
-    nt.assert_equal(tm.check_complete("a = [1,\n2,"), ('incomplete', 0))
-    nt.assert_equal(tm.check_complete("a = '''\n   hi"), ('incomplete', 3))
-    nt.assert_equal(tm.check_complete("def a():\n x=1\n global x"), ('invalid', None))
+    cc = ipt2.TransformerManager().check_complete
+    nt.assert_equal(cc("a = 1"), ('complete', None))
+    nt.assert_equal(cc("for a in range(5):"), ('incomplete', 4))
+    nt.assert_equal(cc("raise = 2"), ('invalid', None))
+    nt.assert_equal(cc("a = [1,\n2,"), ('incomplete', 0))
+    nt.assert_equal(cc("a = '''\n   hi"), ('incomplete', 3))
+    nt.assert_equal(cc("def a():\n x=1\n global x"), ('invalid', None))
+    nt.assert_equal(cc("a \\ "), ('invalid', None))  # Nothing allowed after backslash
