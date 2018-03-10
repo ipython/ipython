6f00ca811d3c62896cd8eb8bcfa2495a3924a073
Thomas Kluyver <thomas@kluyver.me.uk>
2018-03-10 19:54:33 +0000

    Start adding code for checking when input is complete

--- IPython/core/tests/test_inputtransformer2.py
+++ IPython/core/tests/test_inputtransformer2.py
@@ -177,3 +177,12 @@ def test_transform_help():
 
     tf = ipt2.HelpEnd((1, 0), (2, 8))
     nt.assert_equal(tf.transform(HELP_MULTILINE[0]), HELP_MULTILINE[2])
+
+def test_check_complete():
+    tm = ipt2.TransformerManager()
+    nt.assert_equal(tm.check_complete("a = 1"), ('complete', None))
+    nt.assert_equal(tm.check_complete("for a in range(5):"), ('incomplete', 4))
+    nt.assert_equal(tm.check_complete("raise = 2"), ('invalid', None))
+    nt.assert_equal(tm.check_complete("a = [1,\n2,"), ('incomplete', 0))
+    nt.assert_equal(tm.check_complete("a = '''\n   hi"), ('incomplete', 3))
+    nt.assert_equal(tm.check_complete("def a():\n x=1\n global x"), ('invalid', None))
