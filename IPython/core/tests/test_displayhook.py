24aae1cb757b79e4ec16237ec132b38114abc896
Matthias Bussonnier <bussonniermatthias@gmail.com>
2018-05-02 11:37:02 -0700

    Backport PR #11101: Added fix for display hook call output format

--- IPython/core/tests/test_displayhook.py
+++ IPython/core/tests/test_displayhook.py
@@ -1,4 +1,7 @@
+import sys
 from IPython.testing.tools import AssertPrints, AssertNotPrints
+from IPython.core.displayhook import CapturingDisplayHook
+from IPython.utils.capture import CapturedIO
 
 ip = get_ipython()
 
@@ -101,3 +104,11 @@ def test_interactivehooks_ast_modes_semi_supress():
 
     finally:
         ip.ast_node_interactivity = saved_mode
+
+def test_capture_display_hook_format():
+    """Tests that the capture display hook conforms to the CapturedIO output format"""
+    hook = CapturingDisplayHook(ip)
+    hook({"foo": "bar"})
+    captured = CapturedIO(sys.stdout, sys.stderr, hook.outputs)
+    # Should not raise with RichOutput transformation error
+    captured.outputs
