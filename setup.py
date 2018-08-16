702b9463a72045028f4173cc8556e65206b3207f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2018-08-16 11:56:44 -0700

    test with trio

--- setup.py
+++ setup.py
@@ -175,7 +175,7 @@ extras_require = dict(
     parallel = ['ipyparallel'],
     qtconsole = ['qtconsole'],
     doc = ['Sphinx>=1.3'],
-    test = ['nose>=0.10.1', 'requests', 'testpath', 'pygments', 'nbformat', 'ipykernel', 'numpy'],
+    test = ['nose>=0.10.1', 'requests', 'testpath', 'pygments', 'nbformat', 'ipykernel', 'numpy', 'trio'],
     terminal = [],
     kernel = ['ipykernel'],
     nbformat = ['nbformat'],
