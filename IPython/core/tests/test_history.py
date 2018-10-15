073beab3a33215a9534d75ac0ca915a13f7c231d
luciana <lucianadacostamarques@gmail.com>
2018-10-15 19:58:21 -0300

    Backport PR #11401: Added skipif for sqlite3 version > 3.24.0

--- IPython/core/tests/test_history.py
+++ IPython/core/tests/test_history.py
@@ -11,6 +11,7 @@ import os
 import sys
 import tempfile
 from datetime import datetime
+import sqlite3
 
 # third party
 import nose.tools as nt
@@ -19,11 +20,13 @@ import nose.tools as nt
 from traitlets.config.loader import Config
 from IPython.utils.tempdir import TemporaryDirectory
 from IPython.core.history import HistoryManager, extract_hist_ranges
+from IPython.testing.decorators import skipif
 from IPython.utils import py3compat
 
 def setUp():
     nt.assert_equal(sys.getdefaultencoding(), "utf-8" if py3compat.PY3 else "ascii")
 
+@skipif(sqlite3.sqlite_version_info > (3,24,0))
 def test_history():
     ip = get_ipython()
     with TemporaryDirectory() as tmpdir:
@@ -41,7 +44,7 @@ def test_history():
             ip.history_manager.store_output(3)
 
             nt.assert_equal(ip.history_manager.input_hist_raw, [''] + hist)
-            
+
             # Detailed tests for _get_range_session
             grs = ip.history_manager._get_range_session
             nt.assert_equal(list(grs(start=2,stop=-1)), list(zip([0], [2], hist[1:-1])))
@@ -51,7 +54,7 @@ def test_history():
             # Check whether specifying a range beyond the end of the current
             # session results in an error (gh-804)
             ip.magic('%hist 2-500')
-            
+
             # Check that we can write non-ascii characters to a file
             ip.magic("%%hist -f %s" % os.path.join(tmpdir, "test1"))
             ip.magic("%%hist -pf %s" % os.path.join(tmpdir, "test2"))
@@ -86,6 +89,7 @@ def test_history():
             nt.assert_equal(list(gothist), expected)
 
             # Check get_hist_search
+
             gothist = ip.history_manager.search("*test*")
             nt.assert_equal(list(gothist), [(1,2,hist[1])] )
 
