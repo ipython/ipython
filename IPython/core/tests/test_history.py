6830e3d44cb82733ed32777743e033dd2faa487c
luciana <lucianadacostamarques@gmail.com>
2018-10-15 19:58:21 -0300

    added skipif for sqlite3 version > 3.24.0

--- IPython/core/tests/test_history.py
+++ IPython/core/tests/test_history.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 """Tests for the IPython tab-completion machinery.
 """
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Module imports
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 # stdlib
 import io
@@ -11,6 +11,7 @@ import os
 import sys
 import tempfile
 from datetime import datetime
+import sqlite3
 
 # third party
 import nose.tools as nt
@@ -20,9 +21,14 @@ from traitlets.config.loader import Config
 from IPython.utils.tempdir import TemporaryDirectory
 from IPython.core.history import HistoryManager, extract_hist_ranges
 
+from testing.decorators import skipif
+
+
 def setUp():
     nt.assert_equal(sys.getdefaultencoding(), "utf-8")
 
+
+@skipif(sqlite3.version_info > (3, 24, 0))
 def test_history():
     ip = get_ipython()
     with TemporaryDirectory() as tmpdir:
@@ -40,17 +46,17 @@ def test_history():
             ip.history_manager.store_output(3)
 
             nt.assert_equal(ip.history_manager.input_hist_raw, [''] + hist)
-            
+
             # Detailed tests for _get_range_session
             grs = ip.history_manager._get_range_session
-            nt.assert_equal(list(grs(start=2,stop=-1)), list(zip([0], [2], hist[1:-1])))
-            nt.assert_equal(list(grs(start=-2)), list(zip([0,0], [2,3], hist[-2:])))
-            nt.assert_equal(list(grs(output=True)), list(zip([0,0,0], [1,2,3], zip(hist, [None,None,'spam']))))
+            nt.assert_equal(list(grs(start=2, stop=-1)), list(zip([0], [2], hist[1:-1])))
+            nt.assert_equal(list(grs(start=-2)), list(zip([0, 0], [2, 3], hist[-2:])))
+            nt.assert_equal(list(grs(output=True)), list(zip([0, 0, 0], [1, 2, 3], zip(hist, [None, None, 'spam']))))
 
             # Check whether specifying a range beyond the end of the current
             # session results in an error (gh-804)
             ip.magic('%hist 2-500')
-            
+
             # Check that we can write non-ascii characters to a file
             ip.magic("%%hist -f %s" % os.path.join(tmpdir, "test1"))
             ip.magic("%%hist -pf %s" % os.path.join(tmpdir, "test2"))
@@ -66,18 +72,18 @@ def test_history():
             for i, cmd in enumerate(newcmds, start=1):
                 ip.history_manager.store_inputs(i, cmd)
             gothist = ip.history_manager.get_range(start=1, stop=4)
-            nt.assert_equal(list(gothist), list(zip([0,0,0],[1,2,3], newcmds)))
+            nt.assert_equal(list(gothist), list(zip([0, 0, 0], [1, 2, 3], newcmds)))
             # Previous session:
             gothist = ip.history_manager.get_range(-1, 1, 4)
-            nt.assert_equal(list(gothist), list(zip([1,1,1],[1,2,3], hist)))
+            nt.assert_equal(list(gothist), list(zip([1, 1, 1], [1, 2, 3], hist)))
 
             newhist = [(2, i, c) for (i, c) in enumerate(newcmds, 1)]
 
             # Check get_hist_tail
             gothist = ip.history_manager.get_tail(5, output=True,
-                                                    include_latest=True)
+                                                  include_latest=True)
             expected = [(1, 3, (hist[-1], "spam"))] \
-                + [(s, n, (c, None)) for (s, n, c) in newhist]
+                       + [(s, n, (c, None)) for (s, n, c) in newhist]
             nt.assert_equal(list(gothist), expected)
 
             gothist = ip.history_manager.get_tail(2)
@@ -85,8 +91,9 @@ def test_history():
             nt.assert_equal(list(gothist), expected)
 
             # Check get_hist_search
+
             gothist = ip.history_manager.search("*test*")
-            nt.assert_equal(list(gothist), [(1,2,hist[1])] )
+            nt.assert_equal(list(gothist), [(1, 2, hist[1])])
 
             gothist = ip.history_manager.search("*=*")
             nt.assert_equal(list(gothist),
@@ -119,14 +126,14 @@ def test_history():
                              newhist[3]])
 
             gothist = ip.history_manager.search("b*", output=True)
-            nt.assert_equal(list(gothist), [(1,3,(hist[2],"spam"))] )
+            nt.assert_equal(list(gothist), [(1, 3, (hist[2], "spam"))])
 
             # Cross testing: check that magic %save can get previous session.
             testfilename = os.path.realpath(os.path.join(tmpdir, "test.py"))
             ip.magic("save " + testfilename + " ~1/1-3")
             with io.open(testfilename, encoding='utf-8') as testfile:
                 nt.assert_equal(testfile.read(),
-                                        u"# coding: utf-8\n" + u"\n".join(hist)+u"\n")
+                                u"# coding: utf-8\n" + u"\n".join(hist) + u"\n")
 
             # Duplicate line numbers - check that it doesn't crash, and
             # gets a new session
@@ -155,6 +162,7 @@ def test_extract_hist_ranges():
     actual = list(extract_hist_ranges(instr))
     nt.assert_equal(actual, expected)
 
+
 def test_magic_rerun():
     """Simple test for %rerun (no args -> rerun last line)"""
     ip = get_ipython()
@@ -164,10 +172,12 @@ def test_magic_rerun():
     ip.run_cell("%rerun", store_history=True)
     nt.assert_equal(ip.user_ns["a"], 12)
 
+
 def test_timestamp_type():
     ip = get_ipython()
     info = ip.history_manager.get_session_info()
     nt.assert_true(isinstance(info[1], datetime))
+
 
 def test_hist_file_config():
     cfg = Config()
@@ -184,6 +194,7 @@ def test_hist_file_config():
             # On Windows, even though we close the file, we still can't
             # delete it.  I have no clue why
             pass
+
 
 def test_histmanager_disabled():
     """Ensure that disabling the history manager doesn't create a database."""
