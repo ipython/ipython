e5ec6eb286615660dee78a6a8cf0b1268649f880
Massimo Santini <massimo.santini@gmail.com>
2018-10-15 23:15:39 +0200

    Attempt to fix latex symbols in the completer

--- tools/gen_latex_symbols.py
+++ tools/gen_latex_symbols.py
@@ -18,28 +18,33 @@ if not sys.version_info[0] == 3:
 # Import the Julia LaTeX symbols
 print('Importing latex_symbols.js from Julia...')
 import requests
-url = 'https://raw.githubusercontent.com/JuliaLang/julia/master/base/latex_symbols.jl'
+url = 'https://raw.githubusercontent.com/JuliaLang/julia/master/stdlib/REPL/src/latex_symbols.jl'
 r = requests.get(url)
 
 
 # Build a list of key, value pairs
 print('Building a list of (latex, unicode) key-value pairs...')
-lines = r.text.splitlines()[60:]
-lines = [line for line in lines if '=>' in line]
-lines = [line.replace('=>',':') for line in lines]
+lines = r.text.splitlines()
 
-def line_to_tuple(line):
-    """Convert a single line of the .jl file to a 2-tuple of strings like ("\\alpha", "α")"""
-    kv = line.split(',')[0].split(':')
-#     kv = tuple(line.strip(', ').split(':'))
-    k, v = kv[0].strip(' "'), kv[1].strip(' "')
-#     if not test_ident(v):
-#         print(line)
-    return k, v
+prefixes_line = lines.index('# "font" prefixes')
+symbols_line = lines.index('# manual additions:')
 
-assert line_to_tuple('    "\\sqrt" : "\u221A",') == ('\\sqrt', '\u221A')
-lines = [line_to_tuple(line) for line in lines]
+prefix_dict = {}
+for l in lines[prefixes_line + 1: symbols_line]:
+    p = l.split()
+    if not p or p[1] == 'latex_symbols': continue
+    prefix_dict[p[1]] = p[3]
 
+idents = []
+for l in lines[symbols_line:]:
+    if not '=>' in l: continue # if it's not a def, skip
+    if '#' in l: l = l[:l.index('#')] # get rid of eol comments
+    x, y = l.strip().split('=>') 
+    if '*' in x: # if a prefix is present substitute it with its value
+        p, x = x.split('*')
+        x = prefix_dict[p][:-1] + x[1:]
+    x, y = x.split('"')[1], y.split('"')[1] # get the values in quotes
+    idents.append((x, y))
 
 # Filter out non-valid identifiers
 print('Filtering out characters that are not valid Python 3 identifiers')
@@ -53,8 +58,7 @@ def test_ident(i):
 assert test_ident("α")
 assert not test_ident('‴')
 
-valid_idents = [line for line in lines if test_ident(line[1])]
-
+valid_idents = [line for line in idents if test_ident(line[1])]
 
 # Write the `latex_symbols.py` module in the cwd
 
