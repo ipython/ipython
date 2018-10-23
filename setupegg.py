c8e64ffff659ca8d643e05c6b03c1cbc94cd151b
animeandmangalover <44209734+animeandmangalover@users.noreply.github.com>
2018-10-23 12:49:37 -0500

    Update setupegg.py

--- setupegg.py
+++ setupegg.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Wrapper to run setup.py using setuptools."""
-
+execfile('setup.py')
 # Import setuptools and call the actual setup
 import setuptools
-execfile('setup.py')
