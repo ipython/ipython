92f3e0283c7a7b9d96fea8c36c639c8c86f4ddef
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2019-08-30 09:34:34 -0700

    update colors

--- tools/release_helper.sh
+++ tools/release_helper.sh
@@ -18,31 +18,38 @@ read VERSION
 echo -n 'branch (master|X.y):'
 read branch
 
+BLACK=$(tput setaf 1)
 RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
 NOR=$(tput sgr0)
 
 echo 
-echo "updating what's new with informations from docs/source/whatsnew/pr"
+echo $BLUE"Updating what's new with informations from docs/source/whatsnew/pr"$NOR
 python tools/update_whatsnew.py
 
 echo
-echo "please move the contents of "docs/source/whatsnew/development.rst" to version-X.rst"
-echo "Press enter to continue"
+echo $BLUE"please move the contents of "docs/source/whatsnew/development.rst" to version-X.rst"$NOR
+echo $GREEN"Press enter to continue"$NOR
 read
 
 echo 
-echo "here are all the authors that contributed to this release:"
+echo $BLUE"here are all the authors that contributed to this release:"$NOR
 git log --format="%aN <%aE>" $PREV_RELEASE... | sort -u -f
 
 echo 
-echo "If you see any duplicates cancel (Ctrl-C), then edit .mailmap" Press enter to continue
+echo $BLUE"If you see any duplicates cancel (Ctrl-C), then edit .mailmap."$GREEN"Press enter to continue:"$NOR
 read
 
-echo "generating stats"
+echo $BLUE"generating stats"NOR
 python tools/github_stats.py --milestone $MILESTONE > stats.rst
 
-echo "stats.rst files generated. Please merge it with the right file (github-stats-X.rst)"
-echo "press enter to continue."
+echo $BLUE"stats.rst files generated."$GREEN"Please merge it with the right file (github-stats-X.rst)"$NOR
+echo $GREEN"press enter to continue."$NOR
 read
 
 echo "Cleaning repository"
