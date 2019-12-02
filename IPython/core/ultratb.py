7b234536d859baa939ee3a19004a2d96f86ce6cb
Quentin Peter <qaep2@cam.ac.uk>
2019-12-02 09:00:00 +0000

    fix 'direct cause' message

--- IPython/core/ultratb.py
+++ IPython/core/ultratb.py
@@ -681,15 +681,18 @@ class ListTB(TBTools):
         exception = self.get_parts_of_chained_exception(evalue)
 
         if exception and not id(exception[1]) in chained_exc_ids:
+            chained_exception_message = self.prepare_chained_exception_message(
+                evalue.__cause__)[0]
             etype, evalue, etb = exception
-            chained_exc_ids.add(id(exception[1])) # trace exception to avoid infinite 'cause' loop
+            # Trace exception to avoid infinite 'cause' loop
+            chained_exc_ids.add(id(exception[1]))
             chained_exceptions_tb_offset = 0
-            out_list = (self.structured_traceback(
-                etype, evalue, (etb, chained_exc_ids),
-                chained_exceptions_tb_offset, context)
-                        + self.prepare_chained_exception_message(
-                            evalue.__cause__)[0]
-                        + out_list)
+            out_list = (
+                self.structured_traceback(
+                    etype, evalue, (etb, chained_exc_ids),
+                    chained_exceptions_tb_offset, context)
+                + chained_exception_message
+                + out_list)
 
         return out_list
 
