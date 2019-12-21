003ab938319421376e3d573e27a27833a8e85d93
Jonathan Feinberg <jonathf@gmail.com>
2019-12-21 15:39:04 +0100

    Adding VI-mode in prompt
    From the code it is apparent that if vi-mode is enabled, it should
    display in the prompt. However, as the compare done is done against an
    Enum class, it will always return False.
    
    The solution is either to access the Enum's `.value` attribute, or to
    compare it to the declared Enum value directly. The latter is likely
    more readable.

--- IPython/terminal/prompts.py
+++ IPython/terminal/prompts.py
@@ -7,6 +7,7 @@ from IPython.core.displayhook import DisplayHook
 
 from prompt_toolkit.formatted_text import fragment_list_width, PygmentsTokens
 from prompt_toolkit.shortcuts import print_formatted_text
+from prompt_toolkit.enums import EditingMode
 
 
 class Prompts(object):
@@ -14,7 +15,7 @@ class Prompts(object):
         self.shell = shell
 
     def vi_mode(self):
-        if (getattr(self.shell.pt_app, 'editing_mode', None) == 'VI'
+        if (getattr(self.shell.pt_app, 'editing_mode', None) == EditingMode.VI
                 and self.shell.prompt_includes_vi_mode):
             return '['+str(self.shell.pt_app.app.vi_state.input_mode)[3:6]+'] '
         return ''
