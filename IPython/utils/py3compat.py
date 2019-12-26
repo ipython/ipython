464c86589599524d7bcf3faf1ed2f2686ce89c38
Pratyay Pandey <pratyayrakesh@gmail.com>
2019-12-25 18:25:13 -0800

    Remove 'annotate' function from IPython/utils/py3compat.py, an apparent relic of Python 2 compatibility time

--- IPython/utils/py3compat.py
+++ IPython/utils/py3compat.py
@@ -190,20 +190,6 @@ PY2 = not PY3
 PYPY = platform.python_implementation() == "PyPy"
 
 
-def annotate(**kwargs):
-    """Python 3 compatible function annotation for Python 2."""
-    if not kwargs:
-        raise ValueError('annotations must be provided as keyword arguments')
-    def dec(f):
-        if hasattr(f, '__annotations__'):
-            for k, v in kwargs.items():
-                f.__annotations__[k] = v
-        else:
-            f.__annotations__ = kwargs
-        return f
-    return dec
-
-
 # Parts below taken from six:
 # Copyright (c) 2010-2013 Benjamin Peterson
 #
