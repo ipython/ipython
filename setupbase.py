53d7fe3082213f31c89f2c16b39920fb4c7097ce
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2020-02-12 16:38:06 -0800

    Apply black to setupbase.py
        black --target-version py36 setupbase.py
    
    To ignore those changes when using git blame see the content of
    .git-blame-ignore-revs

--- setupbase.py
+++ setupbase.py
@@ -26,23 +26,25 @@ from glob import glob
 
 from setupext import install_data_ext
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Useful globals and utility functions
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 # A few handy globals
 isfile = os.path.isfile
 pjoin = os.path.join
 repo_root = os.path.dirname(os.path.abspath(__file__))
 
+
 def execfile(fname, globs, locs=None):
     locs = locs or globs
     with open(fname) as f:
         exec(compile(f.read(), fname, "exec"), globs, locs)
 
+
 # A little utility we'll need below, since glob() does NOT allow you to do
 # exclusion on multiple endings!
-def file_doesnt_endwith(test,endings):
+def file_doesnt_endwith(test, endings):
     """Return true if test is a file and its name does NOT end with any
     of the strings listed in endings."""
     if not isfile(test):
@@ -52,61 +54,65 @@ def file_doesnt_endwith(test,endings):
             return False
     return True
 
-#---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 # Basic project information
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 
 # release.py contains version, authors, license, url, keywords, etc.
-execfile(pjoin(repo_root, 'IPython','core','release.py'), globals())
+execfile(pjoin(repo_root, "IPython", "core", "release.py"), globals())
 
 # Create a dict with the basic information
 # This dict is eventually passed to setup after additional keys are added.
 setup_args = dict(
-      name             = name,
-      version          = version,
-      description      = description,
-      long_description = long_description,
-      author           = author,
-      author_email     = author_email,
-      url              = url,
-      license          = license,
-      platforms        = platforms,
-      keywords         = keywords,
-      classifiers      = classifiers,
-      cmdclass         = {'install_data': install_data_ext},
-      project_urls={
-          'Documentation': 'https://ipython.readthedocs.io/',
-          'Funding'      : 'https://numfocus.org/',
-          'Source'       : 'https://github.com/ipython/ipython',
-          'Tracker'      : 'https://github.com/ipython/ipython/issues',
-      }
-      )
+    name=name,
+    version=version,
+    description=description,
+    long_description=long_description,
+    author=author,
+    author_email=author_email,
+    url=url,
+    license=license,
+    platforms=platforms,
+    keywords=keywords,
+    classifiers=classifiers,
+    cmdclass={"install_data": install_data_ext},
+    project_urls={
+        "Documentation": "https://ipython.readthedocs.io/",
+        "Funding": "https://numfocus.org/",
+        "Source": "https://github.com/ipython/ipython",
+        "Tracker": "https://github.com/ipython/ipython/issues",
+    },
+)
 
 
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # Find packages
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+
 
 def find_packages():
     """
     Find all of IPython's packages.
     """
-    excludes = ['deathrow', 'quarantine']
+    excludes = ["deathrow", "quarantine"]
     packages = []
-    for dir,subdirs,files in os.walk('IPython'):
-        package = dir.replace(os.path.sep, '.')
-        if any(package.startswith('IPython.'+exc) for exc in excludes):
+    for dir, subdirs, files in os.walk("IPython"):
+        package = dir.replace(os.path.sep, ".")
+        if any(package.startswith("IPython." + exc) for exc in excludes):
             # package is to be excluded (e.g. deathrow)
             continue
-        if '__init__.py' not in files:
+        if "__init__.py" not in files:
             # not a package
             continue
         packages.append(package)
     return packages
 
-#---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 # Find package data
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+
 
 def find_package_data():
     """
@@ -114,14 +120,14 @@ def find_package_data():
     """
     # This is not enough for these things to appear in an sdist.
     # We need to muck with the MANIFEST to get this to work
-    
+
     package_data = {
-        'IPython.core' : ['profile/README*'],
-        'IPython.core.tests' : ['*.png', '*.jpg', 'daft_extension/*.py'],
-        'IPython.lib.tests' : ['*.wav'],
-        'IPython.testing.plugin' : ['*.txt'],
+        "IPython.core": ["profile/README*"],
+        "IPython.core.tests": ["*.png", "*.jpg", "daft_extension/*.py"],
+        "IPython.lib.tests": ["*.wav"],
+        "IPython.testing.plugin": ["*.txt"],
     }
-    
+
     return package_data
 
 
@@ -129,10 +135,10 @@ def check_package_data(package_data):
     """verify that package_data globs make sense"""
     print("checking package data")
     for pkg, data in package_data.items():
-        pkg_root = pjoin(*pkg.split('.'))
+        pkg_root = pjoin(*pkg.split("."))
         for d in data:
             path = pjoin(pkg_root, d)
-            if '*' in path:
+            if "*" in path:
                 assert len(glob(path)) > 0, "No files match pattern %s" % path
             else:
                 assert os.path.exists(path), "Missing package data: %s" % path
@@ -143,16 +149,19 @@ def check_package_data_first(command):
     
     Probably only needs to wrap build_py
     """
+
     class DecoratedCommand(command):
         def run(self):
             check_package_data(self.package_data)
             command.run(self)
+
     return DecoratedCommand
 
 
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # Find data files
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+
 
 def find_data_files():
     """
@@ -162,18 +171,18 @@ def find_data_files():
     """
 
     if "freebsd" in sys.platform:
-        manpagebase = pjoin('man', 'man1')
+        manpagebase = pjoin("man", "man1")
     else:
-        manpagebase = pjoin('share', 'man', 'man1')
+        manpagebase = pjoin("share", "man", "man1")
 
     # Simple file lists can be made by hand
-    manpages = [f for f in glob(pjoin('docs','man','*.1.gz')) if isfile(f)]
+    manpages = [f for f in glob(pjoin("docs", "man", "*.1.gz")) if isfile(f)]
     if not manpages:
         # When running from a source tree, the manpages aren't gzipped
-        manpages = [f for f in glob(pjoin('docs','man','*.1')) if isfile(f)]
+        manpages = [f for f in glob(pjoin("docs", "man", "*.1")) if isfile(f)]
 
     # And assemble the entire output list
-    data_files = [ (manpagebase, manpages) ]
+    data_files = [(manpagebase, manpages)]
 
     return data_files
 
@@ -181,7 +190,8 @@ def find_data_files():
 # The two functions below are copied from IPython.utils.path, so we don't need
 # to import IPython during setup, which fails on Python 3.
 
-def target_outdated(target,deps):
+
+def target_outdated(target, deps):
     """Determine whether a target is out of date.
 
     target_outdated(target,deps) -> 1/0
@@ -199,13 +209,13 @@ def target_outdated(target,deps):
     for dep in deps:
         dep_time = os.path.getmtime(dep)
         if dep_time > target_time:
-            #print "For target",target,"Dep failed:",dep # dbg
-            #print "times (dep,tar):",dep_time,target_time # dbg
+            # print "For target",target,"Dep failed:",dep # dbg
+            # print "times (dep,tar):",dep_time,target_time # dbg
             return 1
     return 0
 
 
-def target_update(target,deps,cmd):
+def target_update(target, deps, cmd):
     """Update a target with a given command given a list of dependencies.
 
     target_update(target,deps,cmd) -> runs cmd if target is outdated.
@@ -213,12 +223,14 @@ def target_update(target,deps,cmd):
     This is just a wrapper around target_outdated() which calls the given
     command if target is outdated."""
 
-    if target_outdated(target,deps):
+    if target_outdated(target, deps):
         os.system(cmd)
 
-#---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 # Find scripts
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+
 
 def find_entry_points():
     """Defines the command line entry points for IPython
@@ -231,11 +243,12 @@ def find_entry_points():
     suffixed with the Python major version number, e.g. ipython3. 
     """
     ep = [
-            'ipython%s = IPython:start_ipython',
-            'iptest%s = IPython.testing.iptestcontroller:main',
-        ]
+        "ipython%s = IPython:start_ipython",
+        "iptest%s = IPython.testing.iptestcontroller:main",
+    ]
     suffix = str(sys.version_info[0])
-    return [e % '' for e in ep] + [e % suffix for e in ep]
+    return [e % "" for e in ep] + [e % suffix for e in ep]
+
 
 script_src = """#!{executable}
 # This script was automatically generated by setup.py
@@ -243,6 +256,7 @@ if __name__ == '__main__':
     from {mod} import {func}
     {func}()
 """
+
 
 class build_scripts_entrypt(build_scripts):
     """Build the command line scripts
@@ -253,99 +267,109 @@ class build_scripts_entrypt(build_scripts):
     On Windows, this also creates .cmd wrappers for the scripts so that you can
     easily launch them from a command line.
     """
+
     def run(self):
         self.mkpath(self.build_dir)
         outfiles = []
         for script in find_entry_points():
-            name, entrypt = script.split('=')
+            name, entrypt = script.split("=")
             name = name.strip()
             entrypt = entrypt.strip()
             outfile = os.path.join(self.build_dir, name)
             outfiles.append(outfile)
-            print('Writing script to', outfile)
+            print("Writing script to", outfile)
 
-            mod, func = entrypt.split(':')
-            with open(outfile, 'w') as f:
-                f.write(script_src.format(executable=sys.executable,
-                                          mod=mod, func=func))
-            
-            if sys.platform == 'win32':
+            mod, func = entrypt.split(":")
+            with open(outfile, "w") as f:
+                f.write(
+                    script_src.format(executable=sys.executable, mod=mod, func=func)
+                )
+
+            if sys.platform == "win32":
                 # Write .cmd wrappers for Windows so 'ipython' etc. work at the
                 # command line
-                cmd_file = os.path.join(self.build_dir, name + '.cmd')
+                cmd_file = os.path.join(self.build_dir, name + ".cmd")
                 cmd = r'@"{python}" "%~dp0\{script}" %*\r\n'.format(
-                        python=sys.executable, script=name)
+                    python=sys.executable, script=name
+                )
                 log.info("Writing %s wrapper script" % cmd_file)
-                with open(cmd_file, 'w') as f:
+                with open(cmd_file, "w") as f:
                     f.write(cmd)
 
         return outfiles, outfiles
 
+
 class install_lib_symlink(Command):
     user_options = [
-        ('install-dir=', 'd', "directory to install to"),
-        ]
+        ("install-dir=", "d", "directory to install to"),
+    ]
 
     def initialize_options(self):
         self.install_dir = None
 
     def finalize_options(self):
-        self.set_undefined_options('symlink',
-                                   ('install_lib', 'install_dir'),
-                                  )
+        self.set_undefined_options(
+            "symlink", ("install_lib", "install_dir"),
+        )
 
     def run(self):
-        if sys.platform == 'win32':
+        if sys.platform == "win32":
             raise Exception("This doesn't work on Windows.")
-        pkg = os.path.join(os.getcwd(), 'IPython')
-        dest = os.path.join(self.install_dir, 'IPython')
+        pkg = os.path.join(os.getcwd(), "IPython")
+        dest = os.path.join(self.install_dir, "IPython")
         if os.path.islink(dest):
-            print('removing existing symlink at %s' % dest)
+            print("removing existing symlink at %s" % dest)
             os.unlink(dest)
-        print(f'symlinking {pkg} -> {dest}')
+        print(f"symlinking {pkg} -> {dest}")
         os.symlink(pkg, dest)
+
 
 class unsymlink(install):
     def run(self):
-        dest = os.path.join(self.install_lib, 'IPython')
+        dest = os.path.join(self.install_lib, "IPython")
         if os.path.islink(dest):
-            print('removing symlink at %s' % dest)
+            print("removing symlink at %s" % dest)
             os.unlink(dest)
         else:
-            print('No symlink exists at %s' % dest)
+            print("No symlink exists at %s" % dest)
+
 
 class install_symlinked(install):
     def run(self):
-        if sys.platform == 'win32':
+        if sys.platform == "win32":
             raise Exception("This doesn't work on Windows.")
 
         # Run all sub-commands (at least those that need to be run)
         for cmd_name in self.get_sub_commands():
             self.run_command(cmd_name)
-    
+
     # 'sub_commands': a list of commands this command might have to run to
     # get its work done.  See cmd.py for more info.
-    sub_commands = [('install_lib_symlink', lambda self:True),
-                    ('install_scripts_sym', lambda self:True),
-                   ]
+    sub_commands = [
+        ("install_lib_symlink", lambda self: True),
+        ("install_scripts_sym", lambda self: True),
+    ]
+
 
 class install_scripts_for_symlink(install_scripts):
     """Redefined to get options from 'symlink' instead of 'install'.
     
     I love distutils almost as much as I love setuptools.
     """
+
     def finalize_options(self):
-        self.set_undefined_options('build', ('build_scripts', 'build_dir'))
-        self.set_undefined_options('symlink',
-                                   ('install_scripts', 'install_dir'),
-                                   ('force', 'force'),
-                                   ('skip_build', 'skip_build'),
-                                  )
+        self.set_undefined_options("build", ("build_scripts", "build_dir"))
+        self.set_undefined_options(
+            "symlink",
+            ("install_scripts", "install_dir"),
+            ("force", "force"),
+            ("skip_build", "skip_build"),
+        )
 
 
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # VCS related
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 
 
 def git_prebuild(pkg_dir, build_cmd=build_py):
@@ -355,52 +379,59 @@ def git_prebuild(pkg_dir, build_cmd=build_py):
     
     for use in IPython.utils.sysinfo.sys_info() calls after installation.
     """
-    
+
     class MyBuildPy(build_cmd):
-        ''' Subclass to write commit data into installation tree '''
+        """ Subclass to write commit data into installation tree """
+
         def run(self):
             # loose as `.dev` is suppose to be invalid
             print("check version number")
-            loose_pep440re = re.compile(r'^(\d+)\.(\d+)\.(\d+((a|b|rc)\d+)?)(\.post\d+)?(\.dev\d*)?$')
+            loose_pep440re = re.compile(
+                r"^(\d+)\.(\d+)\.(\d+((a|b|rc)\d+)?)(\.post\d+)?(\.dev\d*)?$"
+            )
             if not loose_pep440re.match(version):
-                raise ValueError("Version number '%s' is not valid (should match [N!]N(.N)*[{a|b|rc}N][.postN][.devN])" % version)
-
+                raise ValueError(
+                    "Version number '%s' is not valid (should match [N!]N(.N)*[{a|b|rc}N][.postN][.devN])"
+                    % version
+                )
 
             build_cmd.run(self)
             # this one will only fire for build commands
-            if hasattr(self, 'build_lib'):
+            if hasattr(self, "build_lib"):
                 self._record_commit(self.build_lib)
-        
+
         def make_release_tree(self, base_dir, files):
             # this one will fire for sdist
             build_cmd.make_release_tree(self, base_dir, files)
             self._record_commit(base_dir)
-        
+
         def _record_commit(self, base_dir):
             import subprocess
-            proc = subprocess.Popen('git rev-parse --short HEAD',
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    shell=True)
+
+            proc = subprocess.Popen(
+                "git rev-parse --short HEAD",
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+            )
             repo_commit, _ = proc.communicate()
             repo_commit = repo_commit.strip().decode("ascii")
-            
-            out_pth = pjoin(base_dir, pkg_dir, 'utils', '_sysinfo.py')
+
+            out_pth = pjoin(base_dir, pkg_dir, "utils", "_sysinfo.py")
             if os.path.isfile(out_pth) and not repo_commit:
                 # nothing to write, don't clobber
                 return
-            
+
             print(f"writing git commit '{repo_commit}' to {out_pth}")
-            
+
             # remove to avoid overwriting original via hard link
             try:
                 os.remove(out_pth)
             except OSError:
                 pass
-            with open(out_pth, 'w') as out_file:
-                out_file.writelines([
-                    '# GENERATED BY setup.py\n',
-                    'commit = u"%s"\n' % repo_commit,
-                ])
-    return MyBuildPy
+            with open(out_pth, "w") as out_file:
+                out_file.writelines(
+                    ["# GENERATED BY setup.py\n", 'commit = u"%s"\n' % repo_commit,]
+                )
 
+    return MyBuildPy
