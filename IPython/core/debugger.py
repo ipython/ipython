f29b47050ab2197d8bbfd9a9bc59f03294bd89df
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-04-18 14:40:51 -0700

    Backport PR #12219: Catch ValueError

--- IPython/core/debugger.py
+++ IPython/core/debugger.py
@@ -622,7 +622,11 @@ class Pdb(OldPdb):
         Take a number as argument as an (optional) number of context line to
         print"""
         if arg:
-            context = int(arg)
+            try:
+                context = int(arg)
+            except ValueError as err:
+                self.error(err)
+                return
             self.print_stack_trace(context)
         else:
             self.print_stack_trace()
