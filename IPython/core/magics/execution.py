f1425b8ca6ce9ef9935a141b0ccb0938be425c91
palewire <ben.welsh@gmail.com>
2020-05-09 07:40:44 -0700

    Exceptions raised when executing notebooks via the %run magic command

--- IPython/core/magics/execution.py
+++ IPython/core/magics/execution.py
@@ -701,7 +701,7 @@ class ExecutionMagics(Magics):
         if filename.lower().endswith(('.ipy', '.ipynb')):
             with preserve_keys(self.shell.user_ns, '__file__'):
                 self.shell.user_ns['__file__'] = filename
-                self.shell.safe_execfile_ipy(filename)
+                self.shell.safe_execfile_ipy(filename, raise_exceptions=True)
             return
 
         # Control the response to exit() calls made by the script being run
