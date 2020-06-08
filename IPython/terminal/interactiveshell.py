c5f2d928760daef58a92b3294165220b339d822c
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-06-07 21:12:36 -0700

    Update IPython/terminal/interactiveshell.py
    Co-authored-by: meeseeksdev[bot] <24485218+meeseeksdev[bot]@users.noreply.github.com>

--- IPython/terminal/interactiveshell.py
+++ IPython/terminal/interactiveshell.py
@@ -273,7 +273,7 @@ class TerminalInteractiveShell(InteractiveShell):
     def init_display_formatter(self):
         super(TerminalInteractiveShell, self).init_display_formatter()
         # terminal only supports plain text
-        self.display_formatter.active_types = ['text/plain']
+        self.display_formatter.active_types = ["text/plain"]
 
     def init_prompt_toolkit_cli(self):
         if self.simple_prompt:
