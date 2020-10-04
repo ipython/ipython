df8a993a39909d41ef5837f5d466cfb542c01265
dswij <dharmasw@outlook.com>
2020-10-05 04:39:34 +0700

    run black

--- IPython/lib/tests/test_deepreload.py
+++ IPython/lib/tests/test_deepreload.py
@@ -12,21 +12,22 @@ from IPython.utils.syspathcontext import prepended_to_syspath
 from IPython.utils.tempdir import TemporaryDirectory
 from IPython.lib.deepreload import reload as dreload
 
+
 def test_deepreload():
     "Test that dreload does deep reloads and skips excluded modules."
     with TemporaryDirectory() as tmpdir:
         with prepended_to_syspath(tmpdir):
             tmpdirpath = Path(tmpdir)
-            with open( tmpdirpath / 'A.py' , 'w') as f:
+            with open(tmpdirpath / "A.py", "w") as f:
                 f.write("class Object(object):\n    pass\n")
-            with open( tmpdirpath / 'B.py' , 'w') as f:
+            with open(tmpdirpath / "B.py", "w") as f:
                 f.write("import A\n")
             import A
             import B
 
             # Test that A is not reloaded.
             obj = A.Object()
-            dreload(B, exclude=['A'])
+            dreload(B, exclude=["A"])
             nt.assert_true(isinstance(obj, A.Object))
 
             # Test that A is reloaded.
