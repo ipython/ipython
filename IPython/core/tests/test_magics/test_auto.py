c65510c0845069c12992e25183aea8f8a57b0123
Khaled Ismaeel <k.ismaeel@innopolis.university>
2021-05-13 02:46:09 +0300

    Refactor auto_magic test

--- IPython/core/tests/test_magics/test_auto.py
+++ IPython/core/tests/test_magics/test_auto.py
@@ -1,20 +1,23 @@
 class TestAutomagic:
-    def test_automagic(self, ipython):
+    def test_set_true(self, ipython):
         ipython.run_line_magic('automagic', 'true')
         ipython.run_cell('1 + 1')
         res = ipython.run_cell('recall')
         assert ipython.rl_next_input == '2'
-
+    
+    def test_set_false(self, ipython):
         ipython.run_line_magic('automagic', 'false')
         ipython.run_cell('1 + 1')
         res = ipython.run_cell('recall')
         assert isinstance(res.error_in_exec, Exception)
-
+    
+    def test_toggle_1(self, ipython):
         ipython.run_line_magic('automagic', '')
         ipython.run_cell('1 + 1')
         res = ipython.run_cell('recall')
         assert ipython.rl_next_input == '2'
 
+    def test_toggle_2(self, ipython):
         ipython.run_line_magic('automagic', '')
         ipython.run_cell('1 + 1')
         res = ipython.run_cell('recall')
