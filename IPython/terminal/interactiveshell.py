b18c585c730230ea66326fd34675d7e2e179676c
Artur Svistunov <18216480+madbird1304@users.noreply.github.com>
2021-08-16 00:22:30 +0300

    ' -> "

--- IPython/terminal/interactiveshell.py
+++ IPython/terminal/interactiveshell.py
@@ -202,7 +202,7 @@ class TerminalInteractiveShell(InteractiveShell):
         return self.editing_mode
 
 
-    @observe('editing_mode')
+    @observe("editing_mode")
     def _editing_mode_changed(self, change):
         u_mode = change.new.upper()
         if self.pt_app:
