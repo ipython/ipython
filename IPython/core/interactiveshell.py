e62d7600bbb43a53035180a2d84a9b5b6c78e2f1
MRCWirtz <marcus.wirtz@rwth-aachen.de>
2021-08-17 15:54:08 +0200

    Fix virtual environment user warning for lower case pathes

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -928,7 +928,7 @@ class InteractiveShell(SingletonConfigurable):
         elif len(str(p_venv)) >= 2 and str(p_venv)[1] == ":":
             p_venv = Path(str(p_venv)[2:])
 
-        if any(os.fspath(p_venv) in os.fspath(p) for p in paths):
+        if any(os.fspath(p_venv).lower() in os.fspath(p).lower() for p in paths):
             # Our exe is inside or has access to the virtualenv, don't need to do anything.
             return
 
