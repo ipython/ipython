764461856ce1f88d9e4cb6188a0bdaefaec84933
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-08-18 15:08:28 -0700

    remove nose from test_security

--- IPython/lib/tests/test_security.py
+++ IPython/lib/tests/test_security.py
@@ -1,24 +1,25 @@
 # coding: utf-8
 from IPython.lib import passwd
 from IPython.lib.security import passwd_check, salt_len
-import nose.tools as nt
 
 def test_passwd_structure():
-    p = passwd('passphrase')
-    algorithm, salt, hashed = p.split(':')
-    nt.assert_equal(algorithm, 'sha1')
-    nt.assert_equal(len(salt), salt_len)
-    nt.assert_equal(len(hashed), 40)
+    p = passwd("passphrase")
+    algorithm, salt, hashed = p.split(":")
+    assert algorithm == "sha1"
+    assert len(salt) == salt_len
+    assert len(hashed) == 40
 
 def test_roundtrip():
-    p = passwd('passphrase')
-    nt.assert_equal(passwd_check(p, 'passphrase'), True)
+    p = passwd("passphrase")
+    assert passwd_check(p, "passphrase") is True
+
 
 def test_bad():
     p = passwd('passphrase')
-    nt.assert_equal(passwd_check(p, p), False)
-    nt.assert_equal(passwd_check(p, 'a:b:c:d'), False)
-    nt.assert_equal(passwd_check(p, 'a:b'), False)
+    assert passwd_check(p, p) is False
+    assert passwd_check(p, "a:b:c:d") is False
+    assert passwd_check(p, "a:b") is False
+
 
 def test_passwd_check_unicode():
     # GH issue #4524
