7617110e9f3cf8b97991ce3727631f8ed800a571
Nikita Kniazev <nok.raven@gmail.com>
2021-11-03 22:42:00 +0300

    Add builtins._ doctest
    Defining builtins._ should not break anything outside the doctest while also should be working as expected inside the doctest.

--- IPython/testing/plugin/test_ipdoctest.py
+++ IPython/testing/plugin/test_ipdoctest.py
@@ -74,3 +74,19 @@ def doctest_multiline3():
     In [15]: h(0)
     Out[15]: -1
    """
+
+
+def doctest_builtin_underscore():
+    """Defining builtins._ should not break anything outside the doctest
+    while also should be working as expected inside the doctest.
+
+    In [1]: import builtins
+
+    In [2]: builtins._ = 42
+
+    In [3]: builtins._
+    Out[3]: 42
+
+    In [4]: _
+    Out[4]: 42
+    """
