7f364bb801ae9511908f6681120c4e89e2607ebf
Nikita Kniazev <nok.raven@gmail.com>
2021-11-14 01:54:48 +0300

    Port `deepreload` to `importlib`
    `imp` is deprecated since Python 3.3 and scheduled for removal in Python 3.12
    
    The code is severely undertested, I added several, and also fixed a bug that `modules_reloading` was not cleaned-up on an import failure.

--- IPython/lib/tests/test_deepreload.py
+++ IPython/lib/tests/test_deepreload.py
@@ -4,11 +4,14 @@
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import pytest
+import types
+
 from pathlib import Path
 
 from IPython.utils.syspathcontext import prepended_to_syspath
 from IPython.utils.tempdir import TemporaryDirectory
-from IPython.lib.deepreload import reload as dreload
+from IPython.lib.deepreload import reload as dreload, modules_reloading
 
 
 def test_deepreload():
@@ -17,9 +20,9 @@ def test_deepreload():
         with prepended_to_syspath(tmpdir):
             tmpdirpath = Path(tmpdir)
             with open(tmpdirpath / "A.py", "w") as f:
-                f.write("class Object(object):\n    pass\n")
+                f.write("class Object:\n    pass\nok = True\n")
             with open(tmpdirpath / "B.py", "w") as f:
-                f.write("import A\n")
+                f.write("import A\nassert A.ok, 'we are fine'\n")
             import A
             import B
 
@@ -28,7 +31,26 @@ def test_deepreload():
             dreload(B, exclude=["A"])
             assert isinstance(obj, A.Object) is True
 
+            # Test that an import failure will not blow-up us.
+            A.ok = False
+            with pytest.raises(AssertionError, match="we are fine"):
+                dreload(B, exclude=["A"])
+            assert len(modules_reloading) == 0
+            assert not A.ok
+
             # Test that A is reloaded.
             obj = A.Object()
+            A.ok = False
             dreload(B)
+            assert A.ok
             assert isinstance(obj, A.Object) is False
+
+
+def test_not_module():
+    pytest.raises(TypeError, dreload, "modulename")
+
+
+def test_not_in_sys_modules():
+    fake_module = types.ModuleType("fake_module")
+    with pytest.raises(ImportError, match="not in sys.modules"):
+        dreload(fake_module)
