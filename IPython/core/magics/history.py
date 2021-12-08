c22ed50325ad51c2c5ee080702d7fca96bc8b780
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2021-12-08 02:36:48 +0100

    Fix uncaught ValueError in rerun magic int conversion

--- IPython/core/magics/history.py
+++ IPython/core/magics/history.py
@@ -299,7 +299,11 @@ class HistoryMagics(Magics):
         """
         opts, args = self.parse_options(parameter_s, 'l:g:', mode='string')
         if "l" in opts:         # Last n lines
-            n = int(opts['l'])
+            try:
+                n = int(opts["l"])
+            except ValueError:
+                print("Number of lines must be an integer")
+                return
 
             if n == 0:
                 print("Requested 0 last lines - nothing to run")
