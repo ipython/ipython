55c0cedfa94e71a49cd4e60df803098f8d93daef
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-06-23 12:22:53 +0200

    debug

--- IPython/core/tests/test_debugger.py
+++ IPython/core/tests/test_debugger.py
@@ -400,12 +400,15 @@ def test_decorator_skip():
 
     child = _decorator_skip_setup()
 
+    child.expect_exact("ipython-input-8")
     child.expect_exact("3     bar(3, 4)")
     child.expect("ipdb>")
 
     child.expect("ipdb>")
     child.sendline("step")
     child.expect_exact("step")
+    child.expect_exact("--Call--")
+    child.expect_exact("ipython-input-6")
 
     child.expect_exact("1 @pdb_skipped_decorator")
 
