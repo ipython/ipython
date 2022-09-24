5bb0259a747651290f91c0384ca93492a423c82d
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2022-09-24 21:15:53 +0100

    Remove outdated header as suggested
    Co-authored-by: Matthias Bussonnier <bussonniermatthias@gmail.com>

--- IPython/utils/docs.py
+++ IPython/utils/docs.py
@@ -1,7 +1,3 @@
-# encoding: utf-8
-
-# Copyright (c) IPython Development Team.
-# Distributed under the terms of the Modified BSD License.
 import os
 
 GENERATING_DOCUMENTATION = os.environ.get("IN_SPHINX_RUN", None) == "True"
