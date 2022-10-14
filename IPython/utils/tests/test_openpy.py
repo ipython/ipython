30e91d114a0093bcfc78a59dfbbb84763cf1295e
ChandanChainani <chainanichan@gmail.com>
2022-10-15 00:10:24 +0530

    Use `pathlib` instead of `os` in IPython/utils/tests/test_openpy

--- IPython/utils/tests/test_openpy.py
+++ IPython/utils/tests/test_openpy.py
@@ -1,38 +1,34 @@
-import io
-import os.path
+from pathlib import Path
 
-from IPython.utils import openpy
+from IPython.utils.openpy import detect_encoding, read_py_file, source_to_unicode
 
-mydir = os.path.dirname(__file__)
-nonascii_path = os.path.join(mydir, "../../core/tests/nonascii.py")
+nonascii_path = Path(__file__).parent / "../../core/tests/nonascii.py"
 
 
 def test_detect_encoding():
-    with open(nonascii_path, "rb") as f:
-        enc, lines = openpy.detect_encoding(f.readline)
+    with nonascii_path.open("rb") as f:
+        enc, lines = detect_encoding(f.readline)
     assert enc == "iso-8859-5"
 
 
 def test_read_file():
-    with io.open(nonascii_path, encoding="iso-8859-5") as f:
+    with nonascii_path.open(encoding="iso-8859-5") as f:
         read_specified_enc = f.read()
-    read_detected_enc = openpy.read_py_file(nonascii_path, skip_encoding_cookie=False)
+    read_detected_enc = read_py_file(nonascii_path, skip_encoding_cookie=False)
     assert read_detected_enc == read_specified_enc
     assert "coding: iso-8859-5" in read_detected_enc
 
-    read_strip_enc_cookie = openpy.read_py_file(
-        nonascii_path, skip_encoding_cookie=True
-    )
+    read_strip_enc_cookie = read_py_file(nonascii_path, skip_encoding_cookie=True)
     assert "coding: iso-8859-5" not in read_strip_enc_cookie
 
 
 def test_source_to_unicode():
-    with io.open(nonascii_path, "rb") as f:
+    with nonascii_path.open("rb") as f:
         source_bytes = f.read()
     assert (
-        openpy.source_to_unicode(source_bytes, skip_encoding_cookie=False).splitlines()
+        source_to_unicode(source_bytes, skip_encoding_cookie=False).splitlines()
         == source_bytes.decode("iso-8859-5").splitlines()
     )
 
-    source_no_cookie = openpy.source_to_unicode(source_bytes, skip_encoding_cookie=True)
+    source_no_cookie = source_to_unicode(source_bytes, skip_encoding_cookie=True)
     assert "coding: iso-8859-5" not in source_no_cookie
