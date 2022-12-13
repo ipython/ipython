0833adb1c1fec59950622acf60ad43e32d5a51e3
Emilio Graff <1@emil.io>
2022-12-12 16:10:30 -0800

    More debug printing

--- IPython/external/qt_for_kernel.py
+++ IPython/external/qt_for_kernel.py
@@ -100,6 +100,7 @@ def get_options():
     #already imported Qt somewhere. Use that
     loaded = loaded_api()
     if loaded is not None:
+        print(f'`QtCore` already imported: {loaded=}')
         return [loaded]
 
     mpl = sys.modules.get('matplotlib', None)
