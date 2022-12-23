023a594a32450ad02d771de6f83805f3bcd1662a
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-12-23 19:04:09 +0100

    extend unicode for Python 3.12

--- IPython/core/completer.py
+++ IPython/core/completer.py
@@ -285,7 +285,7 @@ if GENERATING_DOCUMENTATION:
 # write this). With below range we cover them all, with a density of ~67%
 # biggest next gap we consider only adds up about 1% density and there are 600
 # gaps that would need hard coding.
-_UNICODE_RANGES = [(32, 0x3134b), (0xe0001, 0xe01f0)]
+_UNICODE_RANGES = [(32, 0x323B0), (0xE0001, 0xE01F0)]
 
 # Public API
 __all__ = ["Completer", "IPCompleter"]
