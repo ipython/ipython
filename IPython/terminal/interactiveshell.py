06a622cfeeb356a83c77fe3d2bfb11b88e3692fb
Emilio Graff <1@emil.io>
2023-05-03 14:13:50 -0700

    Formatting

--- IPython/terminal/interactiveshell.py
+++ IPython/terminal/interactiveshell.py
@@ -906,18 +906,20 @@ class TerminalInteractiveShell(InteractiveShell):
 
         self._atexit_once()
 
-
     _inputhook = None  # The actual event loop hook function
+
     def inputhook(self, context):
         if self._inputhook is not None:
             self._inputhook(context)
 
-    active_eventloop = None # The name of the event loop hook, e.g. "qt5"
+    active_eventloop = None  # The name of the event loop hook, e.g. "qt5"
+
     def enable_gui(self, gui=None):
         # NOTE: these two variables should be in sync; `active_eventloop` is the name and
         # `_inputhook` is the actual function.
-        assert (self.active_eventloop is None and self._inputhook is None) \
-            or (self.active_eventloop is not None and self._inputhook is not None)
+        assert (self.active_eventloop is None and self._inputhook is None) or (
+            self.active_eventloop is not None and self._inputhook is not None
+        )
 
         if gui is None:
             # Requesting to remove the event loop hook.
@@ -930,45 +932,63 @@ class TerminalInteractiveShell(InteractiveShell):
                 self.active_eventloop = self._inputhook = None
                 if PTK3:
                     import asyncio
+
                     self.pt_loop = asyncio.new_event_loop()
                 print(msg)
                 return
         else:
             # Requesting an event loop hook.
             import asyncio
+
             if self._inputhook is None:
                 # No event loop hook currently installed.
                 if gui in {"inline", "webagg"}:
                     # There's no actual hook for these.
                     return
                 else:
-                    self.active_eventloop, self._inputhook = get_inputhook_name_and_func(gui)
+                    (
+                        self.active_eventloop,
+                        self._inputhook,
+                    ) = get_inputhook_name_and_func(gui)
                     if PTK3:
-                        from prompt_toolkit.eventloop import new_eventloop_with_inputhook
+                        from prompt_toolkit.eventloop import (
+                            new_eventloop_with_inputhook,
+                        )
+
                         self.pt_loop = new_eventloop_with_inputhook(self._inputhook)
                     print(f"Installed {self.active_eventloop} event loop hook.")
             else:
+
                 def cannot_change_hook_msg():
-                    print(f"Shell is already running a gui event loop for {self.active_eventloop}. "
-                          "Call with no arguments to disable the current loop.")
+                    print(
+                        f"Shell is already running a gui event loop for {self.active_eventloop}. "
+                        "Call with no arguments to disable the current loop."
+                    )
+
                 # An event loop hook is already installed.
                 if self.active_eventloop == gui:
                     # already installed; nothing to do.
                     return
-                elif self.active_eventloop.startswith('qt'):
-                    if gui == 'qt':
+                elif self.active_eventloop.startswith("qt"):
+                    if gui == "qt":
                         # This means "requesting latest version". But you can't switch versions once
                         # you've chosen one, so we just accept this if the event loop hook is
                         # already *any* Qt version.
                         return
-                    elif gui.startswith('qt'):
+                    elif gui.startswith("qt"):
                         # Requesting a specific version of qt, but not the same version as the one
                         # installed last time. We run this code, which will let the Qt importing
                         # logic tell the user that the version cannot be switched. Effectively, we
                         # are re-hooking the same hook.
-                        self.active_eventloop, self._inputhook = get_inputhook_name_and_func(gui)
+                        (
+                            self.active_eventloop,
+                            self._inputhook,
+                        ) = get_inputhook_name_and_func(gui)
                         if PTK3:
-                            from prompt_toolkit.eventloop import new_eventloop_with_inputhook
+                            from prompt_toolkit.eventloop import (
+                                new_eventloop_with_inputhook,
+                            )
+
                             self.pt_loop = new_eventloop_with_inputhook(self._inputhook)
                         # Still print this so it's clear what's happening.
                         print(f"Installed {self.active_eventloop} event loop hook.")
