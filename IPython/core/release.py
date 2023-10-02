a2af83ddfda76f7a84a0b32700a4259596bc4683
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-10-02 10:25:24 +0200

    release 8.16.1

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -17,7 +17,7 @@
 # version
 _version_major = 8
 _version_minor = 16
-_version_patch = 0
+_version_patch = 1
 _version_extra = ".dev"
 # _version_extra = "rc1"
 _version_extra = ""  # Uncomment this for full releases
