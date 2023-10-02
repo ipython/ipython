b33bab93a2a2bad19d93b40d2dd6a7fca5138eb2
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-10-02 10:25:46 +0200

    back to dev

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -17,10 +17,10 @@
 # version
 _version_major = 8
 _version_minor = 16
-_version_patch = 1
+_version_patch = 2
 _version_extra = ".dev"
 # _version_extra = "rc1"
-_version_extra = ""  # Uncomment this for full releases
+# _version_extra = ""  # Uncomment this for full releases
 
 # Construct full version string from these.
 _ver = [_version_major, _version_minor, _version_patch]
