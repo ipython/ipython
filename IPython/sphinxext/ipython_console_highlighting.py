56f48b87bedd0a9baf238619099260e5e652bb4a
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-10-30 04:54:29 -0700

    Revert "Cleanup Python 2 compact from Lexers"

--- IPython/sphinxext/ipython_console_highlighting.py
+++ IPython/sphinxext/ipython_console_highlighting.py
@@ -20,5 +20,9 @@ def setup(app):
 # Alternatively, we could register the lexer with pygments instead. This would
 # require using setuptools entrypoints: http://pygments.org/docs/plugins
 
-highlighting.lexers["ipython"] = IPyLexer()
-highlighting.lexers["ipython3"] = IPyLexer()
+ipy2 = IPyLexer(python3=False)
+ipy3 = IPyLexer(python3=True)
+
+highlighting.lexers['ipython'] = ipy2
+highlighting.lexers['ipython2'] = ipy2
+highlighting.lexers['ipython3'] = ipy3
