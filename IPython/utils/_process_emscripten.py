1fa6a775688f4f4df82da8dd488763b48798e2c1
Hood Chatham <roberthoodchatham@gmail.com>
2024-01-31 10:15:36 -0800

    IPython/utils/_process_emscripten.py: New

---
+++ IPython/utils/_process_emscripten.py
@@ -0,0 +1,12 @@
+def system(cmd):
+    raise OSError("Not available")
+
+def getoutput(cmd):
+    raise OSError("Not available")
+
+def check_pid(cmd):
+    raise OSError("Not available")
+
+def arg_split(s, posix=False, strict=True):
+    """This one could be made to work but it's not clear if it would be useful..."""
+    raise OSError("Not available")
