f7a09f5f5fe52740825acf5a72a427922eb48dbe
Wim Jeantine-Glenn <wim.glenn@gmail.com>
2024-10-24 11:01:19 -0500

    Fix re pattern pretty print
    Alternative to #14558

--- IPython/lib/pretty.py
+++ IPython/lib/pretty.py
@@ -724,7 +724,7 @@ class _ReFlags:
 
     def _repr_pretty_(self, p, cycle):
         done_one = False
-        for flag in ('TEMPLATE', 'IGNORECASE', 'LOCALE', 'MULTILINE', 'DOTALL',
+        for flag in ('IGNORECASE', 'LOCALE', 'MULTILINE', 'DOTALL',
             'UNICODE', 'VERBOSE', 'DEBUG'):
             if self.value & getattr(re, flag):
                 if done_one:
