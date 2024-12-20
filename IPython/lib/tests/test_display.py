7365aa071f8ada07727d2799864f57700222bbab
M Bussonnier <bussonniermatthias@gmail.com>
2024-12-20 10:38:43 +0100

    A Large number of code updates.
    Mostly starting to run ruff on the codebase excluding tests.

--- IPython/lib/tests/test_display.py
+++ IPython/lib/tests/test_display.py
@@ -56,21 +56,21 @@ def test_warning_on_non_existent_path_FileLink():
 def test_existing_path_FileLink():
     """FileLink: Calling _repr_html_ functions as expected on existing filepath
     """
-    tf = NamedTemporaryFile()
-    fl = display.FileLink(tf.name)
-    actual = fl._repr_html_()
-    expected = "<a href='%s' target='_blank'>%s</a><br>" % (tf.name, tf.name)
-    assert actual == expected
+    with NamedTemporaryFile() as tf:
+        fl = display.FileLink(tf.name)
+        actual = fl._repr_html_()
+        expected = "<a href='%s' target='_blank'>%s</a><br>" % (tf.name, tf.name)
+        assert actual == expected
 
 
 def test_existing_path_FileLink_repr():
     """FileLink: Calling repr() functions as expected on existing filepath
     """
-    tf = NamedTemporaryFile()
-    fl = display.FileLink(tf.name)
-    actual = repr(fl)
-    expected = tf.name
-    assert actual == expected
+    with NamedTemporaryFile() as tf:
+        fl = display.FileLink(tf.name)
+        actual = repr(fl)
+        expected = tf.name
+        assert actual == expected
 
 
 def test_error_on_directory_to_FileLink():
@@ -98,98 +98,104 @@ def test_existing_path_FileLinks():
     """FileLinks: Calling _repr_html_ functions as expected on existing dir
     """
     td = mkdtemp()
-    tf1 = NamedTemporaryFile(dir=td)
-    tf2 = NamedTemporaryFile(dir=td)
-    fl = display.FileLinks(td)
-    actual = fl._repr_html_()
-    actual = actual.split('\n')
-    actual.sort()
-    # the links should always have forward slashes, even on windows, so replace
-    # backslashes with forward slashes here
-    expected = ["%s/<br>" % td,
-                "&nbsp;&nbsp;<a href='%s' target='_blank'>%s</a><br>" %\
-                 (tf2.name.replace("\\","/"),split(tf2.name)[1]),
-                "&nbsp;&nbsp;<a href='%s' target='_blank'>%s</a><br>" %\
-                 (tf1.name.replace("\\","/"),split(tf1.name)[1])]
-    expected.sort()
-    # We compare the sorted list of links here as that's more reliable
-    assert actual == expected
+    with NamedTemporaryFile(dir=td) as tf1, NamedTemporaryFile(dir=td) as tf2:
+        fl = display.FileLinks(td)
+        actual = fl._repr_html_()
+        actual = actual.split("\n")
+        actual.sort()
+        # the links should always have forward slashes, even on windows, so replace
+        # backslashes with forward slashes here
+        expected = [
+            "%s/<br>" % td,
+            "&nbsp;&nbsp;<a href='%s' target='_blank'>%s</a><br>"
+            % (tf2.name.replace("\\", "/"), split(tf2.name)[1]),
+            "&nbsp;&nbsp;<a href='%s' target='_blank'>%s</a><br>"
+            % (tf1.name.replace("\\", "/"), split(tf1.name)[1]),
+        ]
+        expected.sort()
+        # We compare the sorted list of links here as that's more reliable
+        assert actual == expected
 
 
 def test_existing_path_FileLinks_alt_formatter():
     """FileLinks: Calling _repr_html_ functions as expected w/ an alt formatter
     """
     td = mkdtemp()
-    tf1 = NamedTemporaryFile(dir=td)
-    tf2 = NamedTemporaryFile(dir=td)
-    def fake_formatter(dirname,fnames,included_suffixes):
-        return ["hello","world"]
-    fl = display.FileLinks(td,notebook_display_formatter=fake_formatter)
-    actual = fl._repr_html_()
-    actual = actual.split('\n')
-    actual.sort()
-    expected = ["hello","world"]
-    expected.sort()
-    # We compare the sorted list of links here as that's more reliable
-    assert actual == expected
+    with NamedTemporaryFile(dir=td), NamedTemporaryFile(dir=td):
+
+        def fake_formatter(dirname, fnames, included_suffixes):
+            return ["hello", "world"]
+
+        fl = display.FileLinks(td, notebook_display_formatter=fake_formatter)
+        actual = fl._repr_html_()
+        actual = actual.split("\n")
+        actual.sort()
+        expected = ["hello", "world"]
+        expected.sort()
+        # We compare the sorted list of links here as that's more reliable
+        assert actual == expected
 
 
 def test_existing_path_FileLinks_repr():
     """FileLinks: Calling repr() functions as expected on existing directory """
     td = mkdtemp()
-    tf1 = NamedTemporaryFile(dir=td)
-    tf2 = NamedTemporaryFile(dir=td)
-    fl = display.FileLinks(td)
-    actual = repr(fl)
-    actual = actual.split('\n')
-    actual.sort()
-    expected = ['%s/' % td, '  %s' % split(tf1.name)[1],'  %s' % split(tf2.name)[1]]
-    expected.sort()
-    # We compare the sorted list of links here as that's more reliable
-    assert actual == expected
+    with NamedTemporaryFile(dir=td) as tf1, NamedTemporaryFile(dir=td) as tf2:
+        fl = display.FileLinks(td)
+        actual = repr(fl)
+        actual = actual.split("\n")
+        actual.sort()
+        expected = [
+            "%s/" % td,
+            "  %s" % split(tf1.name)[1],
+            "  %s" % split(tf2.name)[1],
+        ]
+        expected.sort()
+        # We compare the sorted list of links here as that's more reliable
+        assert actual == expected
 
 
 def test_existing_path_FileLinks_repr_alt_formatter():
     """FileLinks: Calling repr() functions as expected w/ alt formatter
     """
     td = mkdtemp()
-    tf1 = NamedTemporaryFile(dir=td)
-    tf2 = NamedTemporaryFile(dir=td)
-    def fake_formatter(dirname,fnames,included_suffixes):
-        return ["hello","world"]
-    fl = display.FileLinks(td,terminal_display_formatter=fake_formatter)
-    actual = repr(fl)
-    actual = actual.split('\n')
-    actual.sort()
-    expected = ["hello","world"]
-    expected.sort()
-    # We compare the sorted list of links here as that's more reliable
-    assert actual == expected
+    with NamedTemporaryFile(dir=td), NamedTemporaryFile(dir=td):
+
+        def fake_formatter(dirname, fnames, included_suffixes):
+            return ["hello", "world"]
+
+        fl = display.FileLinks(td, terminal_display_formatter=fake_formatter)
+        actual = repr(fl)
+        actual = actual.split("\n")
+        actual.sort()
+        expected = ["hello", "world"]
+        expected.sort()
+        # We compare the sorted list of links here as that's more reliable
+        assert actual == expected
 
 
 def test_error_on_file_to_FileLinks():
     """FileLinks: Raises error when passed file
     """
     td = mkdtemp()
-    tf1 = NamedTemporaryFile(dir=td)
-    pytest.raises(ValueError, display.FileLinks, tf1.name)
+    with NamedTemporaryFile(dir=td) as tf:
+        pytest.raises(ValueError, display.FileLinks, tf.name)
 
 
 def test_recursive_FileLinks():
     """FileLinks: Does not recurse when recursive=False
     """
     td = mkdtemp()
-    tf = NamedTemporaryFile(dir=td)
-    subtd = mkdtemp(dir=td)
-    subtf = NamedTemporaryFile(dir=subtd)
-    fl = display.FileLinks(td)
-    actual = str(fl)
-    actual = actual.split('\n')
-    assert len(actual) == 4, actual
-    fl = display.FileLinks(td, recursive=False)
-    actual = str(fl)
-    actual = actual.split('\n')
-    assert len(actual) == 2, actual
+    with NamedTemporaryFile(dir=td):
+        subtd = mkdtemp(dir=td)
+        with NamedTemporaryFile(dir=subtd):
+            fl = display.FileLinks(td)
+            actual = str(fl)
+            actual = actual.split("\n")
+            assert len(actual) == 4, actual
+            fl = display.FileLinks(td, recursive=False)
+            actual = str(fl)
+            actual = actual.split("\n")
+            assert len(actual) == 2, actual
 
 def test_audio_from_file():
     path = pjoin(dirname(__file__), 'test.wav')
