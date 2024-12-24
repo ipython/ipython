177b14aa58f964bb3f2b0f063b3a361b104c15ad
Stephen Macke <stephen.macke@gmail.com>
2024-12-23 21:08:15 -0800

    token at cursor improvements

--- IPython/utils/tokenutil.py
+++ IPython/utils/tokenutil.py
@@ -2,20 +2,29 @@
 
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
-
-from collections import namedtuple
-from io import StringIO
-from keyword import iskeyword
+from __future__ import annotations
 
 import tokenize
+from io import StringIO
+from keyword import iskeyword
 from tokenize import TokenInfo
-from typing import List, Optional
+from typing import Generator, NamedTuple
 
 
-Token = namedtuple('Token', ['token', 'text', 'start', 'end', 'line'])
+class Token(NamedTuple):
+    token: int
+    text: str
+    start: int
+    end: int
+    line: str
 
-def generate_tokens(readline):
+
+def generate_tokens(text_or_readline) -> Generator[TokenInfo, None, None]:
     """wrap generate_tkens to catch EOF errors"""
+    if isinstance(text_or_readline, str):
+        readline = StringIO(text_or_readline).readline
+    else:
+        readline = text_or_readline
     try:
         for token in tokenize.generate_tokens(readline):
             yield token
@@ -25,7 +34,7 @@ def generate_tokens(readline):
 
 
 def generate_tokens_catch_errors(
-    readline, extra_errors_to_catch: Optional[List[str]] = None
+    readline, extra_errors_to_catch: list[str] | None = None
 ):
     default_errors_to_catch = [
         "unterminated string literal",
@@ -35,7 +44,7 @@ def generate_tokens_catch_errors(
     assert extra_errors_to_catch is None or isinstance(extra_errors_to_catch, list)
     errors_to_catch = default_errors_to_catch + (extra_errors_to_catch or [])
 
-    tokens: List[TokenInfo] = []
+    tokens: list[TokenInfo] = []
     try:
         for token in tokenize.generate_tokens(readline):
             tokens.append(token)
@@ -49,13 +58,13 @@ def generate_tokens_catch_errors(
             else:
                 start = end = (1, 0)
                 line = ""
-            yield tokenize.TokenInfo(tokenize.ERRORTOKEN, "", start, end, line)
+            yield TokenInfo(tokenize.ERRORTOKEN, "", start, end, line)
         else:
             # Catch EOF
             raise
 
 
-def line_at_cursor(cell, cursor_pos=0):
+def line_at_cursor(cell: str, cursor_pos: int = 0) -> tuple[str, int]:
     """Return the line in a cell at a given cursor position
 
     Used for calling line-based APIs that don't support multi-line input, yet.
@@ -86,7 +95,7 @@ def line_at_cursor(cell, cursor_pos=0):
         offset = next_offset
     else:
         line = ""
-    return (line, offset)
+    return line, offset
 
 
 def token_at_cursor(cell: str, cursor_pos: int = 0):
@@ -104,15 +113,19 @@ def token_at_cursor(cell: str, cursor_pos: int = 0):
     cursor_pos : int
         The location of the cursor in the block where the token should be found
     """
-    names: List[str] = []
-    tokens: List[Token] = []
-    call_names = []
-    
-    offsets = {1: 0} # lines start at 1
-    for tup in generate_tokens(StringIO(cell).readline):
-        
-        tok = Token(*tup)
-        
+    names: list[str] = []
+    call_names: list[str] = []
+    closing_call_name: str | None = None
+    most_recent_outer_name: str | None = None
+
+    offsets = {1: 0}  # lines start at 1
+    intersects_with_cursor = False
+    cur_token_is_name = False
+    tokens: list[Token] = [Token(*tup) for tup in generate_tokens(cell)]
+    if not tokens:
+        return ""
+    next_tokens: list[Token | None] = tokens[1:] + [None]
+    for tok, next_tok in zip(tokens, next_tokens):
         # token, text, start, end, line = tup
         start_line, start_col = tok.start
         end_line, end_col = tok.end
@@ -121,40 +134,69 @@ def token_at_cursor(cell: str, cursor_pos: int = 0):
             lines = tok.line.splitlines(True)
             for lineno, line in enumerate(lines, start_line + 1):
                 if lineno not in offsets:
-                    offsets[lineno] = offsets[lineno-1] + len(line)
+                    offsets[lineno] = offsets[lineno - 1] + len(line)
+
+        closing_call_name = None
         
         offset = offsets[start_line]
-        # allow '|foo' to find 'foo' at the beginning of a line
-        boundary = cursor_pos + 1 if start_col == 0 else cursor_pos
-        if offset + start_col >= boundary:
+        if offset + start_col > cursor_pos:
             # current token starts after the cursor,
             # don't consume it
             break
         
-        if tok.token == tokenize.NAME and not iskeyword(tok.text):
-            if names and tokens and tokens[-1].token == tokenize.OP and tokens[-1].text == '.':
+        if cur_token_is_name := tok.token == tokenize.NAME and not iskeyword(tok.text):
+            if (
+                names
+                and tokens
+                and tokens[-1].token == tokenize.OP
+                and tokens[-1].text == "."
+            ):
                 names[-1] = "%s.%s" % (names[-1], tok.text)
             else:
                 names.append(tok.text)
-        elif tok.token == tokenize.OP:
-            if tok.text == '=' and names:
+            if (
+                next_tok is not None
+                and next_tok.token == tokenize.OP
+                and next_tok.text == "="
+            ):
                 # don't inspect the lhs of an assignment
                 names.pop(-1)
-            if tok.text == '(' and names:
+                cur_token_is_name = False
+            if not call_names:
+                most_recent_outer_name = names[-1] if names else None
+        elif tok.token == tokenize.OP:
+            if tok.text == "(" and names:
                 # if we are inside a function call, inspect the function
                 call_names.append(names[-1])
-            elif tok.text == ')' and call_names:
-                call_names.pop(-1)
+            elif tok.text == ")" and call_names:
+                # keep track of the most recently popped call_name from the stack
+                closing_call_name = call_names.pop(-1)
         
         tokens.append(tok)
         
         if offsets[end_line] + end_col > cursor_pos:
             # we found the cursor, stop reading
+            # if the current token intersects directly, use it instead of the call token
+            intersects_with_cursor = offsets[start_line] + start_col <= cursor_pos
             break
         
-    if call_names:
+    if cur_token_is_name and intersects_with_cursor:
+        return names[-1]
+    # if the cursor isn't directly over a name token, use the most recent
+    # call name if we can find one
+    elif closing_call_name:
+        # if we're on a ")", use the most recently popped call name
+        return closing_call_name
+    elif call_names:
+        # otherwise, look for the most recent call name in the stack
         return call_names[-1]
+    elif most_recent_outer_name:
+        # if we've popped all the call names, use the most recently-seen
+        # outer name
+        return most_recent_outer_name
     elif names:
+        # failing that, use the most recently seen name
         return names[-1]
     else:
-        return ''
+        # give up
+        return ""
