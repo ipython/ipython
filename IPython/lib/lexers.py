30d9b1fe0af959aefa976d90b97e79f8db03e3e7
Thomas Kluyver <thomas@kluyver.me.uk>
2025-01-15 17:07:34 +0000

    Reformat remaining IPython.lib.lexers

--- IPython/lib/lexers.py
+++ IPython/lib/lexers.py
@@ -4,13 +4,13 @@ The IPython lexers are now a separate package, ipython-pygments-lexers.
 
 Importing from here is deprecated and may break in the future.
 """
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (c) 2013, the IPython Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 from ipython_pygments_lexers import (
     IPythonLexer,
@@ -18,11 +18,15 @@ from ipython_pygments_lexers import (
     IPythonPartialTracebackLexer,
     IPythonTracebackLexer,
     IPythonConsoleLexer,
-    IPyLexer
+    IPyLexer,
 )
 
 
-__all__ = ['IPython3Lexer', 'IPythonLexer',
-           'IPythonPartialTracebackLexer', 'IPythonTracebackLexer',
-           'IPythonConsoleLexer', 'IPyLexer']
-
+__all__ = [
+    "IPython3Lexer",
+    "IPythonLexer",
+    "IPythonPartialTracebackLexer",
+    "IPythonTracebackLexer",
+    "IPythonConsoleLexer",
+    "IPyLexer",
+]
