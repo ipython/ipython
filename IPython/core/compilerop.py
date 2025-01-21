718da708020286b78f01d9d8e234921274e748ee
M Bussonnier <bussonniermatthias@gmail.com>
2025-01-21 16:33:21 +0100

    Remove check_linecache_ipython – deprecated since 8.6
    Part of the spring cleaning for IPython 9.0

--- IPython/core/compilerop.py
+++ IPython/core/compilerop.py
@@ -190,25 +190,3 @@ class CachingCompiler(codeop.Compile):
             # turn off only the bits we turned on so that something like
             # __future__ that set flags stays.
             self.flags &= ~turn_on_bits
-
-
-def check_linecache_ipython(*args):
-    """Deprecated since IPython 8.6.  Call linecache.checkcache() directly.
-
-    It was already not necessary to call this function directly.  If no
-    CachingCompiler had been created, this function would fail badly.  If
-    an instance had been created, this function would've been monkeypatched
-    into place.
-
-    As of IPython 8.6, the monkeypatching has gone away entirely.  But there
-    were still internal callers of this function, so maybe external callers
-    also existed?
-    """
-    import warnings
-
-    warnings.warn(
-        "Deprecated Since IPython 8.6, Just call linecache.checkcache() directly.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    linecache.checkcache()
