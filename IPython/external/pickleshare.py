44b6e4157e877bdec70e2b318a97c49807fccc0f
M Bussonnier <bussonniermatthias@gmail.com>
2025-02-03 10:03:42 +0100

    reformat and ignore mypy in pickleshare

--- IPython/external/pickleshare.py
+++ IPython/external/pickleshare.py
@@ -44,7 +44,8 @@ except ImportError:
     # Python 2 backport
     from pathlib2 import Path
 
-import os,stat,time
+import os, stat, time
+
 try:
     import collections.abc as collections_abc
 except ImportError:
@@ -61,15 +62,19 @@ if sys.version_info[0] >= 3:
 else:
     string_types = (str, unicode)
 
+
 def gethashfile(key):
     return ("%02x" % abs(hash(key) % 256))[-2:]
 
+
 _sentinel = object()
 
+
 class PickleShareDB(collections_abc.MutableMapping):
-    """ The main 'connection' object for PickleShare database """
-    def __init__(self,root):
-        """ Return a db object that will manage the specied directory"""
+    """The main 'connection' object for PickleShare database"""
+
+    def __init__(self, root):
+        """Return a db object that will manage the specied directory"""
         if not isinstance(root, string_types):
             root = str(root)
         root = os.path.abspath(os.path.expanduser(root))
@@ -85,12 +90,11 @@ class PickleShareDB(collections_abc.MutableMapping):
         # cache has { 'key' : (obj, orig_mod_time) }
         self.cache = {}
 
-
-    def __getitem__(self,key):
-        """ db['key'] reading """
+    def __getitem__(self, key):
+        """db['key'] reading"""
         fil = self.root / key
         try:
-            mtime = (fil.stat()[stat.ST_MTIME])
+            mtime = fil.stat()[stat.ST_MTIME]
         except OSError:
             raise KeyError(key)
 
@@ -103,18 +107,18 @@ class PickleShareDB(collections_abc.MutableMapping):
         except:
             raise KeyError(key)
 
-        self.cache[fil] = (obj,mtime)
+        self.cache[fil] = (obj, mtime)
         return obj
 
-    def __setitem__(self,key,value):
-        """ db['key'] = 5 """
+    def __setitem__(self, key, value):
+        """db['key'] = 5"""
         fil = self.root / key
         parent = fil.parent
         if parent and not parent.is_dir():
             parent.mkdir(parents=True)
         # We specify protocol 2, so that we can mostly go between Python 2
         # and Python 3. We can upgrade to protocol 3 when Python 2 is obsolete.
-        with fil.open('wb') as f:
+        with fil.open("wb") as f:
             pickle.dump(value, f, protocol=2)
         try:
             self.cache[fil] = (value, fil.stat().st_mtime)
@@ -123,24 +127,22 @@ class PickleShareDB(collections_abc.MutableMapping):
                 raise
 
     def hset(self, hashroot, key, value):
-        """ hashed set """
+        """hashed set"""
         hroot = self.root / hashroot
         if not hroot.is_dir():
             hroot.mkdir()
         hfile = hroot / gethashfile(key)
         d = self.get(hfile, {})
-        d.update( {key : value})
+        d.update({key: value})
         self[hfile] = d
 
-
-
-    def hget(self, hashroot, key, default = _sentinel, fast_only = True):
-        """ hashed get """
+    def hget(self, hashroot, key, default=_sentinel, fast_only=True):
+        """hashed get"""
         hroot = self.root / hashroot
         hfile = hroot / gethashfile(key)
 
-        d = self.get(hfile, _sentinel )
-        #print "got dict",d,"from",hfile
+        d = self.get(hfile, _sentinel)
+        # print "got dict",d,"from",hfile
         if d is _sentinel:
             if fast_only:
                 if default is _sentinel:
@@ -154,11 +156,11 @@ class PickleShareDB(collections_abc.MutableMapping):
         return d.get(key, default)
 
     def hdict(self, hashroot):
-        """ Get all data contained in hashed category 'hashroot' as dict """
+        """Get all data contained in hashed category 'hashroot' as dict"""
         hfiles = self.keys(hashroot + "/*")
         hfiles.sort()
-        last = len(hfiles) and hfiles[-1] or ''
-        if last.endswith('xx'):
+        last = len(hfiles) and hfiles[-1] or ""
+        if last.endswith("xx"):
             # print "using xx"
             hfiles = [last] + hfiles[:-1]
 
@@ -169,7 +171,7 @@ class PickleShareDB(collections_abc.MutableMapping):
             try:
                 all.update(self[f])
             except KeyError:
-                print("Corrupt",f,"deleted - hset is not threadsafe!")
+                print("Corrupt", f, "deleted - hset is not threadsafe!")
                 del self[f]
 
             self.uncache(f)
@@ -177,7 +179,7 @@ class PickleShareDB(collections_abc.MutableMapping):
         return all
 
     def hcompress(self, hashroot):
-        """ Compress category 'hashroot', so hset is fast again
+        """Compress category 'hashroot', so hset is fast again
 
         hget will fail if fast_only is True for compressed items (that were
         hset before hcompress).
@@ -190,19 +192,17 @@ class PickleShareDB(collections_abc.MutableMapping):
             all.update(self[f])
             self.uncache(f)
 
-        self[hashroot + '/xx'] = all
+        self[hashroot + "/xx"] = all
         for f in hfiles:
             p = self.root / f
-            if p.name == 'xx':
+            if p.name == "xx":
                 continue
             p.unlink()
 
-
-
-    def __delitem__(self,key):
-        """ del db["key"] """
+    def __delitem__(self, key):
+        """del db["key"]"""
         fil = self.root / key
-        self.cache.pop(fil,None)
+        self.cache.pop(fil, None)
         try:
             fil.unlink()
         except OSError:
@@ -211,14 +211,14 @@ class PickleShareDB(collections_abc.MutableMapping):
             pass
 
     def _normalized(self, p):
-        """ Make a key suitable for user's eyes """
-        return str(p.relative_to(self.root)).replace('\\','/')
+        """Make a key suitable for user's eyes"""
+        return str(p.relative_to(self.root)).replace("\\", "/")
 
-    def keys(self, globpat = None):
-        """ All keys in DB, or all keys matching a glob"""
+    def keys(self, globpat=None):
+        """All keys in DB, or all keys matching a glob"""
 
         if globpat is None:
-            files = self.root.rglob('*')
+            files = self.root.rglob("*")
         else:
             files = self.root.glob(globpat)
         return [self._normalized(p) for p in files if p.is_file()]
@@ -229,8 +229,8 @@ class PickleShareDB(collections_abc.MutableMapping):
     def __len__(self):
         return len(self.keys())
 
-    def uncache(self,*items):
-        """ Removes all, or specified items from cache
+    def uncache(self, *items):
+        """Removes all, or specified items from cache
 
         Use this after reading a large amount of large objects
         to free up memory, when you won't be needing the objects
@@ -240,10 +240,10 @@ class PickleShareDB(collections_abc.MutableMapping):
         if not items:
             self.cache = {}
         for it in items:
-            self.cache.pop(it,None)
+            self.cache.pop(it, None)
 
-    def waitget(self,key, maxwaittime = 60 ):
-        """ Wait (poll) for a key to get a value
+    def waitget(self, key, maxwaittime=60):
+        """Wait (poll) for a key to get a value
 
         Will wait for `maxwaittime` seconds before raising a KeyError.
         The call exits normally if the `key` field in db gets a value
@@ -270,21 +270,20 @@ class PickleShareDB(collections_abc.MutableMapping):
                 raise KeyError(key)
 
             time.sleep(wtimes[tries])
-            waited+=wtimes[tries]
-            if tries < len(wtimes) -1:
-                tries+=1
+            waited += wtimes[tries]
+            if tries < len(wtimes) - 1:
+                tries += 1
 
-    def getlink(self,folder):
-        """ Get a convenient link for accessing items  """
+    def getlink(self, folder):
+        """Get a convenient link for accessing items"""
         return PickleShareLink(self, folder)
 
     def __repr__(self):
         return "PickleShareDB('%s')" % self.root
 
 
-
 class PickleShareLink:
-    """ A shortdand for accessing nested PickleShare data conveniently.
+    """A shortdand for accessing nested PickleShare data conveniently.
 
     Created through PickleShareDB.getlink(), example::
 
@@ -293,23 +292,30 @@ class PickleShareLink:
         lnk.bar = lnk.foo + 5
 
     """
-    def __init__(self, db, keydir ):
+
+    def __init__(self, db, keydir):
         self.__dict__.update(locals())
 
-    def __getattr__(self,key):
-        return self.__dict__['db'][self.__dict__['keydir']+'/' + key]
-    def __setattr__(self,key,val):
-        self.db[self.keydir+'/' + key] = val
+    def __getattr__(self, key):
+        return self.__dict__["db"][self.__dict__["keydir"] + "/" + key]
+
+    def __setattr__(self, key, val):
+        self.db[self.keydir + "/" + key] = val
+
     def __repr__(self):
-        db = self.__dict__['db']
-        keys = db.keys( self.__dict__['keydir'] +"/*")
+        db = self.__dict__["db"]
+        keys = db.keys(self.__dict__["keydir"] + "/*")
         return "<PickleShareLink '%s': %s>" % (
-            self.__dict__['keydir'],
-            ";".join([Path(k).basename() for k in keys]))
+            self.__dict__["keydir"],
+            ";".join([Path(k).basename() for k in keys]),
+        )
+
 
 def main():
     import textwrap
-    usage = textwrap.dedent("""\
+
+    usage = textwrap.dedent(
+        """\
     pickleshare - manage PickleShare databases
 
     Usage:
@@ -317,35 +323,39 @@ def main():
         pickleshare dump /path/to/db > dump.txt
         pickleshare load /path/to/db < dump.txt
         pickleshare test /path/to/db
-    """)
+    """
+    )
     DB = PickleShareDB
     import sys
+
     if len(sys.argv) < 2:
         print(usage)
         return
 
     cmd = sys.argv[1]
     args = sys.argv[2:]
-    if cmd == 'dump':
-        if not args: args= ['.']
+    if cmd == "dump":
+        if not args:
+            args = ["."]
         db = DB(args[0])
         import pprint
+
         pprint.pprint(db.items())
-    elif cmd == 'load':
+    elif cmd == "load":
         cont = sys.stdin.read()
         db = DB(args[0])
         data = eval(cont)
         db.clear()
-        for k,v in db.items():
+        for k, v in db.items():
             db[k] = v
-    elif cmd == 'testwait':
+    elif cmd == "testwait":
         db = DB(args[0])
         db.clear()
-        print(db.waitget('250'))
-    elif cmd == 'test':
+        print(db.waitget("250"))
+    elif cmd == "test":
         test()
         stress()
 
-if __name__== "__main__":
-    main()
 
+if __name__ == "__main__":
+    main()
