eb6a602e191e879ab4eb2119bd408862621c5bbe
Darshan808 <pranishpoudel10@gmail.com>
2025-03-30 00:15:16 +0545

    reset-execution-count-to-1

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -922,6 +922,7 @@ def test_notebook_export_json_with_output():
     _ip = get_ipython()
     _ip.history_manager.reset()
     _ip.colors = "neutral"
+    _ip.execution_count = 1
 
     try:
         commands = [
