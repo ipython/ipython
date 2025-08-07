2d1c243db8fa55be108f2fb3daa8127dcff27aa5
krassowski <5832902+krassowski@users.noreply.github.com>
2025-08-07 13:50:47 +0100

    Populate notebook metadata when exporting with `%notebook` magic

--- IPython/core/magics/basic.py
+++ IPython/core/magics/basic.py
@@ -4,6 +4,7 @@
 from logging import error
 import io
 import os
+import platform
 from pprint import pformat
 import sys
 from warnings import warn
@@ -603,7 +604,28 @@ Currently the magic system has the following functions:""",
                 )
             cells.append(cell)
 
-        nb = v4.new_notebook(cells=cells)
+        nb = v4.new_notebook(
+            cells=cells,
+            metadata={
+                "kernelspec": {
+                    "display_name": "Python 3 (ipykernel)",
+                    "language": "python",
+                    "name": "python3",
+                },
+                "language_info": {
+                    "codemirror_mode": {
+                        "name": "ipython",
+                        "version": 3
+                    },
+                    "file_extension": ".py",
+                    "mimetype": "text/x-python",
+                    "name": "python",
+                    "nbconvert_exporter": "python",
+                    "pygments_lexer": "ipython3",
+                    "version": platform.python_version()
+                }
+            }
+        )
         with io.open(outfname, "w", encoding="utf-8") as f:
             write(nb, f, version=4)
 
