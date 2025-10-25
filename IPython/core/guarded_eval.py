a765762227ce30824895460d43a8027150585ed9
Darshan808 <pranishpoudel10@gmail.com>
2025-10-25 20:40:55 +0545

    fix-locals-scoping

--- IPython/core/guarded_eval.py
+++ IPython/core/guarded_eval.py
@@ -736,8 +736,13 @@ def eval_node(node: Union[ast.AST, None], context: EvaluationContext):
         return None
     if isinstance(node, ast.ClassDef):
         # TODO support class decorators?
-        class_locals = context.transient_locals.copy()
-        class_context = context.replace(transient_locals=class_locals)
+        # class_locals = context.transient_locals.copy()
+        class_locals = {}
+        outer_locals = context.locals.copy()
+        outer_locals.update(context.transient_locals)
+        class_context = context.replace(
+            transient_locals=class_locals, locals=outer_locals
+        )
         class_context.class_transients = class_locals
         for child_node in node.body:
             eval_node(child_node, class_context)
