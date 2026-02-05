63476b25df9b0404eed3aa6e653a90acf8f541f5
M Bussonnier <bussonniermatthias@gmail.com>
2026-02-05 15:23:48 +0100

    update the unicode ranges

--- IPython/core/completer.py
+++ IPython/core/completer.py
@@ -270,7 +270,7 @@ except ImportError:
 # write this). With below range we cover them all, with a density of ~67%
 # biggest next gap we consider only adds up about 1% density and there are 600
 # gaps that would need hard coding.
-_UNICODE_RANGES = [(32, 0x323B0), (0xE0001, 0xE01F0)]
+_UNICODE_RANGES = [(32, 0x3347A), (0xE0001, 0xE01F0)]
 
 # Public API
 __all__ = ["Completer", "IPCompleter"]
