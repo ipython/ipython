d61f11a9c0c5ceff2203ef6ee575c0a733328c00
mayank-bithash <mayankrai0030@gmail.com>
2026-05-22 15:30:02 +0530

    Add 'exception' as alias for 'exceptions' pdb command

--- IPython/core/debugger.py
+++ IPython/core/debugger.py
@@ -469,13 +469,14 @@ class Pdb(OldPdb):
                 self._chained_exceptions = tuple()
                 self._chained_exception_index = 0
 
-        def do_exceptions(self, arg):
+                def do_exceptions(self, arg):
             """exceptions [number]
             List or change current exception in an exception chain.
             Without arguments, list all the current exception in the exception
             chain. Exceptions will be numbered, with the current exception indicated
             with an arrow.
             If given an integer as argument, switch to the exception at that index.
+            ``exception`` can be used as an alias for this command.
             """
             if not self._chained_exceptions:
                 self.message(
