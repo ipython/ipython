bab28aaa24461eada45f24923f52fc8bc78d0df5
Jens H. Nielsen <jenielse@microsoft.com>
2026-06-01 09:14:15 +0200

    Annotate get_ipython

--- IPython/core/getipython.py
+++ IPython/core/getipython.py
@@ -1,23 +1,27 @@
-"""Simple function to call to get the current InteractiveShell instance
-"""
+"""Simple function to call to get the current InteractiveShell instance"""
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #  Copyright (C) 2013  The IPython Development Team
 #
 #  Distributed under the terms of the BSD License.  The full license is in
 #  the file COPYING, distributed as part of this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Classes and functions
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from IPython.core.interactiveshell import InteractiveShell
 
 
-def get_ipython():
+def get_ipython() -> "InteractiveShell | None":
     """Get the global InteractiveShell instance.
 
     Returns None if no InteractiveShell instance is registered.
     """
     from IPython.core.interactiveshell import InteractiveShell
+
     if InteractiveShell.initialized():
         return InteractiveShell.instance()
