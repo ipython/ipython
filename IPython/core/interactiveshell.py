ad31011d9004462b30868121a16c2f02a19041fe
Kavya <kavyapbarangi@gmail.com>
2026-07-24 17:20:24 +0530

    Refactor banner property logic (#15314)
    * Refactor banner property logic
    
    Refactor banner property to check SOURCE_DATE_EPOCH environment variable directly.
    
    * Update IPython/core/interactiveshell.py
    
    Co-authored-by: Gonzalo Tornaría <tornaria@cmat.edu.uy>
    
    * Update IPython/core/interactiveshell.py
    
    Co-authored-by: Gonzalo Tornaría <tornaria@cmat.edu.uy>
    
    * reformat
    
    ---------
    
    Co-authored-by: Gonzalo Tornaría <tornaria@cmat.edu.uy>
    Co-authored-by: M Bussonnier <bussonniermatthias@gmail.com>

--- IPython/core/interactiveshell.py
+++ IPython/core/interactiveshell.py
@@ -1034,8 +1034,11 @@ class InteractiveShell(SingletonConfigurable):
     @property
     def banner(self):
         banner = self.banner1
-        # Only use SOURCE_DATE_EPOCH if the user hasn't set a custom banner (default starts with "Python")
-        if banner is self.banner1 and (when := os.environ.get("SOURCE_DATE_EPOCH", None)) is not None:
+        # Only use SOURCE_DATE_EPOCH if the user hasn't set a custom banner
+        if (
+            banner is default_banner
+            and (when := os.environ.get("SOURCE_DATE_EPOCH", None)) is not None
+        ):
             from datetime import datetime
             date = datetime.fromtimestamp(int(when))
             banner = textwrap.dedent(
